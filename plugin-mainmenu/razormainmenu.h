7df3605d939cf62de6149fddc8674b794eb62544
SokoloffA <sokoloff.a@gmail.com>
2010-12-24 16:37:22 +0000

    The new realization of librasorqt    * XdgEnv    * XdgDesktopFile    * XdgMenu

--- plugin-mainmenu/razormainmenu.h
+++ plugin-mainmenu/razormainmenu.h
@@ -1,55 +1,46 @@
+/********************************************************************
+  Copyright: 2010 Alexander Sokoloff <sokoloff.a@gmail.ru>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License.
+  version 2 as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*********************************************************************/
+
 #ifndef RAZOR_MAINMENU_H
 #define RAZOR_MAINMENU_H
+
 #include "defs.h"
 #include "razorplugin.h"
-/**
- * @file razormainmenu.h
- * @brief declares Razormainmenu
- * @author Christopher "VdoP" Regali
- */
+#include <QPushButton>
 
-
-/**
- * @brief This makes the menugui - uses the xdgmanager for it
- */
-class RazorMainMenu : public QLabel
+class RazorMainMenu : public RazorPlugin
 {
     Q_OBJECT
 public:
-    RazorMainMenu(RazorPluginSquare* _owner);
+    RazorMainMenu(RazorBar * panel, QWidget * parent, const QString & name=0);
     ~RazorMainMenu();
-    void makeUp();
-private:
-    QAction* shutdown;
 
-    QAction* reboot;
-    QAction* logout;
-    QAction* about;
-    QMenu* mainMenu;
-    void createMenus();
-    QString icon;
-    QPixmap picon;
-    QString acticon;
-    QPixmap actpicon;
+    int widthForHeight(int h) { return mButton.sizeHint().width(); }
+    int heightForWidth(int w) { return w; }
+    RazorPlugin::RazorPluginSizing sizePriority() { return RazorPlugin::Static; }
+
 signals:
 
 protected:
-    void mousePressEvent(QMouseEvent *);
-    void enterEvent(QEvent *);
-    void leaveEvent(QEvent *);
-};
+    QPushButton mButton;
 
-/**
- * @brief the plugin-class for the mainmenu
- */
-class RazorMenu : public RazorPluginSquare
-{
-    Q_OBJECT
-public:
-    RazorMenu(RazorBar * panel, QWidget * parent, const QString & name);
-    ~RazorMenu();
-private:
-    RazorMainMenu* gui;
+private slots:
+    void showMenu();
 };
 
 extern "C" RazorPlugin* init(RazorBar* panel, QWidget* parent, const QString & name);
