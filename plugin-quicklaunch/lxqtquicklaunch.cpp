ed5082bb9c8673348eb7ff6f0e8b4379cf63b323
Paulo Lieuthier <paulolieuthier@gmail.com>
2014-07-17 10:23:37 -0300

    Fix dropping an action from menu

--- plugin-quicklaunch/lxqtquicklaunch.cpp
+++ plugin-quicklaunch/lxqtquicklaunch.cpp
@@ -194,12 +194,12 @@ void LxQtQuickLaunch::dropEvent(QDropEvent *e)
     // urls from mainmenu
     foreach (QUrl url, mime->urls())
     {
-    if (duplicates.contains(url))
-            continue;
-    else
-        duplicates << url;
+        if (duplicates.contains(url))
+                continue;
+        else
+            duplicates << url;
 
-        QString fileName(url.toLocalFile());
+        QString fileName(url.url());
         XdgDesktopFile * xdg = XdgDesktopFileCache::getFile(fileName);
         QFileInfo fi(fileName);
 
