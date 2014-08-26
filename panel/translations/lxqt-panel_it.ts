faf41834fcb4000f93dd211cce2f35e406e0f68f
dforsi <daniele@forsi.it>
2014-08-26 15:35:01 +0000

    Commit from LXDE Pootle server by user dforsi.: 57 of 57 strings translated (0 need review).

--- panel/translations/lxqt-panel_it.ts
+++ panel/translations/lxqt-panel_it.ts
@@ -1,89 +1,90 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.0">
+<!DOCTYPE TS>
+<TS language="it" version="2.0">
 <context>
     <name>ConfigPanelDialog</name>
     <message>
         <source>Configure panel</source>
-        <translation type="unfinished"/>
+        <translation>Configura il pannello</translation>
     </message>
     <message>
         <source>Panel size</source>
-        <translation type="unfinished"/>
+        <translation>Dimensione pannello</translation>
     </message>
     <message>
         <source>Size:</source>
-        <translation type="unfinished"/>
+        <translation>Dimensione:</translation>
     </message>
     <message>
         <source>px</source>
-        <translation type="unfinished"/>
+        <translation>px</translation>
     </message>
     <message>
         <source>Use automatic sizing</source>
-        <translation type="unfinished"/>
+        <translation>Usa dimensionamento automatico</translation>
     </message>
     <message>
         <source>Panel length &amp;&amp; position</source>
-        <translation type="unfinished"/>
+        <translation>Lunghezza e posizione del pannello</translation>
     </message>
     <message>
         <source>Left</source>
-        <translation type="unfinished"/>
+        <translation>Sinistra</translation>
     </message>
     <message>
         <source>Center</source>
-        <translation type="unfinished"/>
+        <translation>Centro</translation>
     </message>
     <message>
         <source>Right</source>
-        <translation type="unfinished"/>
+        <translation>Destra</translation>
     </message>
     <message>
         <source>%</source>
-        <translation type="unfinished"/>
+        <translation>%</translation>
     </message>
     <message>
         <source>Alignment:</source>
-        <translation type="unfinished"/>
+        <translation>Allineamento:</translation>
     </message>
     <message>
         <source>Length:</source>
-        <translation type="unfinished"/>
+        <translation>Lunghezza:</translation>
     </message>
     <message>
         <source>Position:</source>
-        <translation type="unfinished"/>
+        <translation>Posizione:</translation>
     </message>
     <message>
         <source>Top of desktop</source>
-        <translation type="unfinished"/>
+        <translation>Alto del desktop</translation>
     </message>
     <message>
         <source>Left of desktop</source>
-        <translation type="unfinished"/>
+        <translation>Sinistra del desktop</translation>
     </message>
     <message>
         <source>Right of desktop</source>
-        <translation type="unfinished"/>
+        <translation>Destra del desktop</translation>
     </message>
     <message>
         <source>Bottom of desktop</source>
-        <translation type="unfinished"/>
+        <translation>Basso del desktop</translation>
     </message>
     <message>
         <source>Top of desktop %1</source>
-        <translation type="unfinished"/>
+        <translation>Alto del desktop %1</translation>
     </message>
     <message>
         <source>Left of desktop %1</source>
-        <translation type="unfinished"/>
+        <translation>Sinistra del desktop %1</translation>
     </message>
     <message>
         <source>Right of desktop %1</source>
-        <translation type="unfinished"/>
+        <translation>Destra del desktop %1</translation>
     </message>
     <message>
         <source>Bottom of desktop %1</source>
-        <translation type="unfinished"/>
+        <translation>Basso del desktop %1</translation>
     </message>
 </context>
 <context>
@@ -94,33 +95,33 @@
     </message>
     <message>
         <source>Configure panel...</source>
-        <translation type="unfinished"/>
+        <translation>Configura pannello...</translation>
     </message>
     <message>
         <source>Add plugins ...</source>
-        <translation type="unfinished"/>
+        <translation>Aggiungi plugin...</translation>
     </message>
 </context>
 <context>
     <name>LxQtPanelPlugin</name>
     <message>
         <source>Configure</source>
-        <translation type="unfinished"/>
+        <translation>Configura</translation>
     </message>
     <message>
         <source>Move</source>
-        <translation type="unfinished"/>
+        <translation>Sposta</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"/>
+        <translation>Rimuovi</translation>
     </message>
 </context>
 <context>
     <name>LxQtPanelPrivate</name>
     <message>
         <source>Configure panel</source>
-        <translation type="unfinished"/>
+        <translation>Configura pannello</translation>
     </message>
 </context>
 </TS>
