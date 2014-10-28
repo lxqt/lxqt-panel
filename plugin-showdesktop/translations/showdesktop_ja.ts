64a9245eb149d166c7430c9b85adca1b7318c615
rago1975 <take1975@kumamoto.email.ne.jp>
2014-10-28 15:52:02 +0000

    Commit from LXDE Pootle server by user rago1975.: 10 of 10 strings translated (0 need review).

--- plugin-showdesktop/translations/showdesktop_ja.ts
+++ plugin-showdesktop/translations/showdesktop_ja.ts
@@ -1,9 +1,11 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.0">
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS language="ja" version="2.0">
 <context>
     <name>ShowDesktop</name>
     <message>
-        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="unfinished"/>
+        <source>Show Desktop: Global shortcut '%1' cannot be registered</source>
+        <translation>デスクトップの表示: グローバルなショートカット '%1' は登録できません</translation>
     </message>
     <message>
         <source>Show Desktop</source>
