0a27e5826060ab2d8c1effef3b4e8f3cab6ddf12
Jakub Laník - PC <kubanecxxx@gmail.com>
2014-12-22 13:39:06 +0100

    added licence into source files

--- plugin-taskbar/lxqtmasterpopup.h
+++ plugin-taskbar/lxqtmasterpopup.h
@@ -1,3 +1,34 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ * http://lxqt.org
+ *
+ * Copyright: 2011 Razor team
+ *            2014 LXQt team
+ * Authors:
+ *   Alexander Sokoloff <sokoloff.a@gmail.com>
+ *   Maciej Płaza <plaza.maciej@gmail.com>
+ *   Kuzma Shapran <kuzma.shapran@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
 #ifndef LXQTMASTERPOPUP_H
 #define LXQTMASTERPOPUP_H
 
