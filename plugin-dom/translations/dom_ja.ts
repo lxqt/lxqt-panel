41ceed003adc46433468e882985c7369c0462327
Mika Kobayashi <mika-k@users.noreply.github.com>
2015-01-17 20:45:34 -0500

    Update Japanese translation

---
+++ plugin-dom/translations/dom_ja.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>TreeWindow</name>
+    <message>
+        <location filename="../treewindow.ui" line="14"/>
+        <source>Panel DOM tree</source>
+        <translation>パネルのDOMツリー</translation>
+    </message>
+    <message>
+        <location filename="../treewindow.ui" line="48"/>
+        <source>Property</source>
+        <translation>プロパティー</translation>
+    </message>
+    <message>
+        <location filename="../treewindow.ui" line="53"/>
+        <source>Value</source>
+        <translation>値</translation>
+    </message>
+</context>
+</TS>
