8296d8d1045974cf6d9543dfa5eb7e2cf598ab35
Luís Pereira <luis.artur.pereira@gmail.com>
2015-03-29 09:08:22 -0700

    Updates translations sources
    Closes lxde/lxqt#566.

--- plugin-directorymenu/translations/directorymenu_de_DE.ts
+++ plugin-directorymenu/translations/directorymenu_de_DE.ts
@@ -1,15 +1,73 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.1" language="en_US">
 <context>
     <name>DirectoryMenu</name>
     <message>
         <source>Open directory</source>
-        <translation>Öffne Ordner</translation>
+        <translation type="vanished">Öffne Ordner</translation>
     </message>
     <message>
         <source>Open in terminal</source>
-        <translation>Öffne in Terminal</translation>
+        <translation type="vanished">Öffne in Terminal</translation>
+    </message>
+    <message>
+        <location filename="../directorymenu.cpp" line="146"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryMenuConfiguration</name>
+    <message>
+        <location filename="../directorymenuconfiguration.ui" line="14"/>
+        <source>Directory Menu Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.ui" line="26"/>
+        <source>Base directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.ui" line="39"/>
+        <source>BaseDirectoryName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.ui" line="46"/>
+        <source>Icon:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.cpp" line="107"/>
+        <source>Choose Base Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.cpp" line="122"/>
+        <source>Choose Icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.cpp" line="123"/>
+        <source>Icons (*.png *.xpm *.jpg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.cpp" line="131"/>
+        <source>Directory Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../directorymenuconfiguration.cpp" line="131"/>
+        <source>An error occurred while loading the icon.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
