ddf9d5663af9dc0be7bb355f7a031e91903c230e
zvacet <ikoli@yahoo.com>
2015-05-21 20:28:42 +0200

    Create mainmenu_hr.ts

---
+++ plugin-mainmenu/mainmenu_hr.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr_HR">
+<context>
+    <name>LxQtMainMenu</name>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="245"/>
+        <source>Show/hide main menu</source>
+        <translation type="unfinished">Pokaži/sakrij glavni izbornik</translation>
+    </message>
+</context>
+<context>
+    <name>LxQtMainMenuConfiguration</name>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation type="unfinished">Općenito</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="14"/>
+        <source>Main Menu settings</source>
+        <translation type="unfinished">Postavke glavnoga izbornika</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="35"/>
+        <source>Button text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="52"/>
+        <source>Custom font size:</source>
+        <translation type="unfinished">Prilagođena veličina fonta:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="62"/>
+        <source> pt</source>
+        <translation type="unfinished"> pt</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="81"/>
+        <source>Menu file</source>
+        <translation type="unfinished">Datoteka izbornika</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="87"/>
+        <source>Menu file:</source>
+        <translation type="unfinished">Datoteka izbornika:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="101"/>
+        <source>...</source>
+        <translation type="unfinished">...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="111"/>
+        <source>Keyboard Shortcut</source>
+        <translation type="unfinished">Kratica tipkovnice</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="130"/>
+        <source>Click the button to record shortcut:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="55"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="100"/>
+        <source>Choose menu file</source>
+        <translation type="unfinished">Izaberite datoteku izbornika</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="100"/>
+        <source>Menu files (*.menu)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
