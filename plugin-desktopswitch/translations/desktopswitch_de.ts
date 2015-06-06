c77b3ad74076718e9b3e6e572fe48fb3bd51f8a0
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-06 13:04:52 +0200

    Updated german translation.

--- plugin-desktopswitch/translations/desktopswitch_de.ts
+++ plugin-desktopswitch/translations/desktopswitch_de.ts
@@ -4,13 +4,13 @@
 <context>
     <name>DesktopSwitch</name>
     <message>
-        <location filename="../desktopswitch.cpp" line="83"/>
+        <location filename="../desktopswitch.cpp" line="85"/>
         <source>Switch to desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Arbeitsfläche %1 wechseln</translation>
     </message>
     <message>
-        <location filename="../desktopswitch.cpp" line="123"/>
-        <location filename="../desktopswitch.cpp" line="133"/>
+        <location filename="../desktopswitch.cpp" line="141"/>
+        <location filename="../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
         <translation>Arbeitsfläche %1</translation>
     </message>
@@ -20,27 +20,37 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Arbeitsflächenumschalter - Einstellungen</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="20"/>
-        <source>Number of rows:</source>
-        <translation type="unfinished"></translation>
+        <source>Rows</source>
+        <translation>Zeilen</translation>
     </message>
     <message>
-        <location filename="../desktopswitchconfiguration.ui" line="40"/>
+        <location filename="../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
-        <translation type="unfinished"></translation>
+        <translation>Arbeitsflächenbezeichnungen:</translation>
     </message>
     <message>
-        <location filename="../desktopswitchconfiguration.ui" line="58"/>
+        <location filename="../desktopswitchconfiguration.ui" line="46"/>
+        <source>Number of rows:</source>
+        <translation>Anzahl der Zeilen:</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziffern</translation>
     </message>
     <message>
-        <location filename="../desktopswitchconfiguration.ui" line="63"/>
+        <location filename="../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
-        <translation type="unfinished"></translation>
+        <translation>Namen</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="70"/>
+        <source>Desktop names</source>
+        <translation>Arbeitsflächenbezeichnungen</translation>
     </message>
 </context>
 </TS>
