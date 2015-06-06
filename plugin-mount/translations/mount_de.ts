d5d6b2dffb7241cec2c11b01aed226749aa97141
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-06 19:38:48 +0200

    Updated german translation for mount plugin.

--- plugin-mount/translations/mount_de.ts
+++ plugin-mount/translations/mount_de.ts
@@ -2,105 +2,81 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de">
 <context>
-    <name>DeviceActionInfo</name>
+    <name>Button</name>
     <message>
-        <location filename="../actions/deviceaction_info.cpp" line="45"/>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde verbunden.</translation>
-    </message>
-    <message>
-        <location filename="../actions/deviceaction_info.cpp" line="53"/>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde entfernt.</translation>
-    </message>
-    <message>
-        <location filename="../actions/deviceaction_info.cpp" line="59"/>
+        <location filename="../button.cpp" line="36"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Entfernbare Medien-/Geräteverwaltung</translation>
+        <translation>Entfernbare Medien-/Geräteverwaltung</translation>
     </message>
 </context>
 <context>
-    <name>LxQtMountConfiguration</name>
+    <name>Configuration</name>
     <message>
-        <source>LxQt Removable media manager settings</source>
-        <translation type="vanished">LxQt entfernbare Medienverwaltung-Einstellungen</translation>
-    </message>
-    <message>
-        <location filename="../lxqtmountconfiguration.ui" line="14"/>
+        <location filename="../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernbare Medien - Einstellungen</translation>
     </message>
     <message>
-        <location filename="../lxqtmountconfiguration.ui" line="20"/>
+        <location filename="../configuration.ui" line="20"/>
         <source>Behaviour</source>
         <translation>Verhalten</translation>
     </message>
     <message>
-        <location filename="../lxqtmountconfiguration.ui" line="26"/>
-        <source>When a device is connected </source>
-        <translation>Wenn ein Gerät verbunden wurde</translation>
+        <location filename="../configuration.ui" line="26"/>
+        <source>When a device is connected :</source>
+        <translation>Wenn ein Gerät verbunden ist:</translation>
     </message>
     <message>
-        <location filename="../lxqtmountconfiguration.cpp" line="44"/>
+        <location filename="../configuration.cpp" line="40"/>
         <source>Popup menu</source>
         <translation>Aufklapp-Menü</translation>
     </message>
     <message>
-        <location filename="../lxqtmountconfiguration.cpp" line="45"/>
+        <location filename="../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation>Zeige Informationen</translation>
+        <translation>Informationen anzeigen</translation>
     </message>
     <message>
-        <location filename="../lxqtmountconfiguration.cpp" line="46"/>
+        <location filename="../configuration.cpp" line="42"/>
         <source>Do nothing</source>
         <translation>Nichts tun</translation>
     </message>
 </context>
 <context>
-    <name>MenuDiskItem</name>
+    <name>DeviceActionInfo</name>
     <message>
-        <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Klicken, um auf dieses Gerät von anderen Anwendungen aus zuzugreifen.</translation>
+        <location filename="../actions/deviceaction_info.cpp" line="40"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
+        <translation>Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde verbunden.</translation>
     </message>
     <message>
-        <source>Click to eject this disc.</source>
-        <translation type="vanished">Klicken, um diese CD auszuwerfen.</translation>
+        <location filename="../actions/deviceaction_info.cpp" line="45"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
+        <translation>Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde entfernt.</translation>
     </message>
     <message>
-        <location filename="../menudiskitem.cpp" line="120"/>
-        <location filename="../menudiskitem.cpp" line="155"/>
+        <location filename="../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Entfernbare Medien-/Geräteverwaltung</translation>
-    </message>
-    <message>
-        <location filename="../menudiskitem.cpp" line="121"/>
-        <source>Mounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../menudiskitem.cpp" line="156"/>
-        <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernbare Medien-/Geräteverwaltung</translation>
     </message>
 </context>
 <context>
-    <name>MountButton</name>
+    <name>MenuDiskItem</name>
     <message>
-        <location filename="../mountbutton.cpp" line="38"/>
+        <location filename="../menudiskitem.cpp" line="153"/>
+        <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
+        <translation>Das Einbinden von &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; ist fehlgeschlagen: %2</translation>
+    </message>
+    <message>
+        <location filename="../menudiskitem.cpp" line="155"/>
+        <location filename="../menudiskitem.cpp" line="176"/>
         <source>Removable media/devices manager</source>
         <translation>Entfernbare Medien-/Geräteverwaltung</translation>
     </message>
     <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde verbunden.</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Das Gerät &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; wurde entfernt.</translation>
-    </message>
-    <message>
-        <source>No devices Available.</source>
-        <translation type="vanished">Kein Gerät verfügbar.</translation>
+        <location filename="../menudiskitem.cpp" line="174"/>
+        <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
+        <translation>Das Lösen von &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; ist fehlgeschlagen: %2</translation>
     </message>
 </context>
 <context>
@@ -108,7 +84,7 @@
     <message>
         <location filename="../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Geräte verfügbar</translation>
     </message>
 </context>
 </TS>
