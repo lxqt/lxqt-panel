3bb32cc8f2ed1a7f312f88b89bffdc7773eae0d1
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-06 19:37:54 +0200

    Added german translation for kbindicator plugin.

---
+++ plugin-kbindicator/translations/kbindicator_de.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>LxQtKbIndicatorConfiguration</name>
+    <message>
+        <location filename="../lxqtkbindicatorconfiguration.ui" line="14"/>
+        <source>Keyboard Indicator Settings</source>
+        <translation>Tastatur-LED-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../lxqtkbindicatorconfiguration.ui" line="20"/>
+        <source>Caps Lock</source>
+        <translation>Feststelltaste</translation>
+    </message>
+    <message>
+        <location filename="../lxqtkbindicatorconfiguration.ui" line="27"/>
+        <source>Num Lock</source>
+        <translation>NumLock-Taste</translation>
+    </message>
+    <message>
+        <location filename="../lxqtkbindicatorconfiguration.ui" line="34"/>
+        <source>Scroll Lock</source>
+        <translation>Rollen-Taste</translation>
+    </message>
+</context>
+</TS>
