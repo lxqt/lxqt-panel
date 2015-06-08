73595852411160d3fb35ff6620b2d3595b812ece
Palo Kisa <palo.kisa@gmail.com>
2015-06-08 12:51:51 +0200

    plugin-spacer: slovak translation

---
+++ plugin-spacer/translations/spacer_sk.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>Nastavenia medzery</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>Šírka medzery:</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>Typ medzery:</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation>plná čiara</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation>bodkovaná čiara</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation>neviditeľná</translation>
+    </message>
+</context>
+</TS>
