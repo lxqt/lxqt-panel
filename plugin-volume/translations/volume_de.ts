2c85e76f64a87526f3d2df115d8244f8fe60c249
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-10 23:00:41 +0200

    Updated german translation for volume plugin.

--- plugin-volume/translations/volume_de.ts
+++ plugin-volume/translations/volume_de.ts
@@ -1,54 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.1" language="de">
 <context>
     <name>LxQtVolume</name>
     <message>
-        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="vanished">Zeige Desktop: Globales Tastenkürzel &apos;%1&apos; kann nicht vergeben werden</translation>
-    </message>
-    <message>
         <location filename="../lxqtvolume.cpp" line="63"/>
         <source>Increase sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke erhöhen</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="69"/>
         <source>Decrease sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke verringern</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="75"/>
         <source>Mute/unmute sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke stummschalten/wiederherstellen</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="136"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärkeeinstellung: Die folgenden Tastaturkürzel konnten nicht registriert werden: %1</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="219"/>
         <location filename="../lxqtvolume.cpp" line="229"/>
         <source>Volume: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke: %1</translation>
     </message>
 </context>
 <context>
     <name>LxQtVolumeConfiguration</name>
     <message>
-        <source>LxQt Volume Control Settings</source>
-        <translation type="vanished">LxQt Lautstärkeregelung</translation>
-    </message>
-    <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärkeeinstellungen</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation>Zu regelndes Gerät</translation>
+        <translation>Zu steuerndes Gerät</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
@@ -63,7 +55,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="42"/>
         <source>OSS</source>
-        <translation type="unfinished"></translation>
+        <translation>OSS</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
@@ -83,30 +75,30 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation>Lautstärken über 100% (0dB) erlauben</translation>
+        <translation>Lautstärke über 100% (0dB) erlauben</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="92"/>
         <source>Volume adjust step</source>
-        <translation>Lautstärken Schrittweite</translation>
+        <translation>Lautstärkeschrittweite</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="111"/>
         <source>External Mixer</source>
-        <translation>Externer Mixer</translation>
+        <translation>Externer Mischer</translation>
     </message>
 </context>
 <context>
     <name>VolumePopup</name>
     <message>
-        <location filename="../volumepopup.cpp" line="54"/>
+        <location filename="../volumepopup.cpp" line="53"/>
         <source>Launch mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Mischer starten</translation>
     </message>
     <message>
-        <location filename="../volumepopup.cpp" line="55"/>
-        <source>Mixer</source>
-        <translation type="unfinished"></translation>
+        <location filename="../volumepopup.cpp" line="54"/>
+        <source>Mi&amp;xer</source>
+        <translation>&amp;Mischer</translation>
     </message>
 </context>
 </TS>
