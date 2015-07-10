1178c0f2763944d620ecb01e4369fe97d395a4b9
Michael Vetter <g.bluehut@gmail.com>
2015-07-10 13:34:16 +0200

    Complete dutch translation
    Regards lxde/lxqt#409

--- plugin-networkmonitor/translations/networkmonitor_nl.ts
+++ plugin-networkmonitor/translations/networkmonitor_nl.ts
@@ -58,7 +58,7 @@
     <message>
         <location filename="../lxqtnetworkmonitorconfiguration.ui" line="14"/>
         <source>Network Monitor settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Netwerkmonitorinstellingen</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitorconfiguration.ui" line="20"/>
