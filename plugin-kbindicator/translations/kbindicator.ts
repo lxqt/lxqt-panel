a8bfeda5fcd69993a9429df2ce8b214cb7fee694
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-07-12 23:06:28 +0200

    Re-added german translation.

---
+++ plugin-kbindicator/translations/kbindicator.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../src/content.cpp" line="60"/>
+        <source>Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/content.cpp" line="60"/>
+        <source>Variant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="20"/>
+        <source>Leds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="74"/>
+        <source>Show keyboard layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="99"/>
+        <source>Show flags instead labels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="106"/>
+        <source>Layout mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="116"/>
+        <source>Global</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="129"/>
+        <source>Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="142"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="152"/>
+        <source>Configure layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
