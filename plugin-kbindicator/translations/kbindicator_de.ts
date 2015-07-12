eb9c0a7aec72503f19ff825018f8fcecf1c4c6cb
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-07-12 23:06:28 +0200

    Re-added german translation.

---
+++ plugin-kbindicator/translations/kbindicator_de.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../src/content.cpp" line="60"/>
+        <source>Layout</source>
+        <translation>Layout</translation>
+    </message>
+    <message>
+        <location filename="../src/content.cpp" line="60"/>
+        <source>Variant</source>
+        <translation>Variante</translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">blahblahblah</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="20"/>
+        <source>Leds</source>
+        <translation>LEDs</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation>Feststelltaste anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation>NumLock-Taste anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation>Rollen-Taste anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="74"/>
+        <source>Show keyboard layout</source>
+        <translation>Tastatur-Layout anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="99"/>
+        <source>Show flags instead labels</source>
+        <translation>Flaggen statt Kennung anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="106"/>
+        <source>Layout mode:</source>
+        <translation>Layout-Modus:</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="116"/>
+        <source>Global</source>
+        <translation>Global</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="129"/>
+        <source>Window</source>
+        <translation>Fenster</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="142"/>
+        <source>Application</source>
+        <translation>Anwendung</translation>
+    </message>
+    <message>
+        <location filename="../src/kbdstateconfig.ui" line="152"/>
+        <source>Configure layouts</source>
+        <translation>Layout konfigurieren...</translation>
+    </message>
+</context>
+</TS>
