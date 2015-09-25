0fa681e98792336112fea5a86bf849e37c32b796
Palo Kisa <palo.kisa@gmail.com>
2015-09-25 14:32:47 +0200

    plugin-statusnotifier: better callable checking for static_assert

--- plugin-statusnotifier/sniasync.h
+++ plugin-statusnotifier/sniasync.h
@@ -31,21 +31,37 @@
 #include <functional>
 #include "statusnotifieriteminterface.h"
 
-template<typename> struct remove_class_type; // undefined
+template<typename>
+struct remove_class_type { using type = void; }; // bluff
 template<typename C, typename R, typename... ArgTypes>
 struct  remove_class_type<R (C::*)(ArgTypes...)> { using type = R(ArgTypes...); };
 template<typename C, typename R, typename... ArgTypes>
 struct remove_class_type<R (C::*)(ArgTypes...) const> { using type = R(ArgTypes...); };
 
-template <typename> struct lambda_signature; //undefined
 template <typename L>
-struct lambda_signature : public remove_class_type<decltype(&L::operator())> {};
+class call_sig_helper
+{
+    template <typename L1>
+        static decltype(&L1::operator()) test(int);
+    template <typename L1>
+        static void test(...); //bluff
+public:
+    using type = decltype(test<L>(0));
+};
+template <typename L>
+struct call_signature : public remove_class_type<typename call_sig_helper<L>::type> {};
+template <typename R, typename... ArgTypes>
+struct call_signature<R (ArgTypes...)> { using type = R (ArgTypes...); };
+template <typename R, typename... ArgTypes>
+struct call_signature<R (*)(ArgTypes...)> { using type = R (ArgTypes...); };
+template <typename C, typename R, typename... ArgTypes>
+struct call_signature<R (C::*)(ArgTypes...)> { using type = R (ArgTypes...); };
+template<typename C, typename R, typename... ArgTypes>
+struct call_signature<R (C::*)(ArgTypes...) const>  { using type = R(ArgTypes...); };
 
 template <typename> struct is_valid_signature : public std::false_type {};
 template <typename Arg>
 struct is_valid_signature<void (Arg)> : public std::true_type {};
-template <typename L>
-struct is_valid_lambda : public is_valid_signature<typename lambda_signature<L>::type> {};
 
 class SniAsync : public QObject
 {
@@ -56,7 +72,7 @@ public:
     template <typename F>
     inline void propertyGetAsync(QString const &name, F finished)
     {
-        static_assert(is_valid_lambda<F>::value, "need lambda [] (Arg) -> void");
+        static_assert(is_valid_signature<typename call_signature<F>::type>::value, "need callable (lambda, *function, callable obj) (Arg) -> void");
         connect(new QDBusPendingCallWatcher{asyncPropGet(name), this},
                 &QDBusPendingCallWatcher::finished,
                 [this, finished, name] (QDBusPendingCallWatcher * call)
@@ -64,7 +80,7 @@ public:
                     QDBusPendingReply<QVariant> reply = *call;
                     if (reply.isError())
                         qDebug() << "Error on DBus request:" << reply.error();
-                    finished(qdbus_cast<typename std::function<typename lambda_signature<F>::type>::argument_type>(reply.value()));
+                    finished(qdbus_cast<typename std::function<typename call_signature<F>::type>::argument_type>(reply.value()));
                     call->deleteLater();
                 }
         );
