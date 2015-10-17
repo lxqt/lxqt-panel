3aa6e9fdf8e0eead17d9af3dcd883263de365032
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-10-17 22:56:37 +0200

    Updated german translation.

--- plugin-kbindicator/translations/kbindicator_de.ts
+++ plugin-kbindicator/translations/kbindicator_de.ts
@@ -4,51 +4,51 @@
 <context>
     <name>Content</name>
     <message>
-        <location filename="../src/content.cpp" line="17"/>
+        <location filename="../src/content.cpp" line="43"/>
         <source>C</source>
         <comment>Label for CapsLock indicator</comment>
         <translatorcomment>This capital letter is printed on my keyboard.</translatorcomment>
         <translation>A</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="20"/>
+        <location filename="../src/content.cpp" line="46"/>
         <source>CapsLock</source>
         <comment>Tooltip for CapsLock indicator</comment>
         <translation>Großbuchstaben</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="24"/>
+        <location filename="../src/content.cpp" line="50"/>
         <source>N</source>
         <comment>Label for NumLock indicator</comment>
         <translatorcomment>This digit is printed on my keyboard.</translatorcomment>
         <translation>1</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="26"/>
+        <location filename="../src/content.cpp" line="52"/>
         <source>NumLock</source>
         <comment>Tooltip for NumLock indicator</comment>
         <translation>Ziffern</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="31"/>
+        <location filename="../src/content.cpp" line="57"/>
         <source>S</source>
         <comment>Label for ScrollLock indicator</comment>
         <translatorcomment>From the word &quot;Rollen&quot;.</translatorcomment>
         <translation>R</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="33"/>
+        <location filename="../src/content.cpp" line="59"/>
         <source>ScrollLock</source>
         <comment>Tooltip for ScrollLock indicator</comment>
         <translation>Rollen</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="63"/>
+        <location filename="../src/content.cpp" line="90"/>
         <source>Layout</source>
         <translation>Layout</translation>
     </message>
     <message>
-        <location filename="../src/content.cpp" line="63"/>
+        <location filename="../src/content.cpp" line="90"/>
         <source>Variant</source>
         <translation>Variante</translation>
     </message>
@@ -57,13 +57,13 @@
     <name>KbdStateConfig</name>
     <message>
         <location filename="../src/kbdstateconfig.ui" line="14"/>
-        <source>Keyboard indicator settings</source>
-        <translation>Tastaturanzeigen - Einstellungen</translation>
+        <source>Keyboard state settings</source>
+        <translation>Tastaturstatus - Einstellungen</translation>
     </message>
     <message>
         <location filename="../src/kbdstateconfig.ui" line="20"/>
-        <source>LEDs</source>
-        <translation>LEDs</translation>
+        <source>Lock Indicators</source>
+        <translation>Schalteranzeigen</translation>
     </message>
     <message>
         <location filename="../src/kbdstateconfig.ui" line="26"/>
@@ -81,39 +81,34 @@
         <translation>Rollen-Taste anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="74"/>
-        <source>Show keyboard layout</source>
+        <location filename="../src/kbdstateconfig.ui" line="50"/>
+        <source>Keyboard Layout Indicator</source>
         <translation>Tastatur-Layout anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="99"/>
-        <source>Show flags instead labels</source>
-        <translation>Flaggen statt Kennung anzeigen</translation>
+        <location filename="../src/kbdstateconfig.ui" line="62"/>
+        <source>Switching policy</source>
+        <translation>Umschaltrichtlinie</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="106"/>
-        <source>Layout mode:</source>
-        <translation>Layout-Modus:</translation>
+        <location filename="../src/kbdstateconfig.ui" line="102"/>
+        <source>Configure layouts</source>
+        <translation>Layout konfigurieren</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="116"/>
+        <location filename="../src/kbdstateconfig.ui" line="69"/>
         <source>Global</source>
         <translation>Global</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="129"/>
+        <location filename="../src/kbdstateconfig.ui" line="79"/>
         <source>Window</source>
         <translation>Fenster</translation>
     </message>
     <message>
-        <location filename="../src/kbdstateconfig.ui" line="142"/>
+        <location filename="../src/kbdstateconfig.ui" line="89"/>
         <source>Application</source>
         <translation>Anwendung</translation>
-    </message>
-    <message>
-        <location filename="../src/kbdstateconfig.ui" line="152"/>
-        <source>Configure layouts...</source>
-        <translation>Layout konfigurieren...</translation>
     </message>
 </context>
 </TS>
