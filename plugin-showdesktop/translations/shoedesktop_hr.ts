9ad322979dc9314b9a57378663ec10c65fdf8291
zvacet <ikoli@yahoo.com>
2015-10-29 17:18:34 +0100

    Create shoedesktop_hr.ts

---
+++ plugin-showdesktop/translations/shoedesktop_hr.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>ShowDesktop</name>
+    <message>
+        <location filename="../showdesktop.cpp" line="48"/>
+        <source>Show desktop</source>
+        <translation type="unfinished">Pokaži radnu površinu</translation>
+    </message>
+    <message>
+        <location filename="../showdesktop.cpp" line="69"/>
+        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../showdesktop.cpp" line="55"/>
+        <source>Show Desktop</source>
+        <translation type="unfinished">Pokaži Radnu površinu</translation>
+    </message>
+</context>
+</TS>
