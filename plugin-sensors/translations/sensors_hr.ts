e35964d0be46b1dacea9735cdf6593f883811547
zvacet <ikoli@yahoo.com>
2015-10-29 17:14:34 +0100

    Create sensors_hr.ts

---
+++ plugin-sensors/translations/sensors_hr.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>LXQtSensorsConfiguration</name>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="14"/>
+        <source>Sensors Settings</source>
+        <translation type="unfinished">Postavke senzora</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="30"/>
+        <source>Common</source>
+        <translation type="unfinished">Uobičajeno</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="42"/>
+        <source>Update interval (seconds)</source>
+        <translation type="unfinished">Interval ažuriranja (sekunde)</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="49"/>
+        <source>Temperature bar width</source>
+        <translation type="unfinished">Duljina trake temperature</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="104"/>
+        <source>Temperature scale</source>
+        <translation type="unfinished">Skala temperature</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="110"/>
+        <source>Celsius</source>
+        <translation type="unfinished">Celzijus</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="120"/>
+        <source>Fahrenheit</source>
+        <translation type="unfinished">Fahrenheit</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
+        <source>Blink progress bars when the temperature is too high</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
+        <source>Warning about high temperature</source>
+        <translation type="unfinished">Upozori na visoku temperaturu</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="152"/>
+        <source>Sensors</source>
+        <translation type="unfinished">Senzori</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="168"/>
+        <source>Detected chips:</source>
+        <translation type="unfinished">Nađeni čipovi:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.ui" line="175"/>
+        <source>Chip features:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.cpp" line="212"/>
+        <source>Enabled</source>
+        <translation type="unfinished">Omogućeno</translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.cpp" line="212"/>
+        <source>Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtsensorsconfiguration.cpp" line="212"/>
+        <source>Color</source>
+        <translation type="unfinished">Boja</translation>
+    </message>
+</context>
+</TS>
