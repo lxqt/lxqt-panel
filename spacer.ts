86f03fa153f217a4e3e90d3ed94c28ca447ac870
Palo Kisa <palo.kisa@gmail.com>
2016-01-15 21:40:12 +0100

    lxqt-panel: Move spacer's translations

---
+++ spacer.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ spacer_it.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="it">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>Impostazioni spaziatore</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>Larghezza:</translation>
+    </message>
+    <message>
+        <location filename="../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>Tipo:</translation>
+    </message>
+</context>
+</TS>
