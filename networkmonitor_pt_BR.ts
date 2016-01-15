316d4915adb103cd2cf0f7df80acfbe63728ace6
Palo Kisa <palo.kisa@gmail.com>
2016-01-15 21:39:49 +0100

    lxqt-panel: Move networkmonitor's translations

---
+++ networkmonitor_pt_BR.ts
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>LXQtNetworkMonitor</name>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="161"/>
+        <source>Network interface &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Interface de rede &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="162"/>
+        <source>Transmitted %1</source>
+        <translation>Transmitido %1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="163"/>
+        <source>Received %1</source>
+        <translation>Recebido %1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="208"/>
+        <source>B</source>
+        <translation>Byte</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
+        <source>KiB</source>
+        <translation>KByte</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
+        <source>MiB</source>
+        <translation>MByte</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
+        <source>GiB</source>
+        <translation>GByte</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
+        <source>TiB</source>
+        <translation>TByte</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
+        <source>PiB</source>
+        <translation>PByte</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtNetworkMonitorConfiguration</name>
+    <message>
+        <source>LXQt Network Monitor settings</source>
+        <translation type="vanished">Configurações de Monitoramento de Rede</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="14"/>
+        <source>Network Monitor settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Geral</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="35"/>
+        <source>Interface</source>
+        <translation>Interface</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="56"/>
+        <source>Modem</source>
+        <translation>Modem</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="65"/>
+        <source>Monitor</source>
+        <translation>Monitoramento</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="74"/>
+        <source>Network</source>
+        <translation>Rede</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="83"/>
+        <source>Wireless</source>
+        <translation>Sem fio</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="95"/>
+        <source>Icon</source>
+        <translation>Ícone</translation>
+    </message>
+</context>
+</TS>
