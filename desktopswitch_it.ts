8e7b93024374239b2359e0d0f68260837262acb8
Stefano Binde <stef.binde@riseup.net>
2016-05-02 13:40:26 +0200

    Update desktopswitch_it.ts
    Updated italian translation

--- desktopswitch_it.ts
+++ desktopswitch_it.ts
@@ -1,16 +1,16 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="it">
+<TS version="2.1" language="it">
 <context>
     <name>DesktopSwitch</name>
     <message>
-        <location filename="../../../desktopswitch.cpp" line="83"/>
+        <location filename="../../../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
         <translation>Passa al desktop %1</translation>
     </message>
     <message>
-        <location filename="../../../desktopswitch.cpp" line="123"/>
-        <location filename="../../../desktopswitch.cpp" line="133"/>
+        <location filename="../../../desktopswitch.cpp" line="141"/>
+        <location filename="../../../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
         <translation>Desktop %1</translation>
     </message>
@@ -24,23 +24,33 @@
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Aspetto</translation>
+    </message>
+    <message>
+        <location filename="../../../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
         <translation>Numero righe:</translation>
     </message>
     <message>
-        <location filename="../../../desktopswitchconfiguration.ui" line="40"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
         <translation>Etichette desktop:</translation>
     </message>
     <message>
-        <location filename="../../../desktopswitchconfiguration.ui" line="58"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
         <translation>Numeri</translation>
     </message>
     <message>
-        <location filename="../../../desktopswitchconfiguration.ui" line="63"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
         <translation>Nomi</translation>
+    </message>
+    <message>
+        <location filename="../../../desktopswitchconfiguration.ui" line="70"/>
+        <source>Desktop names</source>
+        <translation>Nomi dei desktop</translation>
     </message>
 </context>
 </TS>
