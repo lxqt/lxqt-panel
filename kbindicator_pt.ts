a4d9838f0e8772857f3d922ee6b614c56b222bc3
Palo Kisa <palo.kisa@gmail.com>
2016-05-02 17:39:32 +0200

    kbindicator: (Re)Add .ts translations
    As they were removed in @3b9c7b9

---
+++ kbindicator_pt.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../../../src/content.cpp" line="43"/>
+        <source>C</source>
+        <comment>Label for CapsLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="46"/>
+        <source>CapsLock</source>
+        <comment>Tooltip for CapsLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="50"/>
+        <source>N</source>
+        <comment>Label for NumLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="52"/>
+        <source>NumLock</source>
+        <comment>Tooltip for NumLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="57"/>
+        <source>S</source>
+        <comment>Label for ScrollLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="59"/>
+        <source>ScrollLock</source>
+        <comment>Tooltip for ScrollLock indicator</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Variant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="14"/>
+        <source>Keyboard state settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="20"/>
+        <source>Lock Indicators</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="50"/>
+        <source>Keyboard Layout Indicator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="62"/>
+        <source>Switching policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="69"/>
+        <source>Global</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="79"/>
+        <source>Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="89"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="102"/>
+        <source>Configure layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LxQtKbIndicatorConfiguration</name>
+    <message>
+        <source>LxQt Keyboard Indicator Settings</source>
+        <translation type="vanished">Definições do indicador de teclado do LxQt</translation>
+    </message>
+    <message>
+        <source>&amp;Caps lock</source>
+        <translation type="vanished">&amp;Caps lock</translation>
+    </message>
+    <message>
+        <source>&amp;Num lock</source>
+        <translation type="vanished">&amp;Num lock</translation>
+    </message>
+    <message>
+        <source>&amp;Scroll lock</source>
+        <translation type="vanished">&amp;Scroll lock</translation>
+    </message>
+    <message>
+        <source>&amp;Advanced control</source>
+        <translation type="vanished">Controlo &amp;avançado</translation>
+    </message>
+    <message>
+        <source>&amp;Bit</source>
+        <translation type="vanished">&amp;Bits</translation>
+    </message>
+    <message>
+        <source>&amp;Text</source>
+        <translation type="vanished">&amp;Texto</translation>
+    </message>
+    <message>
+        <source>Keyboard Indicator Settings</source>
+        <translation type="vanished">Definições do indicador de teclado</translation>
+    </message>
+    <message>
+        <source>Caps Lock</source>
+        <translation type="vanished">Caps Lock</translation>
+    </message>
+    <message>
+        <source>Num Lock</source>
+        <translation type="vanished">Num Lock</translation>
+    </message>
+    <message>
+        <source>Scroll Lock</source>
+        <translation type="vanished">Scroll Lock</translation>
+    </message>
+</context>
+</TS>
