73bd9f48c79b0c068b1242c3bb1675aec6cede5f
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- quicklaunch_ar.ts
+++ quicklaunch_ar.ts
@@ -6,18 +6,18 @@
     <message>
         <location filename="../../../lxqtquicklaunch.cpp" line="213"/>
         <source>Drop Error</source>
-        <translation>خطاٌ في النَّقل</translation>
+        <translation>خطأ في الإفلات</translation>
     </message>
     <message>
         <location filename="../../../lxqtquicklaunch.cpp" line="214"/>
         <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
-        <translation>ﻻ يمكن تضمين الملف أو الرَّابط %1 في لوحة البدء السَّريع حاليَّاً</translation>
+        <translation>يتعذّر الآن تضمين العنوان/المسار &apos;%1&apos; إلى المُطلق السّريع</translation>
     </message>
     <message>
         <location filename="../../../lxqtquicklaunch.cpp" line="320"/>
         <source>Drop application
 icons here</source>
-        <translation type="unfinished"></translation>
+        <translation>أفلت أيقونات التّطبيقات هنا</translation>
     </message>
 </context>
 <context>
@@ -25,17 +25,17 @@ icons here</source>
     <message>
         <location filename="../../../quicklaunchbutton.cpp" line="55"/>
         <source>Move left</source>
-        <translation>إلى اليسار</translation>
+        <translation>انقل لليسار</translation>
     </message>
     <message>
         <location filename="../../../quicklaunchbutton.cpp" line="58"/>
         <source>Move right</source>
-        <translation>إلى اليمين</translation>
+        <translation>انقل لليمين</translation>
     </message>
     <message>
         <location filename="../../../quicklaunchbutton.cpp" line="62"/>
         <source>Remove from quicklaunch</source>
-        <translation>إزالة من لوحة البدء السَّريع</translation>
+        <translation>أزل من المُطلق السّريع</translation>
     </message>
 </context>
 </TS>
