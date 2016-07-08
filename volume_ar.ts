21ef4a9846fb0038bef0bb2333007475afcab152
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- volume_ar.ts
+++ volume_ar.ts
@@ -1,37 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ar">
+<TS version="2.1" language="en_US">
 <context>
     <name>LXQtVolume</name>
     <message>
-        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="vanished">إظهار سطح المكتب: ﻻ يمكن تسجيل اختصار لوحة المفاتيح العامُّ `%1`</translation>
-    </message>
-    <message>
         <location filename="../../../lxqtvolume.cpp" line="62"/>
         <source>Increase sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>زِد مستوى الصّوت</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="68"/>
         <source>Decrease sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>أنقص مستوى الصّوت</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="74"/>
         <source>Mute/unmute sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>اكتم/أزل كتم الصّوت</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="135"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>التّحكّم بالصّوت: تعذّر تسجيل الاختصارات الآتية: %1</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="223"/>
         <location filename="../../../lxqtvolume.cpp" line="233"/>
         <source>Volume: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>المستوى: %1</translation>
     </message>
 </context>
 <context>
@@ -39,57 +35,57 @@
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>إعدادات التّحكّم بالصّوت</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation type="unfinished"></translation>
+        <translation>الجهاز للتّحكّم به</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="28"/>
         <source>Alsa</source>
-        <translation type="unfinished"></translation>
+        <translation>Alsa</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="35"/>
         <source>PulseAudio</source>
-        <translation type="unfinished"></translation>
+        <translation>PulseAudio</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="42"/>
         <source>OSS</source>
-        <translation type="unfinished"></translation>
+        <translation>OSS</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation type="unfinished"></translation>
+        <translation>السّلوك</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation type="unfinished"></translation>
+        <translation>اكتم بالنّقر بالوسط</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation type="unfinished"></translation>
+        <translation>أظهر بالنّقر بالوسط</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation type="unfinished"></translation>
+        <translation>اسمح بارتفاع المستوى عن 100% (0ديسيبل)</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="92"/>
         <source>Volume adjust step</source>
-        <translation type="unfinished"></translation>
+        <translation>عتبة ضبط المستوى</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="111"/>
         <source>External Mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>المازج الخارجيّ</translation>
     </message>
 </context>
 <context>
@@ -97,12 +93,12 @@
     <message>
         <location filename="../../../volumepopup.cpp" line="53"/>
         <source>Launch mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>أطلق المازج</translation>
     </message>
     <message>
         <location filename="../../../volumepopup.cpp" line="54"/>
         <source>Mi&amp;xer</source>
-        <translation type="unfinished"></translation>
+        <translation>الما&amp;زج</translation>
     </message>
 </context>
 </TS>
