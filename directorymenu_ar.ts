887a44892e56e719801cef0d7599e8c46910de17
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

---
+++ directorymenu_ar.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DirectoryMenu</name>
+    <message>
+        <location filename="../../../directorymenu.cpp" line="146"/>
+        <source>Open</source>
+        <translation>افتح</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryMenuConfiguration</name>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="14"/>
+        <source>Directory Menu Settings</source>
+        <translation>إعدادات قائمة الأدلّة</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>المظهر</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="26"/>
+        <source>Base directory:</source>
+        <translation>الدّليل الأساس:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="39"/>
+        <source>BaseDirectoryName</source>
+        <translation>اسم الدّليل الأساس</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="46"/>
+        <source>Icon:</source>
+        <translation>الأيقونة:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="107"/>
+        <source>Choose Base Directory</source>
+        <translation>اختر الدّليل الأساس</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="122"/>
+        <source>Choose Icon</source>
+        <translation>اختر أيقونة</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="123"/>
+        <source>Icons (*.png *.xpm *.jpg)</source>
+        <translation>صور (‎*.png *.xpm *.jpg)</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>Directory Menu</source>
+        <translation>قائمة الأدلّة</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>An error occurred while loading the icon.</source>
+        <translation>حدث خطأ أثناء تحميل الأيقونة.</translation>
+    </message>
+</context>
+</TS>
