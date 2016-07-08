21f627c1335064b08a551d4f5d27a7b470310785
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

---
+++ spacer_ar.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>إعدادات المُباعد</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>عرض المُباعد:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>نوع المُباعد:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation>خطّيّ</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation>منقّط</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation>مخفيّ</translation>
+    </message>
+</context>
+</TS>
