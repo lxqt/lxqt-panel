88a8e8b311eba5783a79abe902bee394cf70edbc
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- mount_ar.ts
+++ mount_ar.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">مدير الوسائط واﻷقراص القابلة للفصل</translation>
+        <translation>مدير الأجهزة/الوسائط المنفصلة</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>إعدادات الوسيط المنفصل</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">السلوك</translation>
+        <translation>السّلوك</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>عند وصل جهاز:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">القائمة اﻵنيَّة</translation>
+        <translation>أبثق قائمة</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">إظهار المعلومات</translation>
+        <translation>أظهر معلوماته</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">عدم القيام بأيِّ شيء</translation>
+        <translation>لا تفعل شيئًا</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,36 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/b&gt; موصول.</translation>
+        <translation>وُصل الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/b&gt; مفصول.</translation>
+        <translation>أُزيل الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">مدير الوسائط واﻷقراص القابلة للفصل</translation>
-    </message>
-</context>
-<context>
-    <name>LXQtMountConfiguration</name>
-    <message>
-        <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">إعدادات مدير ريزر للوسائط القابلة للفصل</translation>
-    </message>
-    <message>
-        <source>Behaviour</source>
-        <translation type="vanished">السلوك</translation>
-    </message>
-    <message>
-        <source>When a device is connected </source>
-        <translation type="vanished">عند وصل الجهاز</translation>
-    </message>
-    <message>
-        <source>Popup menu</source>
-        <translation type="vanished">القائمة اﻵنيَّة</translation>
-    </message>
-    <message>
-        <source>Show info</source>
-        <translation type="vanished">إظهار المعلومات</translation>
-    </message>
-    <message>
-        <source>Do nothing</source>
-        <translation type="vanished">عدم القيام بأيِّ شيء</translation>
+        <translation>مدير الأجهزة/الوسائط المنفصلة</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
-        <source>Click to access this device from other applications.</source>
-        <translation type="vanished">اضغط للوصول إلى هذا الجهاز من تطبيقاتٍ أخرى.</translation>
-    </message>
-    <message>
-        <source>Click to eject this disc.</source>
-        <translation type="vanished">اضغط ﻹخراج هذا القرص.</translation>
-    </message>
-    <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>فشل ضمّ &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;:‏ %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">مدير الوسائط واﻷقراص القابلة للفصل</translation>
+        <translation>مدير الأجهزة/الوسائط المنفصلة</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MountButton</name>
-    <message>
-        <source>Removable media/devices manager</source>
-        <translation type="vanished">مدير الوسائط واﻷقراص القابلة للفصل</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/b&gt; موصول.</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">الجهاز &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/b&gt; مفصول.</translation>
-    </message>
-    <message>
-        <source>No devices Available.</source>
-        <translation type="vanished">ﻻ توجد أجهزة.</translation>
+        <translation>فشلت إزالة ضمّ &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt;:‏ %2</translation>
     </message>
 </context>
 <context>
@@ -138,7 +84,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>لا أجهزة متوفّرة</translation>
     </message>
 </context>
 </TS>
