61d7c6f40e5ad4ac09c7228e534d45c8bed748c0
Safa Alfulaij <safa1996alfulaij@gmail.com>
2016-07-26 02:22:09 +0300

    Update Arabic translation of dom Plugin (#32)
    * Update Arabic translation of dom Plugin
    
    * Fix Language Header of Arabic Translation of dom Plugin

--- dom_ar.ts
+++ dom_ar.ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ar">
 <context>
     <name>TreeWindow</name>
     <message>
         <location filename="../../../treewindow.ui" line="14"/>
         <source>Panel DOM tree</source>
-        <translation type="unfinished"></translation>
+        <translation>شجرة DOM للّوحة</translation>
     </message>
     <message>
         <location filename="../../../treewindow.ui" line="63"/>
         <location filename="../../../treewindow.ui" line="96"/>
         <source>Property</source>
-        <translation type="unfinished"></translation>
+        <translation>الخاصيّة</translation>
     </message>
     <message>
         <location filename="../../../treewindow.ui" line="68"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>القيمة</translation>
     </message>
     <message>
         <location filename="../../../treewindow.ui" line="76"/>
         <source>All properties</source>
-        <translation type="unfinished"></translation>
+        <translation>كلّ الصّفات</translation>
     </message>
     <message>
         <location filename="../../../treewindow.ui" line="101"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>النّوع</translation>
     </message>
     <message>
         <location filename="../../../treewindow.ui" line="106"/>
         <source>String value</source>
-        <translation type="unfinished"></translation>
+        <translation>قيمة السّلسلة</translation>
     </message>
 </context>
 </TS>
