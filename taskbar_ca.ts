78940090d0d78781b2d2a18b355dfae0ea2d61b4
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:22:01 +0200

    lxqt-panel: Add/update Catalan translations
        Add clock_ca.ts (Catalan)
        Add cpuload_ca.ts (Catalan)
        Add desktopswitch_ca.ts (Catalan)
        Add directorymenu_ca.ts (Catalan)
        Add dom_ca.ts (Catalan)
        Add kbindicator_ca.ts (Catalan)
        Add mainmenu_ca.ts (Catalan)
        Add mount_ca.ts (Catalan)
        Add networkmonitor_ca.ts (Catalan)
        Add quicklaunch_ca.ts (Catalan)
        Translate sensors_ca.ts (Catalan)
        Add showdesktop_ca.ts (Catalan)
        Add spacer_ca.ts (Catalan)
        Add sysstat_ca.ts (Catalan)
        Add taskbar_ca.ts (Catalan)
        Add volume_ca.ts (Catalan)
        Add worldclock_ca.ts (Catalan)

---
+++ taskbar_ca.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>LXQtTaskButton</name>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="477"/>
+        <source>Application</source>
+        <translation>Aplicació</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="511"/>
+        <source>To &amp;Desktop</source>
+        <translation>A l&apos;&amp;escriptori</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="513"/>
+        <source>&amp;All Desktops</source>
+        <translation>T&amp;ots els escriptoris</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="521"/>
+        <source>Desktop &amp;%1</source>
+        <translation>Escriptori &amp;%1</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="528"/>
+        <source>&amp;To Current Desktop</source>
+        <translation>A l&apos;escriptori ac&amp;tual</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="537"/>
+        <source>Ma&amp;ximize</source>
+        <translation>Ma&amp;ximitza</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="544"/>
+        <source>Maximize vertically</source>
+        <translation>Maximitza verticalment</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="549"/>
+        <source>Maximize horizontally</source>
+        <translation>Maximitza horitzontalment</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="555"/>
+        <source>&amp;Restore</source>
+        <translation>&amp;Restaura</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="559"/>
+        <source>Mi&amp;nimize</source>
+        <translation>Mi&amp;nimitza</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="565"/>
+        <source>Roll down</source>
+        <translation>Enrotlla cap a baix</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="571"/>
+        <source>Roll up</source>
+        <translation>Enrotlla cap a dalt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="579"/>
+        <source>&amp;Layer</source>
+        <translation>&amp;Capa</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="581"/>
+        <source>Always on &amp;top</source>
+        <translation>Sempre a la par&amp;t superior</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="587"/>
+        <source>&amp;Normal</source>
+        <translation>&amp;Normal</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="593"/>
+        <source>Always on &amp;bottom</source>
+        <translation>Sempre a la part in&amp;ferior</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="601"/>
+        <source>&amp;Close</source>
+        <translation>Tan&amp;ca</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskGroup</name>
+    <message>
+        <location filename="../../../lxqttaskgroup.cpp" line="83"/>
+        <source>Group</source>
+        <translation>Grup</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskgroup.cpp" line="85"/>
+        <source>Close group</source>
+        <translation>Tanca el grup</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskbarConfiguration</name>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="14"/>
+        <source>Task Manager Settings</source>
+        <translation>Ajusts del gestor de tasques</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="34"/>
+        <source>Show only windows from desktop</source>
+        <translation>Mostra únicament les finestres de l&apos;escriptori</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="46"/>
+        <source>Show only windows from &amp;panel&apos;s screen</source>
+        <translation>Mostra únicament les finestres de la pantalla del &amp;plafó</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="53"/>
+        <source>Show only minimized windows</source>
+        <translation>Mostra únicament les finestres minimitzades</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="60"/>
+        <source>Raise minimized windows on current desktop</source>
+        <translation>Eleva les finestres minimitzades en l&apos;escriptori actual</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="67"/>
+        <source>Close on middle-click</source>
+        <translation>Tanca amb el clic del botó del mig</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="77"/>
+        <source>Window &amp;grouping</source>
+        <translation>A&amp;grupació de les finestres</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="86"/>
+        <source>Show popup on mouse hover </source>
+        <translation>Mostra el menú emergent en passar per sobre el ratolí</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="96"/>
+        <source>Appearance</source>
+        <translation>Aparença</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="105"/>
+        <source>Button style</source>
+        <translation>Estil del botó</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="115"/>
+        <source>Maximum button width</source>
+        <translation>Amplada màxima del botó</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="128"/>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="154"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="141"/>
+        <source>Maximum button height</source>
+        <translation>Alçada màxima del botó</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="167"/>
+        <source>Auto&amp;rotate buttons when the panel is vertical</source>
+        <translation>Gi&amp;ra automàticament els botons quan el plafó sigui vertical</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="174"/>
+        <source>Use icons by WindowClass, if available</source>
+        <translation>Utilitza les icones per WindowClass, si estigués disponible</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="45"/>
+        <source>Icon and text</source>
+        <translation>Icona i text</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="46"/>
+        <source>Only icon</source>
+        <translation>Només la icona</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="47"/>
+        <source>Only text</source>
+        <translation>Només el text</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="49"/>
+        <source>Current</source>
+        <translation>Actual</translation>
+    </message>
+</context>
+</TS>
