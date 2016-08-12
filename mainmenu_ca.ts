9dcc6c016deccb8e81b71de6a2163cdcd5784882
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:22:01 +0200

    lxqt-panel: Add/update Catalan translations
        Add clock_ca.ts (Catalan)
        Add cpuload_ca.ts (Catalan)
        Add desktopswitch_ca.ts (Catalan)
        Add directorymenu_ca.ts (Catalan)
        Add dom_ca.ts (Catalan)
        Add kbindicator_ca.ts (Catalan)
        Add mainmenu_ca.ts (Catalan)
        Add mount_ca.ts (Catalan)
        Add networkmonitor_ca.ts (Catalan)
        Add quicklaunch_ca.ts (Catalan)
        Translate sensors_ca.ts (Catalan)
        Add showdesktop_ca.ts (Catalan)
        Add spacer_ca.ts (Catalan)
        Add sysstat_ca.ts (Catalan)
        Add taskbar_ca.ts (Catalan)
        Add volume_ca.ts (Catalan)
        Add worldclock_ca.ts (Catalan)

---
+++ mainmenu_ca.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>LXQtMainMenu</name>
+    <message>
+        <location filename="../../../lxqtmainmenu.cpp" line="95"/>
+        <source>Search...</source>
+        <translation>Cerca...</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenu.cpp" line="101"/>
+        <source>Show/hide main menu</source>
+        <translation>Mostra o oculta el menú principal</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtMainMenuConfiguration</name>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="14"/>
+        <source>Main Menu settings</source>
+        <translation>Ajusts del menú principal</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="35"/>
+        <source>Icon:</source>
+        <translation>Icona:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="59"/>
+        <source>Button text:</source>
+        <translation>Text del botó:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="76"/>
+        <source>Custom font size:</source>
+        <translation>Mida personalitzada de la lletra:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="86"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="105"/>
+        <source>Menu file</source>
+        <translation>Fitxer del menú</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="111"/>
+        <source>Menu file:</source>
+        <translation>Fitxer del menú:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="169"/>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="175"/>
+        <source>max. item width:</source>
+        <translation>Amplada màxima de l&apos;ítem:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="182"/>
+        <source>Show matching entries:</source>
+        <translation>Mostra les entrades coincidents:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="196"/>
+        <source>Maximum visible items:</source>
+        <translation>Nombre màxim d&apos;ítems visibles:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="203"/>
+        <source>Filter menu entries</source>
+        <translation>Filtra les entrades del menú</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="210"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="223"/>
+        <source>Hide menu entries while searching</source>
+        <translation>Oculta les entrades del menú mentre se cerca</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="127"/>
+        <source>Keyboard Shortcut</source>
+        <translation>Drecera de teclat</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="146"/>
+        <source>Click the button to record shortcut:</source>
+        <translation>Feu clic al botó per enregistrar la drecera:</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="71"/>
+        <source>Reset</source>
+        <translation>Restableix</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="156"/>
+        <source>Choose icon file</source>
+        <translation>Trieu el fitxer de la icona</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="158"/>
+        <source>Images (*.svg *.png)</source>
+        <translation>Imatges (*.svg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="170"/>
+        <source>Choose menu file</source>
+        <translation>Triel el fitxer del menú</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="172"/>
+        <source>Menu files (*.menu)</source>
+        <translation>Fitxers de menús (*.menu)</translation>
+    </message>
+</context>
+</TS>
