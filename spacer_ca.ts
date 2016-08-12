0b703c15097a1d0aa21019e79f47f491665ebc2c
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:22:01 +0200

    lxqt-panel: Add/update Catalan translations
        Add clock_ca.ts (Catalan)
        Add cpuload_ca.ts (Catalan)
        Add desktopswitch_ca.ts (Catalan)
        Add directorymenu_ca.ts (Catalan)
        Add dom_ca.ts (Catalan)
        Add kbindicator_ca.ts (Catalan)
        Add mainmenu_ca.ts (Catalan)
        Add mount_ca.ts (Catalan)
        Add networkmonitor_ca.ts (Catalan)
        Add quicklaunch_ca.ts (Catalan)
        Translate sensors_ca.ts (Catalan)
        Add showdesktop_ca.ts (Catalan)
        Add spacer_ca.ts (Catalan)
        Add sysstat_ca.ts (Catalan)
        Add taskbar_ca.ts (Catalan)
        Add volume_ca.ts (Catalan)
        Add worldclock_ca.ts (Catalan)

---
+++ spacer_ca.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>Ajusts de l&apos;espaiador</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>Amplada de l&apos;espai:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>Tipus d&apos;espai:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation>folrat</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation>puntejat</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation>invisible</translation>
+    </message>
+</context>
+</TS>
