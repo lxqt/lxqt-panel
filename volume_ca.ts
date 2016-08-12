acbad4194826867d857feb8c8f295add8dbfe03e
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:22:01 +0200

    lxqt-panel: Add/update Catalan translations
        Add clock_ca.ts (Catalan)
        Add cpuload_ca.ts (Catalan)
        Add desktopswitch_ca.ts (Catalan)
        Add directorymenu_ca.ts (Catalan)
        Add dom_ca.ts (Catalan)
        Add kbindicator_ca.ts (Catalan)
        Add mainmenu_ca.ts (Catalan)
        Add mount_ca.ts (Catalan)
        Add networkmonitor_ca.ts (Catalan)
        Add quicklaunch_ca.ts (Catalan)
        Translate sensors_ca.ts (Catalan)
        Add showdesktop_ca.ts (Catalan)
        Add spacer_ca.ts (Catalan)
        Add sysstat_ca.ts (Catalan)
        Add taskbar_ca.ts (Catalan)
        Add volume_ca.ts (Catalan)
        Add worldclock_ca.ts (Catalan)

---
+++ volume_ca.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>LXQtVolume</name>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="62"/>
+        <source>Increase sound volume</source>
+        <translation>Apuja el volum del so</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="68"/>
+        <source>Decrease sound volume</source>
+        <translation>Abaixa el volum del so</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="74"/>
+        <source>Mute/unmute sound volume</source>
+        <translation>Silencia o dessilencia el volum del so</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="135"/>
+        <source>Volume Control: The following shortcuts can not be registered: %1</source>
+        <translation>Control del volum: Les següents dreceres no es poden registrar: %1</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="223"/>
+        <location filename="../../../lxqtvolume.cpp" line="233"/>
+        <source>Volume: %1</source>
+        <translation>Volum: %1</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtVolumeConfiguration</name>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="14"/>
+        <source>Volume Control Settings</source>
+        <translation>Ajusts del control del volum</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="20"/>
+        <source>Device to control</source>
+        <translation>Dispositiu a controlar</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="28"/>
+        <source>Alsa</source>
+        <translation>Alsa</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="35"/>
+        <source>PulseAudio</source>
+        <translation>PulseAudio</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="42"/>
+        <source>OSS</source>
+        <translation>OSS</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="57"/>
+        <source>Behavior</source>
+        <translation>Comportament</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="63"/>
+        <source>Mute on middle click</source>
+        <translation>Silencia amb el clic del mig</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="70"/>
+        <source>Show on mouse click</source>
+        <translation>Mostra amb el clic del ratolí</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="77"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation>Permet que el volum superi el 100% (0 dB)</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="92"/>
+        <source>Volume adjust step</source>
+        <translation>Pas d&apos;ajust del volum</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="111"/>
+        <source>External Mixer</source>
+        <translation>Mesclador extern</translation>
+    </message>
+</context>
+<context>
+    <name>VolumePopup</name>
+    <message>
+        <location filename="../../../volumepopup.cpp" line="53"/>
+        <source>Launch mixer</source>
+        <translation>Llança el mesclador</translation>
+    </message>
+    <message>
+        <location filename="../../../volumepopup.cpp" line="54"/>
+        <source>Mi&amp;xer</source>
+        <translation>Mes&amp;clador</translation>
+    </message>
+</context>
+</TS>
