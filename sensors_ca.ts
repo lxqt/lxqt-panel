cd44a21dfffffc44d927e6fd82044a1abff6c06a
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:22:01 +0200

    lxqt-panel: Add/update Catalan translations
        Add clock_ca.ts (Catalan)
        Add cpuload_ca.ts (Catalan)
        Add desktopswitch_ca.ts (Catalan)
        Add directorymenu_ca.ts (Catalan)
        Add dom_ca.ts (Catalan)
        Add kbindicator_ca.ts (Catalan)
        Add mainmenu_ca.ts (Catalan)
        Add mount_ca.ts (Catalan)
        Add networkmonitor_ca.ts (Catalan)
        Add quicklaunch_ca.ts (Catalan)
        Translate sensors_ca.ts (Catalan)
        Add showdesktop_ca.ts (Catalan)
        Add spacer_ca.ts (Catalan)
        Add sysstat_ca.ts (Catalan)
        Add taskbar_ca.ts (Catalan)
        Add volume_ca.ts (Catalan)
        Add worldclock_ca.ts (Catalan)

--- sensors_ca.ts
+++ sensors_ca.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="14"/>
         <source>Sensors Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajusts dels sensors</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="30"/>
         <source>Common</source>
-        <translation type="unfinished"></translation>
+        <translation>Comú</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="42"/>
         <source>Update interval (seconds)</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval d&apos;actualització (segons)</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="49"/>
         <source>Temperature bar width</source>
-        <translation type="unfinished"></translation>
+        <translation>Amplada de la barra de la temperatura</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="104"/>
         <source>Temperature scale</source>
-        <translation type="unfinished"></translation>
+        <translation>Escala de la temperatura</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="110"/>
         <source>Celsius</source>
-        <translation type="unfinished"></translation>
+        <translation>Celsius</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="120"/>
         <source>Fahrenheit</source>
-        <translation type="unfinished"></translation>
+        <translation>Fahrenheit</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink progress bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>Parpelleja les barres de progrés quan la temperatura sigui massa alta</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="138"/>
         <source>Warning about high temperature</source>
-        <translation type="unfinished"></translation>
+        <translation>Advertència quant a temperatures elevades</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="152"/>
@@ -56,27 +56,27 @@
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="168"/>
         <source>Detected chips:</source>
-        <translation type="unfinished"></translation>
+        <translation>Xips detectats:</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.ui" line="175"/>
         <source>Chip features:</source>
-        <translation type="unfinished"></translation>
+        <translation>Característiques del xip:</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitat</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Label</source>
-        <translation type="unfinished"></translation>
+        <translation>Etiqueta</translation>
     </message>
     <message>
         <location filename="../../../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Color</translation>
     </message>
 </context>
 </TS>
