1d1f0a11d39d63972fdbb390b31d8a73b665fa08
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ kbindicator_cs.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../../../src/content.cpp" line="43"/>
+        <source>C</source>
+        <comment>Label for CapsLock indicator</comment>
+        <translation>C</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="46"/>
+        <source>CapsLock</source>
+        <comment>Tooltip for CapsLock indicator</comment>
+        <translation>Psaní velkými písmeny (CapsLock)</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="50"/>
+        <source>N</source>
+        <comment>Label for NumLock indicator</comment>
+        <translation>N</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="52"/>
+        <source>NumLock</source>
+        <comment>Tooltip for NumLock indicator</comment>
+        <translation>Zamknutí číselné klávesnice (NumLock)</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="57"/>
+        <source>S</source>
+        <comment>Label for ScrollLock indicator</comment>
+        <translation>S</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="59"/>
+        <source>ScrollLock</source>
+        <comment>Tooltip for ScrollLock indicator</comment>
+        <translation>Zamknutí posunování obrazu (ScrollLock)</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Layout</source>
+        <translation>Rozvržení</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Variant</source>
+        <translation>Varianta</translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="14"/>
+        <source>Keyboard state settings</source>
+        <translation>Nastavení stavu klávesnice</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="20"/>
+        <source>Lock Indicators</source>
+        <translation>Ukázat přepínače</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation>Ukázat klávesu CapsLock</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation>Ukázat klávesu NumLock</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation>Ukázat klávesu ScrollLock</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="50"/>
+        <source>Keyboard Layout Indicator</source>
+        <translation>Ukázat rozvržení klávesnice</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="62"/>
+        <source>Switching policy</source>
+        <translation>Předpis pro přepínání</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="102"/>
+        <source>Configure layouts</source>
+        <translation>Nastavit rozvržení</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="69"/>
+        <source>Global</source>
+        <translation>Celkové</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="79"/>
+        <source>Window</source>
+        <translation>Okno</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="89"/>
+        <source>Application</source>
+        <translation>Program</translation>
+    </message>
+</context>
+</TS>
