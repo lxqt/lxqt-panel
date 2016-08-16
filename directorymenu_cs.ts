9a59e3dd3e89c68177b7e8f266405672e27ea18d
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ directorymenu_cs.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>DirectoryMenu</name>
+    <message>
+        <location filename="../../../directorymenu.cpp" line="146"/>
+        <source>Open</source>
+        <translation>Otevřít</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryMenuConfiguration</name>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="14"/>
+        <source>Directory Menu Settings</source>
+        <translation>Nastavení nabídky k adresáři</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Vzhled</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="26"/>
+        <source>Base directory:</source>
+        <translation>Základní adresář:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="39"/>
+        <source>BaseDirectoryName</source>
+        <translation>Název základního adresáře</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="46"/>
+        <source>Icon:</source>
+        <translation>Ikona:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="107"/>
+        <source>Choose Base Directory</source>
+        <translation>Vybrat základní adresář</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="122"/>
+        <source>Choose Icon</source>
+        <translation>Vybrat ikonu</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="123"/>
+        <source>Icons (*.png *.xpm *.jpg)</source>
+        <translation>Ikony (*.png *.xpm *.jpg)</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>Directory Menu</source>
+        <translation>Nabídka k adresáři</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>An error occurred while loading the icon.</source>
+        <translation>Při nahrávání ikony se vyskytla chyba.</translation>
+    </message>
+</context>
+</TS>
