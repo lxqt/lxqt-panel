d21d33b620ee34a6a15e71ad69bb7d042242367d
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

--- mount_cs.ts
+++ mount_cs.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Správce odstranitelných nosičů/zařízení</translation>
+        <translation>Správce odstranitelných nosičů/zařízení</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavení odstranitelných nosičů</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Chování</translation>
+        <translation>Chování</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Když je zařízení připojeno:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Ukázat vyskakovací nabídku</translation>
+        <translation>Ukázat vyskakovací nabídku</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Ukázat informace</translation>
+        <translation>Ukázat informace</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Nedělat nic</translation>
+        <translation>Nedělat nic</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,90 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je zapojeno.</translation>
+        <translation>Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je připojeno.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je odstraněno.</translation>
+        <translation>Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je odstraněno.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Správce odstranitelných nosičů/zařízení</translation>
+        <translation>Správce odstranitelných nosičů/zařízení</translation>
     </message>
 </context>
 <context>
     <name>LXQtMountConfiguration</name>
     <message>
         <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">Nastavení správce odstranitelných zařízení</translation>
+        <translation>Nastavení správce odstranitelných zařízení</translation>
     </message>
     <message>
         <source>Behaviour</source>
-        <translation type="vanished">Chování</translation>
+        <translation>Chování</translation>
     </message>
     <message>
         <source>When a device is connected </source>
-        <translation type="vanished">Když je zařízení zapojeno</translation>
+        <translation>Když je zařízení zapojeno</translation>
     </message>
     <message>
         <source>Popup menu</source>
-        <translation type="vanished">Ukázat vyskakovací nabídku</translation>
+        <translation>Ukázat vyskakovací nabídku</translation>
     </message>
     <message>
         <source>Show info</source>
-        <translation type="vanished">Ukázat informace</translation>
+        <translation>Ukázat informace</translation>
     </message>
     <message>
         <source>Do nothing</source>
-        <translation type="vanished">Nedělat nic</translation>
+        <translation>Nedělat nic</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
         <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Klepněte pro přistoupení k tomuto zařízení z jiných aplikací.</translation>
+        <translation>Klepněte pro přistoupení k tomuto zařízení z jiných aplikací.</translation>
     </message>
     <message>
         <source>Click to eject this disc.</source>
-        <translation type="vanished">Klepněte pro vysunutí tohoto disku.</translation>
+        <translation>Klepněte pro vysunutí tohoto disku.</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Připojení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; se nezdařilo: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Správce odstranitelných nosičů/zařízení</translation>
+        <translation>Správce odstranitelných nosičů/zařízení</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Odpojení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; se nezdařilo: %2</translation>
     </message>
 </context>
 <context>
     <name>MountButton</name>
     <message>
         <source>Removable media/devices manager</source>
-        <translation type="vanished">Správce odstranitelných nosičů/zařízení</translation>
+        <translation>Správce odstranitelných nosičů/zařízení</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je zapojeno.</translation>
+        <translation>Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je zapojeno.</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je odstraněno.</translation>
+        <translation>Zařízení &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; je odstraněno.</translation>
     </message>
     <message>
         <source>No devices Available.</source>
-        <translation type="vanished">Nejsou dostupná žádná zařízení.</translation>
+        <translation>Nejsou dostupná žádná zařízení.</translation>
     </message>
 </context>
 <context>
@@ -138,7 +138,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Nejsou dostupná žádná zařízení</translation>
     </message>
 </context>
 </TS>
