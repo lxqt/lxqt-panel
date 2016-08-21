aa956c05be7cba0f26725d5753076a48832054e8
Dimitrios Glentadakis <dglent@free.fr>
2016-08-21 10:55:22 +0200

    Update Greek translation

--- mount_el.ts
+++ mount_el.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Διαχειριστής αφαιρούμενων μέσων/συσκευών</translation>
+        <translation>Διαχειριστής αφαιρούμενων μέσων/συσκευών</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished">Ρυθμίσεις αφαιρούμενων μέσων</translation>
+        <translation>Ρυθμίσεις αφαιρούμενων μέσων</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Συμπεριφορά</translation>
+        <translation>Συμπεριφορά</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Όταν μια συσκευή συνδέεται:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Αναδυόμενο μενού</translation>
+        <translation>Αναδυόμενο μενού</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Εμφάνιση πληροφοριών</translation>
+        <translation>Εμφάνιση πληροφοριών</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Καμία ενέργεια</translation>
+        <translation>Καμία ενέργεια</translation>
     </message>
 </context>
 <context>
@@ -64,47 +64,47 @@
     <name>LXQtMountConfiguration</name>
     <message>
         <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">Ρυθμίσεις διαχειριστή αφαιρούμενων μέσων LXQt</translation>
+        <translation>Ρυθμίσεις του διαχειριστή των αφαιρούμενων μέσων του LXQt</translation>
     </message>
     <message>
         <source>Removable Media Settings</source>
-        <translation type="vanished">Ρυθμίσεις αφαιρούμενων μέσων</translation>
+        <translation>Ρυθμίσεις των αφαιρούμενων μέσων</translation>
     </message>
     <message>
         <source>Behaviour</source>
-        <translation type="vanished">Συμπεριφορά</translation>
+        <translation>Συμπεριφορά</translation>
     </message>
     <message>
         <source>When a device is connected </source>
-        <translation type="vanished">Όταν μια συσκευή συνδέεται </translation>
+        <translation>Όταν μια συσκευή συνδέεται </translation>
     </message>
     <message>
         <source>Popup menu</source>
-        <translation type="vanished">Αναδυόμενο μενού</translation>
+        <translation>Αναδυόμενο μενού</translation>
     </message>
     <message>
         <source>Show info</source>
-        <translation type="vanished">Εμφάνιση πληροφοριών</translation>
+        <translation>Εμφάνιση πληροφοριών</translation>
     </message>
     <message>
         <source>Do nothing</source>
-        <translation type="vanished">Καμία ενέργεια</translation>
+        <translation>Καμία ενέργεια</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
         <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Κλίκ για πρόσβαση σε αυτή τη συσκευή από άλλες εφαρμογές.</translation>
+        <translation>Κάντε κλικ για προσπέλαση της συσκευής από άλλες εφαρμογές.</translation>
     </message>
     <message>
         <source>Click to eject this disc.</source>
-        <translation type="vanished">Κλίκ για εξαγωγή αυτού του δίσκου.</translation>
+        <translation>Κάντε κλικ για την εξαγωγή του δίσκου.</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Η προσάρτηση του &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt; απέτυχε: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
@@ -114,7 +114,7 @@
     </message>
     <message>
         <source>Mounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="vanished">Η προσάρτηση του &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; απέτυχε: %2</translation>
+        <translation>Η προσάρτηση του &lt;strong&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/strong&gt; απέτυχε: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
@@ -126,19 +126,19 @@
     <name>MountButton</name>
     <message>
         <source>Removable media/devices manager</source>
-        <translation type="vanished">Διαχειριστής αφαιρούμενων μέσων/συσκευών</translation>
+        <translation>Διαχειριστής των αφαιρούμενων μέσων/συσκευών</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">Συνδέθηκε η συσκευή &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;.</translation>
+        <translation>Συνδέθηκε η συσκευή &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Αφαιρέθηκε η συσκευή &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;.</translation>
+        <translation>Αφαιρέθηκε η συσκευή &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <source>No devices Available.</source>
-        <translation type="vanished">Καμία διαθέσιμη συσκευή.</translation>
+        <translation>Καμία διαθέσιμη συσκευή.</translation>
     </message>
 </context>
 <context>
