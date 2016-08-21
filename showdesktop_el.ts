45246cc03bd3b374af2083acd6ee70ccd218db77
Dimitrios Glentadakis <dglent@free.fr>
2016-08-21 10:55:22 +0200

    Update Greek translation

--- showdesktop_el.ts
+++ showdesktop_el.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../../../showdesktop.cpp" line="48"/>
         <source>Show desktop</source>
-        <translation>Εμφάνιση επιφάνειας εργασίας</translation>
+        <translation>Εμφάνιση της επιφάνειας εργασίας</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="70"/>
         <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation>Εμφάνιση επιφάνειας εργασίας: Δεν είναι δυνατή η καταχώριση της καθολικής συντόμευσης &quot;%1&quot;</translation>
+        <translation>Εμφάνιση της επιφάνειας εργασίας: Δεν είναι δυνατή η καταχώριση της καθολικής συντόμευσης «%1»</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="55"/>
         <source>Show Desktop</source>
-        <translation>Εμφάνιση επιφάνειας εργασίας</translation>
+        <translation>Εμφάνιση της επιφάνειας εργασίας</translation>
     </message>
 </context>
 </TS>
