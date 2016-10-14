5331752f1ce64758543f1d0a888edeedd9fec4fb
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-14 18:37:33 +0200

    Update desktopswitch_nl.ts
    Complete Dutch translation

--- desktopswitch_nl.ts
+++ desktopswitch_nl.ts
@@ -6,13 +6,13 @@
     <message>
         <location filename="../../../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Schakel over naar werkblad %1</translation>
     </message>
     <message>
         <location filename="../../../desktopswitch.cpp" line="141"/>
         <location filename="../../../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
-        <translation>Bureaublad %1</translation>
+        <translation>Werkblad %1</translation>
     </message>
 </context>
 <context>
@@ -20,37 +20,37 @@
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen voor Werkbladwisselaar</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aantal rijen:</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkbladetiketten:</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Uiterlijk</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Getallen</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
-        <translation type="unfinished"></translation>
+        <translation>Namen</translation>
     </message>
     <message>
         <location filename="../../../desktopswitchconfiguration.ui" line="70"/>
         <source>Desktop names</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkbladnamen</translation>
     </message>
 </context>
 </TS>
