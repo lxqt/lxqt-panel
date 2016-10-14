143487319de2ee6a92c46eef0f00caad2a7bab9d
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-14 20:03:30 +0200

    Update showdesktop_nl.ts
    Minor update for Dutch translation

--- showdesktop_nl.ts
+++ showdesktop_nl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../showdesktop.cpp" line="48"/>
         <source>Show desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Toon bureaublad</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="70"/>
