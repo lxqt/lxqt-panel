7d5db9d521353962dbb25c530998658142e2e8fa
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-14 19:25:08 +0200

    Create kbindicator_nl.ts
    Complete Dutch translation

---
+++ kbindicator_nl.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../../../src/content.cpp" line="43"/>
+        <source>C</source>
+        <comment>Label for CapsLock indicator</comment>
+        <translatorcomment>This capital letter is printed on my keyboard.</translatorcomment>
+        <translation>A</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="46"/>
+        <source>CapsLock</source>
+        <comment>Etiket voor CapsLock-indicator</comment>
+        <translation>A</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="50"/>
+        <source>N</source>
+        <comment>Label for NumLock indicator</comment>
+        <translatorcomment>This digit is printed on my keyboard.</translatorcomment>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="52"/>
+        <source>NumLock</source>
+        <comment>Etiket voor NumLock-indicator</comment>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="57"/>
+        <source>S</source>
+        <comment>Label for ScrollLock indicator</comment>
+        <translatorcomment>From the word &quot;Rollen&quot;.</translatorcomment>
+        <translation>R</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="59"/>
+        <source>ScrollLock</source>
+        <comment>Etiket voor ScrollLock-indicator</comment>
+        <translation>Schuiven</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Layout</source>
+        <translation>Indeling</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Variant</source>
+        <translation>Variant</translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="14"/>
+        <source>Keyboard state settings</source>
+        <translation>Toetsenbordstatus-instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="20"/>
+        <source>Lock Indicators</source>
+        <translation>Vergrendelingsindicatoren</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation>Toon Caps Lock-toets</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation>Toon Num Lock-toets</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation>Toon Scroll Lock-toets</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="50"/>
+        <source>Keyboard Layout Indicator</source>
+        <translation>Indicator voor toetsenbordindeling</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="62"/>
+        <source>Switching policy</source>
+        <translation>Omschakelingsbeleid</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="102"/>
+        <source>Configure layouts</source>
+        <translation>Indelingen instellen</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="69"/>
+        <source>Global</source>
+        <translation>Systeembreed</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="79"/>
+        <source>Window</source>
+        <translation>Venster</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="89"/>
+        <source>Application</source>
+        <translation>Toepassing</translation>
+    </message>
+</context>
+</TS>
