a25e60045f03fbfd399012c4e5e1744c0362de08
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-14 19:49:02 +0200

    Update mount_nl.ts
    Complete Dutch translation

--- mount_nl.ts
+++ mount_nl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Beheerder van verwijderbare media/apparaten</translation>
+        <translation>Beheerder van verwijderbare media/apparaten</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen van verwijderbare media</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Gedrag</translation>
+        <translation>Gedrag</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Wanneer een apparaat verbonden is:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Opduikmenu</translation>
+        <translation>Opduikmenu</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Toon informatie</translation>
+        <translation>Toon info</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Niets doen</translation>
+        <translation>Doe niets</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,36 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is verbonden.</translation>
+        <translation>Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is verbonden.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is verwijderd.</translation>
+        <translation>Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; werd verwijderd.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Beheerder van verwijderbare media/apparaten</translation>
-    </message>
-</context>
-<context>
-    <name>LXQtMountConfiguration</name>
-    <message>
-        <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">Instellingen voor beheerder van verwijderbare media in LXQt</translation>
-    </message>
-    <message>
-        <source>Behaviour</source>
-        <translation type="vanished">Gedrag</translation>
-    </message>
-    <message>
-        <source>When a device is connected </source>
-        <translation type="vanished">Wanneer een apparaat wordt verbonden </translation>
-    </message>
-    <message>
-        <source>Popup menu</source>
-        <translation type="vanished">Opduikmenu</translation>
-    </message>
-    <message>
-        <source>Show info</source>
-        <translation type="vanished">Toon informatie</translation>
-    </message>
-    <message>
-        <source>Do nothing</source>
-        <translation type="vanished">Niets doen</translation>
+        <translation>Beheerder van verwijderbare media/apparaten</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
-        <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Klik om dit apparaat te benaderen vanuit andere toepassingen.</translation>
-    </message>
-    <message>
-        <source>Click to eject this disc.</source>
-        <translation type="vanished">Klik om deze schijf uit te werpen.</translation>
-    </message>
-    <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Het aankoppelen van &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is mislukt: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Beheerder van verwijderbare media/apparaten</translation>
+        <translation>Beheerder van verwijderbare media/apparaten</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MountButton</name>
-    <message>
-        <source>Removable media/devices manager</source>
-        <translation type="vanished">Beheerder van verwijderbare media/apparaten</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is verbonden.</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Het apparaat &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is verwijderd.</translation>
-    </message>
-    <message>
-        <source>No devices Available.</source>
-        <translation type="vanished">Geen apparaten beschikbaar.</translation>
+        <translation>het ontkoppelen van &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; is mislukt: %2</translation>
     </message>
 </context>
 <context>
@@ -138,7 +84,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Er zijn geen apparaten beschikbaar</translation>
     </message>
 </context>
 </TS>
