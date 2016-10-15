b623970b096bb2bcc957dbc3998f1df57269a313
Sérgio Marques <smarquespt@gmail.com>
2016-10-15 15:30:45 +0100

    Portuguese update for lxqt-panel modules, pavucontrol-qt and pcmanfm

--- mount_pt.ts
+++ mount_pt.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Gestor de discos e unidades amovíveis</translation>
+        <translation>Gestor de discos e unidades amovíveis</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Definições de Discos amovíveis</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Comportamento</translation>
+        <translation>Comportamento</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Ao conectar um dispositivo:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Menu</translation>
+        <translation>Mostrar menu</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Mostrar informações</translation>
+        <translation>Mostrar informações</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Nada fazer</translation>
+        <translation>Nada fazer</translation>
     </message>
 </context>
 <context>
@@ -47,17 +47,17 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">O dispositivo &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; está conectado.</translation>
+        <translation>O dispositivo &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; está conectado.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">O dispositivo &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; foi removido.</translation>
+        <translation>O dispositivo &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; foi removido.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Gestor de discos e unidades amovíveis</translation>
+        <translation>Gestor de discos e unidades amovíveis</translation>
     </message>
 </context>
 <context>
@@ -100,18 +100,18 @@
     <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro ao montar &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Gestor de discos e unidades amovíveis</translation>
+        <translation>Gestor de discos e unidades amovíveis</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro ao desmontar &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt;: %2</translation>
     </message>
 </context>
 <context>
@@ -138,7 +138,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Não existem dispositivos</translation>
     </message>
 </context>
 </TS>
