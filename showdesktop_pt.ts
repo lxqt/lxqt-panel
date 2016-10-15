70b645cf601c32dedc84ac814b99f7a0361896f9
Sérgio Marques <smarquespt@gmail.com>
2016-10-15 15:30:45 +0100

    Portuguese update for lxqt-panel modules, pavucontrol-qt and pcmanfm

--- showdesktop_pt.ts
+++ showdesktop_pt.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../showdesktop.cpp" line="48"/>
         <source>Show desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar área de trabalho</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="70"/>
