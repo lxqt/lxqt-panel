147daf0e826fc72a9ee88a588b84295369842333
Sérgio Marques <smarquespt@gmail.com>
2016-10-15 15:30:45 +0100

    Portuguese update for lxqt-panel modules, pavucontrol-qt and pcmanfm

--- kbindicator_pt.ts
+++ kbindicator_pt.ts
@@ -7,47 +7,47 @@
         <location filename="../../../src/content.cpp" line="43"/>
         <source>C</source>
         <comment>Label for CapsLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>C</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="46"/>
         <source>CapsLock</source>
         <comment>Tooltip for CapsLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>CapsLock</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="50"/>
         <source>N</source>
         <comment>Label for NumLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>N</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="52"/>
         <source>NumLock</source>
         <comment>Tooltip for NumLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>NumLock</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="57"/>
         <source>S</source>
         <comment>Label for ScrollLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>S</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="59"/>
         <source>ScrollLock</source>
         <comment>Tooltip for ScrollLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>ScrollLock</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="90"/>
         <source>Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Disposição</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="90"/>
         <source>Variant</source>
-        <translation type="unfinished"></translation>
+        <translation>Variante</translation>
     </message>
 </context>
 <context>
@@ -55,57 +55,57 @@
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="14"/>
         <source>Keyboard state settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Definições do estado do teclado</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="20"/>
         <source>Lock Indicators</source>
-        <translation type="unfinished"></translation>
+        <translation>Indicadores</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="26"/>
         <source>Show Caps Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar CapsLock</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="33"/>
         <source>Show Num Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar NumLock</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="40"/>
         <source>Show Scroll Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar ScrollLock</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="50"/>
         <source>Keyboard Layout Indicator</source>
-        <translation type="unfinished"></translation>
+        <translation>Indicador de disposição do teclado</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="62"/>
         <source>Switching policy</source>
-        <translation type="unfinished"></translation>
+        <translation>Política de troca</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="69"/>
         <source>Global</source>
-        <translation type="unfinished"></translation>
+        <translation>Global</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="79"/>
         <source>Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Janela</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="89"/>
         <source>Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicação</translation>
     </message>
     <message>
         <location filename="../../../src/kbdstateconfig.ui" line="102"/>
         <source>Configure layouts</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurar disposição</translation>
     </message>
 </context>
 <context>
