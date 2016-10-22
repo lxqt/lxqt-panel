6459b7d11fb9ef63e225c92106dec6ba20592291
paiiou <paiiou@free.fr>
2016-10-22 18:19:46 +0200

    lxqt-panel: Update French translations

--- volume_fr.ts
+++ volume_fr.ts
@@ -1,37 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.1" language="fr">
 <context>
     <name>LXQtVolume</name>
     <message>
-        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="vanished">Montrer le bureau : le raccourci global &apos;%1&apos; ne peut pas être défini</translation>
-    </message>
-    <message>
         <location filename="../../../lxqtvolume.cpp" line="62"/>
         <source>Increase sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Augmenter le volume du son</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="68"/>
         <source>Decrease sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Diminuer le volume du son</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="74"/>
         <source>Mute/unmute sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Son muet ou pas</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="135"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Contrôle du volume : le raccourci suivant&apos; ne peut pas être défini &apos;%1</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="223"/>
         <location filename="../../../lxqtvolume.cpp" line="233"/>
         <source>Volume: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume : %1</translation>
     </message>
 </context>
 <context>
@@ -39,57 +35,57 @@
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètres du contrôle de volume</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation type="unfinished"></translation>
+        <translation>Périphérique à contrôler</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="28"/>
         <source>Alsa</source>
-        <translation type="unfinished"></translation>
+        <translation>Alsa</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="35"/>
         <source>PulseAudio</source>
-        <translation type="unfinished"></translation>
+        <translation>PulseAudio</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="42"/>
         <source>OSS</source>
-        <translation type="unfinished"></translation>
+        <translation>OSS</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation type="unfinished"></translation>
+        <translation>Propriétés</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation type="unfinished"></translation>
+        <translation>Muet avec le clic central</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher avec un clic de souris</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Permet le volume jusqu&apos;à 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="92"/>
         <source>Volume adjust step</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas du réglage du volume</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="111"/>
         <source>External Mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Mixer externe</translation>
     </message>
 </context>
 <context>
@@ -97,12 +93,12 @@
     <message>
         <location filename="../../../volumepopup.cpp" line="53"/>
         <source>Launch mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarre le mixer</translation>
     </message>
     <message>
         <location filename="../../../volumepopup.cpp" line="54"/>
         <source>Mi&amp;xer</source>
-        <translation type="unfinished"></translation>
+        <translation>Mi&amp;xeur</translation>
     </message>
 </context>
 </TS>
