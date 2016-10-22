a2415b1cd72195534e9eaa18b176c67163f49e31
paiiou <paiiou@free.fr>
2016-10-22 18:19:46 +0200

    lxqt-panel: Update French translations

---
+++ kbindicator_fr.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Content</name>
+    <message>
+        <location filename="../../../src/content.cpp" line="43"/>
+        <source>C</source>
+        <comment>Label for CapsLock indicator</comment>
+        <translation>M</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="46"/>
+        <source>CapsLock</source>
+        <comment>Tooltip for CapsLock indicator</comment>
+        <translation>Verr Maj</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="50"/>
+        <source>N</source>
+        <comment>Label for NumLock indicator</comment>
+        <translation>N</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="52"/>
+        <source>NumLock</source>
+        <comment>Tooltip for NumLock indicator</comment>
+        <translation>Verr Num</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="57"/>
+        <source>S</source>
+        <comment>Label for ScrollLock indicator</comment>
+        <translation>D</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="59"/>
+        <source>ScrollLock</source>
+        <comment>Tooltip for ScrollLock indicator</comment>
+        <translation>Verr Défil</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Layout</source>
+        <translation>Disposition</translation>
+    </message>
+    <message>
+        <location filename="../../../src/content.cpp" line="90"/>
+        <source>Variant</source>
+        <translation>Variant</translation>
+    </message>
+</context>
+<context>
+    <name>KbdStateConfig</name>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="14"/>
+        <source>Keyboard state settings</source>
+        <translation>Paramétrage de l&apos;état du clavier</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="20"/>
+        <source>Lock Indicators</source>
+        <translation>Indicateurs de verrouillage</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="26"/>
+        <source>Show Caps Lock</source>
+        <translation>Allumer Verr Maj</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="33"/>
+        <source>Show Num Lock</source>
+        <translation>Allumer Ver Num</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="40"/>
+        <source>Show Scroll Lock</source>
+        <translation>Allumer Verr Défil</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="50"/>
+        <source>Keyboard Layout Indicator</source>
+        <translation>Indicateur de la disposition du clavier</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="62"/>
+        <source>Switching policy</source>
+        <translation>Changement de règles</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="69"/>
+        <source>Global</source>
+        <translation>Global</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="79"/>
+        <source>Window</source>
+        <translation>Fenêtre</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="89"/>
+        <source>Application</source>
+        <translation>Application</translation>
+    </message>
+    <message>
+        <location filename="../../../src/kbdstateconfig.ui" line="102"/>
+        <source>Configure layouts</source>
+        <translation>Configurer les dispositions</translation>
+    </message>
+</context>
+</TS>
