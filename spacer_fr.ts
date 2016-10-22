5355025ca65410b3b1dbcec9a7a3d70a4403112b
paiiou <paiiou@free.fr>
2016-10-22 18:19:46 +0200

    lxqt-panel: Update French translations

---
+++ spacer_fr.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>Paramétrage du séparateur</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>Largeur de l&apos;espace :</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>Type de l&apos;espace :</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation>une ligne</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation>des points</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation>invisible</translation>
+    </message>
+</context>
+</TS>
