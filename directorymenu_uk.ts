9fa2c69bbfd472d1f8cd213d4917c4c51f241120
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

---
+++ directorymenu_uk.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>DirectoryMenu</name>
+    <message>
+        <location filename="../../../directorymenu.cpp" line="146"/>
+        <source>Open</source>
+        <translation>Відкрити</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryMenuConfiguration</name>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="14"/>
+        <source>Directory Menu Settings</source>
+        <translation>Налаштування меню тек</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Вигляд</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="26"/>
+        <source>Base directory:</source>
+        <translation>Базова тека:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="39"/>
+        <source>BaseDirectoryName</source>
+        <translation>НазваБазовоїТеки</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="46"/>
+        <source>Icon:</source>
+        <translation>Значок:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="107"/>
+        <source>Choose Base Directory</source>
+        <translation>Вибрати базову теку</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="122"/>
+        <source>Choose Icon</source>
+        <translation>Вибрати значок</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="123"/>
+        <source>Icons (*.png *.xpm *.jpg)</source>
+        <translation>Значки (*.png *.xpm *.jpg) </translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>Directory Menu</source>
+        <translation>Меню теки</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="131"/>
+        <source>An error occurred while loading the icon.</source>
+        <translation>Сталася помилка завантаження значка.</translation>
+    </message>
+</context>
+</TS>
