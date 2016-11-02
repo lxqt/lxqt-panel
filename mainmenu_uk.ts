43889018e8b0207a88f9be8e90fd7251f7dd3592
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

--- mainmenu_uk.ts
+++ mainmenu_uk.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../../../lxqtmainmenu.cpp" line="95"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Пошук...</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenu.cpp" line="101"/>
         <source>Show/hide main menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати/сховати головне меню</translation>
     </message>
     <message>
         <source>Leave</source>
@@ -40,27 +40,27 @@
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="14"/>
         <source>Main Menu settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштування головного меню</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="35"/>
         <source>Icon:</source>
-        <translation type="unfinished"></translation>
+        <translation>Значок:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="59"/>
         <source>Button text:</source>
-        <translation type="unfinished"></translation>
+        <translation>Текст кнопки:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="76"/>
         <source>Custom font size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Власний розмір шрифту:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="86"/>
         <source> pt</source>
-        <translation type="unfinished"></translation>
+        <translation> пт</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="105"/>
@@ -70,42 +70,42 @@
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="111"/>
         <source>Menu file:</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл меню:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="169"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Пошук</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="175"/>
         <source>max. item width:</source>
-        <translation type="unfinished"></translation>
+        <translation>макс. розмір:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="182"/>
         <source>Show matching entries:</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати відповідні входження:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="196"/>
         <source>Maximum visible items:</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати максимально:</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="203"/>
         <source>Filter menu entries</source>
-        <translation type="unfinished"></translation>
+        <translation>Фільтрувати меню входжень</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="210"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> пт</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.ui" line="223"/>
         <source>Hide menu entries while searching</source>
-        <translation type="unfinished"></translation>
+        <translation>Приховати меню входжень під час пошуку</translation>
     </message>
     <message>
         <source>...</source>
@@ -124,17 +124,17 @@
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.cpp" line="71"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Скинути</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.cpp" line="156"/>
         <source>Choose icon file</source>
-        <translation type="unfinished"></translation>
+        <translation>Вибрати файл значка</translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.cpp" line="158"/>
         <source>Images (*.svg *.png)</source>
-        <translation type="unfinished"></translation>
+        <translation>Малюнки (*.svg *.png) </translation>
     </message>
     <message>
         <location filename="../../../lxqtmainmenuconfiguration.cpp" line="170"/>
