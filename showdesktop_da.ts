5ac8d126b2b2c57d47e04ff3fa6db96523cb3183
scootergrisen <scootergrisen@gmail.com>
2016-12-13 02:29:38 +0100

    First attempt to change all danish translation files from using i mix of "da" and "da_DK" and both, to only using "da"

--- showdesktop_da.ts
+++ showdesktop_da.ts
@@ -4,14 +4,6 @@
 <context>
     <name>ShowDesktop</name>
     <message>
-        <source>Global keyboard shortcut</source>
-        <translation type="vanished">Global tastaturgenvej</translation>
-    </message>
-    <message>
-        <source>Panel Show Desktop Global shortcut: &apos;%1&apos; cannot be registered</source>
-        <translation type="vanished">Global genvej for Vis Skrivebord : &apos;%1&apos; kan ikke registreres</translation>
-    </message>
-    <message>
         <location filename="../../../showdesktop.cpp" line="48"/>
         <source>Show desktop</source>
         <translation type="unfinished"></translation>
@@ -19,12 +11,12 @@
     <message>
         <location filename="../../../showdesktop.cpp" line="70"/>
         <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis Skrivebord: Global genvej &apos;%1&apos; kan ikke registreres</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="55"/>
         <source>Show Desktop</source>
-        <translation>Vis skrivebord</translation>
+        <translation>Vis Skrivebord</translation>
     </message>
 </context>
 </TS>
