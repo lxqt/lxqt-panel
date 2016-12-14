d4a6c90216ab9e55ecb96ec67cd3ed4f1dc88851
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

--- mount_da.ts
+++ mount_da.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Håndtering af medier/enheder</translation>
+        <translation>Håndtering af flytbare medier/enheder</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Medieindstillinger for flytbare medier</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Opførsel</translation>
+        <translation>Afværd</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Når en enheder tilsluttes:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Pop op-menu</translation>
+        <translation>Pop op-menu</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Vis information</translation>
+        <translation>Vis information</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Gør intet</translation>
+        <translation>Gør intet</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,36 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished">Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er forbundet.</translation>
+        <translation>Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er forbundet.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er fjernet.</translation>
+        <translation>Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er fjernet.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Håndtering af medier/enheder</translation>
-    </message>
-</context>
-<context>
-    <name>LXQtMountConfiguration</name>
-    <message>
-        <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">LXQt Indstillinger for flytbare medier</translation>
-    </message>
-    <message>
-        <source>Behaviour</source>
-        <translation type="vanished">Opførsel</translation>
-    </message>
-    <message>
-        <source>When a device is connected </source>
-        <translation type="vanished">Når en enhed forbinder </translation>
-    </message>
-    <message>
-        <source>Popup menu</source>
-        <translation type="vanished">Pop op-menu</translation>
-    </message>
-    <message>
-        <source>Show info</source>
-        <translation type="vanished">Vis information</translation>
-    </message>
-    <message>
-        <source>Do nothing</source>
-        <translation type="vanished">Gør intet</translation>
+        <translation>Håndtering af flytbare medier/enheder</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
-        <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Klik for at tilgå denne enhed fra andre programmer.</translation>
-    </message>
-    <message>
-        <source>Click to eject this disc.</source>
-        <translation type="vanished">Klik for at skubbe disken ud.</translation>
-    </message>
-    <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Montering af &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; fejlede: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Håndtering af medier/enheder</translation>
+        <translation>Håndtering af flytbare medier/enheder</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MountButton</name>
-    <message>
-        <source>Removable media/devices manager</source>
-        <translation type="vanished">Håndtering af medier/enheder</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished">Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er forbundet.</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Enheden &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; er fjernet.</translation>
-    </message>
-    <message>
-        <source>No devices Available.</source>
-        <translation type="vanished">Ingen tilgængelige enheder.</translation>
+        <translation>Afmontering af &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; fejlede: %2</translation>
     </message>
 </context>
 <context>
@@ -138,7 +84,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen tilgængelig enheder</translation>
     </message>
 </context>
 </TS>
