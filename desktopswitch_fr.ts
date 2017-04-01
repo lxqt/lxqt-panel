52b5383683fff413bb73715eba0599a683a6897c
paiiou <paiiou@free.fr>
2017-04-01 10:18:58 +0200

    Fix french tralations

--- desktopswitch_fr.ts
+++ desktopswitch_fr.ts
@@ -4,13 +4,13 @@
 <context>
     <name>DesktopSwitch</name>
     <message>
-        <location filename="../../desktopswitch.cpp" line="84"/>
+        <location filename="../../../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
         <translation>Basculer sur le bureau %1</translation>
     </message>
     <message>
-        <location filename="../../desktopswitch.cpp" line="141"/>
-        <location filename="../../desktopswitch.cpp" line="151"/>
+        <location filename="../../../desktopswitch.cpp" line="141"/>
+        <location filename="../../../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
         <translation>Bureau %1</translation>
     </message>
@@ -18,37 +18,42 @@
 <context>
     <name>DesktopSwitchConfiguration</name>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="14"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
         <translation>Paramétrage du basculement de bureau</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="46"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
         <translation>Nombre de colonnes :</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="39"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
         <translation>Label des bureaux :</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="20"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="20"/>
         <source>Appearance</source>
         <translation>Apparence</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="54"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
         <translation>Numéros</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="59"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
         <translation>Noms</translation>
     </message>
     <message>
-        <location filename="../../desktopswitchconfiguration.ui" line="70"/>
+        <location filename="../../../desktopswitchconfiguration.ui" line="64"/>
+        <source>None</source>
+        <translation>Aucun</translation>
+    </message>
+    <message>
+        <location filename="../../../desktopswitchconfiguration.ui" line="75"/>
         <source>Desktop names</source>
         <translation>Noms des bureaux</translation>
     </message>
