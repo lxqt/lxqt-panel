74f8a2bc5ce553d323f0180d2252f5f494935168
paiiou <paiiou@free.fr>
2017-04-01 10:18:58 +0200

    Fix french tralations

--- directorymenu_fr.ts
+++ directorymenu_fr.ts
@@ -4,7 +4,7 @@
 <context>
     <name>DirectoryMenu</name>
     <message>
-        <location filename="../../directorymenu.cpp" line="120"/>
+        <location filename="../../../directorymenu.cpp" line="120"/>
         <source>Open</source>
         <translation>Ouvrir</translation>
     </message>
@@ -12,52 +12,52 @@
 <context>
     <name>DirectoryMenuConfiguration</name>
     <message>
-        <location filename="../../directorymenuconfiguration.ui" line="14"/>
+        <location filename="../../../directorymenuconfiguration.ui" line="14"/>
         <source>Directory Menu Settings</source>
         <translation>Paramètrage du menu des répertoires</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.ui" line="20"/>
+        <location filename="../../../directorymenuconfiguration.ui" line="20"/>
         <source>Appearance</source>
         <translation>Apparence</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.ui" line="26"/>
+        <location filename="../../../directorymenuconfiguration.ui" line="26"/>
         <source>Base directory:</source>
         <translation>Répertoire de base :</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.ui" line="39"/>
+        <location filename="../../../directorymenuconfiguration.ui" line="39"/>
         <source>BaseDirectoryName</source>
         <translation>Nom du répertoire de base</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.ui" line="46"/>
+        <location filename="../../../directorymenuconfiguration.ui" line="46"/>
         <source>Icon:</source>
         <translation>Icône :</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.cpp" line="92"/>
+        <location filename="../../../directorymenuconfiguration.cpp" line="92"/>
         <source>Choose Base Directory</source>
         <translation>Choisir le répertoire de base</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.cpp" line="108"/>
+        <location filename="../../../directorymenuconfiguration.cpp" line="108"/>
         <source>Choose Icon</source>
         <translation>Choisir l&apos;icône</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.cpp" line="108"/>
+        <location filename="../../../directorymenuconfiguration.cpp" line="108"/>
         <source>Icons (*.png *.xpm *.jpg)</source>
         <translation>Icônes (*.png *.xpm *.jpg)</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.cpp" line="117"/>
+        <location filename="../../../directorymenuconfiguration.cpp" line="117"/>
         <source>Directory Menu</source>
         <translation>Menu des répertoires</translation>
     </message>
     <message>
-        <location filename="../../directorymenuconfiguration.cpp" line="117"/>
+        <location filename="../../../directorymenuconfiguration.cpp" line="117"/>
         <source>An error occurred while loading the icon.</source>
         <translation>Echec du chargement de l&apos;icône.</translation>
     </message>
