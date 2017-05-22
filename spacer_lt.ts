b106823daf8b680e39732be57652b8fc397ee6a6
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

---
+++ spacer_lt.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>SpacerConfiguration</name>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="12"/>
+        <source>Spacer Settings</source>
+        <translation>Tarpiklio nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="18"/>
+        <source>Space width:</source>
+        <translation>Tarpo plotis:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.ui" line="38"/>
+        <source>Space type:</source>
+        <translation>Tarpo tipas:</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="34"/>
+        <source>lined</source>
+        <translation>linijinis</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="35"/>
+        <source>dotted</source>
+        <translation>taškuotas</translation>
+    </message>
+    <message>
+        <location filename="../../../spacerconfiguration.cpp" line="36"/>
+        <source>invisible</source>
+        <translation>nematomas</translation>
+    </message>
+</context>
+</TS>
