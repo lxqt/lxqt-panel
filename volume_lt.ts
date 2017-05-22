80d2684c5d7c795d177d8c92d6f6ed870372811b
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

--- volume_lt.ts
+++ volume_lt.ts
@@ -4,34 +4,30 @@
 <context>
     <name>LXQtVolume</name>
     <message>
-        <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="vanished">Rodyti darbalaukį: globalusis klavišas „%1“ negali būti registruojamas</translation>
-    </message>
-    <message>
         <location filename="../../../lxqtvolume.cpp" line="62"/>
         <source>Increase sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Pagarsinti</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="68"/>
         <source>Decrease sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Tildyti</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="74"/>
         <source>Mute/unmute sound volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutildyti/įjungti garsį</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="135"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Garsio reguliavimas: Šie spartieji klavišai negali būti užregistruoti: %1</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="223"/>
         <location filename="../../../lxqtvolume.cpp" line="233"/>
         <source>Volume: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Garsis: %1</translation>
     </message>
 </context>
 <context>
@@ -39,57 +35,57 @@
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Garsio reguliavimo nustatymai</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation type="unfinished"></translation>
+        <translation>Įrenginys, kurį valdyti</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="28"/>
         <source>Alsa</source>
-        <translation type="unfinished"></translation>
+        <translation>Alsa</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="35"/>
         <source>PulseAudio</source>
-        <translation type="unfinished"></translation>
+        <translation>PulseAudio</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="42"/>
         <source>OSS</source>
-        <translation type="unfinished"></translation>
+        <translation>OSS</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation type="unfinished"></translation>
+        <translation>Elgsena</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutildyti, spustelėjus vidurinį pelės mygtuką</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodyti, spustelėjus pele</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Leisti garsį virš 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="92"/>
         <source>Volume adjust step</source>
-        <translation type="unfinished"></translation>
+        <translation>Garsio reguliavimo žingsnis</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="111"/>
         <source>External Mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Išorinis maišiklis</translation>
     </message>
 </context>
 <context>
@@ -97,12 +93,12 @@
     <message>
         <location filename="../../../volumepopup.cpp" line="53"/>
         <source>Launch mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Paleisti maišiklį</translation>
     </message>
     <message>
         <location filename="../../../volumepopup.cpp" line="54"/>
         <source>Mi&amp;xer</source>
-        <translation type="unfinished"></translation>
+        <translation>Maiši&amp;klis</translation>
     </message>
 </context>
 </TS>
