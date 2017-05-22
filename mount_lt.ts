d1d90bbab2aa8d877819392dcb1ccf491c346e9c
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

--- mount_lt.ts
+++ mount_lt.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Pašalinamų įrenginių tvarkytuvė</translation>
+        <translation>Keičiamųjų laikmenų/įrenginių tvarkytuvė</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Keičiamųjų laikmenų nustatymai</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Elgsena</translation>
+        <translation>Elgsena</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Kai įrenginys yra prijungiamas :</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Iškylantis meniu</translation>
+        <translation>Iškylantysis meniu</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Rodyti informaciją</translation>
+        <translation>Rodyti informaciją</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Nieko nedaryti</translation>
+        <translation>Nieko nedaryti</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,36 @@
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="unfinished"> Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; prijungtas.</translation>
+        <translation>Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; yra prijungtas.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="unfinished">Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; pašalintas</translation>
+        <translation>Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; yra pašalintas.</translation>
     </message>
     <message>
         <location filename="../../../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Pašalinamų įrenginių tvarkytuvė</translation>
-    </message>
-</context>
-<context>
-    <name>LXQtMountConfiguration</name>
-    <message>
-        <source>LXQt Removable media manager settings</source>
-        <translation type="vanished">LXQt pašalinamų įrenginių tvarkytuvės nuostatos</translation>
-    </message>
-    <message>
-        <source>Behaviour</source>
-        <translation type="vanished">Elgsena</translation>
-    </message>
-    <message>
-        <source>When a device is connected </source>
-        <translation type="vanished">Prijungus įrenginį</translation>
-    </message>
-    <message>
-        <source>Popup menu</source>
-        <translation type="vanished">Iškylantis meniu</translation>
-    </message>
-    <message>
-        <source>Show info</source>
-        <translation type="vanished">Rodyti informaciją</translation>
-    </message>
-    <message>
-        <source>Do nothing</source>
-        <translation type="vanished">Nieko nedaryti</translation>
+        <translation>Keičiamųjų laikmenų/įrenginių tvarkytuvė</translation>
     </message>
 </context>
 <context>
     <name>MenuDiskItem</name>
     <message>
-        <source>Click to access this device from other applications.</source>
-        <translation type="vanished">Norėdami šį įrenginį pasiekti kitomis programomis, spragtelėkite.</translation>
-    </message>
-    <message>
-        <source>Click to eject this disc.</source>
-        <translation type="vanished">Norėdami išstumti diską, spragtelėkite.</translation>
-    </message>
-    <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; prijungimas nepavyko: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Pašalinamų įrenginių tvarkytuvė</translation>
+        <translation>Keičiamųjų laikmenų/įrenginių tvarkytuvė</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MountButton</name>
-    <message>
-        <source>Removable media/devices manager</source>
-        <translation type="vanished">Pašalinamų įrenginių tvarkytuvė</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation type="vanished"> Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; prijungtas.</translation>
-    </message>
-    <message>
-        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation type="vanished">Įrenginys &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; pašalintas</translation>
-    </message>
-    <message>
-        <source>No devices Available.</source>
-        <translation type="vanished">Įrenginių nėra</translation>
+        <translation>&lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; atjungimas nepavyko: %2</translation>
     </message>
 </context>
 <context>
@@ -138,7 +84,7 @@
     <message>
         <location filename="../../../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation type="unfinished"></translation>
+        <translation>Nėra prieinamų įrenginių</translation>
     </message>
 </context>
 </TS>
