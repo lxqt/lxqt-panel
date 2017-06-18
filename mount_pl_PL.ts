e1b6fa8f0b5dfc7dd84c6b8ad13b80814cf20f90
Marcin Mikołajczak <me@m4sk.in>
2017-06-18 20:02:30 +0200

    Updated Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

--- mount_pl_PL.ts
+++ mount_pl_PL.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Menedżer nośników wymiennych</translation>
+        <translation>Menedżer nośników wymiennych</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../../../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation type="unfinished">Ustawienia nośników wymiennych</translation>
+        <translation>Ustawienia nośników wymiennych</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation type="unfinished">Zachowanie</translation>
+        <translation>Zachowanie</translation>
     </message>
     <message>
         <location filename="../../../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation type="unfinished"></translation>
+        <translation>Po podłączeniu urządzenia:</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation type="unfinished">Pokaż listę</translation>
+        <translation>Wyskakujące menu</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation type="unfinished">Pokaż informacje</translation>
+        <translation>Pokaż informacje</translation>
     </message>
     <message>
         <location filename="../../../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation type="unfinished">Nic nierób</translation>
+        <translation>Nic nie rób</translation>
     </message>
 </context>
 <context>
@@ -104,18 +104,18 @@
     <message>
         <location filename="../../../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Montowanie &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; nie powiodło się: %2</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="157"/>
         <location filename="../../../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">Menedżer nośników wymiennych</translation>
+        <translation>Menedżer nośników wymiennych</translation>
     </message>
     <message>
         <location filename="../../../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Odmontowanie &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; nie powiodło się: %2</translation>
     </message>
 </context>
 <context>
