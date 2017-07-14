d9dfdf7f8d471b5a4d0c825b86b61eef3e6a3a12
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-07-14 13:12:55 +0200

    Updated Catalan translation

--- volume_ca.ts
+++ volume_ca.ts
@@ -26,7 +26,7 @@
     <message>
         <location filename="../../../lxqtvolume.cpp" line="284"/>
         <source>Volume: muted</source>
-        <translation type="unfinished"></translation>
+        <translation>Volum: silenci</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolume.cpp" line="286"/>
@@ -84,7 +84,7 @@
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="84"/>
         <source>Always notify about volume changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Notifica sempre sobre els canvis de volum</translation>
     </message>
     <message>
         <location filename="../../../lxqtvolumeconfiguration.ui" line="99"/>
