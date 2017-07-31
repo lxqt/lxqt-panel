c9b764832d5ce3d17f2e09f6982bcbc2c0fe5bfa
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2017-08-01 00:52:28 +0200

    Avoid calling text() in MatchAction on a QAction == 0

--- plugin-mainmenu/lxqtmainmenu.cpp
+++ plugin-mainmenu/lxqtmainmenu.cpp
@@ -497,19 +497,21 @@ bool LXQtMainMenu::eventFilter(QObject *obj, QEvent *event)
                 mMenu->hide(); // close the app menu
                 return true;
             }
-            else // go to the menu item starts with the pressed key
+            else // go to the menu item which starts with the pressed key if there is an active action.
             {
                 QString key = keyEvent->text();
                 if(key.isEmpty())
                     return false;
                 QAction* action = menu->activeAction();
-                QList<QAction*> actions = menu->actions();
-                QList<QAction*>::iterator it = qFind(actions.begin(), actions.end(), action);
-                it = std::find_if(it + 1, actions.end(), MatchAction(key));
-                if(it == actions.end())
-                    it = std::find_if(actions.begin(), it, MatchAction(key));
-                if(it != actions.end())
-                    menu->setActiveAction(*it);
+                if(action !=0) {
+                    QList<QAction*> actions = menu->actions();
+                    QList<QAction*>::iterator it = qFind(actions.begin(), actions.end(), action);
+                    it = std::find_if(it + 1, actions.end(), MatchAction(key));
+                    if(it == actions.end())
+                        it = std::find_if(actions.begin(), it, MatchAction(key));
+                    if(it != actions.end())
+                        menu->setActiveAction(*it);
+                }
             }
         }
 
