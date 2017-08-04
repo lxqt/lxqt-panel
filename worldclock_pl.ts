fab3dd0047da3058f88021fe5fdab2353948b8f7
m4sk1n <me@m4sk.in>
2017-08-04 23:37:16 +0200

    Improve Polish translation

--- worldclock_pl.ts
+++ worldclock_pl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pl-PL">
+<TS version="2.1" language="pl">
 <context>
     <name>LXQtWorldClock</name>
     <message>
@@ -108,7 +108,7 @@
     <message>
         <location filename="../../../lxqtworldclockconfiguration.ui" line="191"/>
         <source>Abbreviation</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrót</translation>
     </message>
     <message>
         <location filename="../../../lxqtworldclockconfiguration.ui" line="431"/>
