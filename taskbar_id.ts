91f9a3107fb6c37fdae22314d2d7f45802be0d00
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

--- taskbar_id.ts
+++ taskbar_id.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="id_ID">
+<TS version="2.0" language="id">
 <context>
     <name>LXQtTaskBar</name>
     <message>
         <location filename="../../../lxqttaskbar.cpp" line="641"/>
         <source>Activate task %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktifkan tugas %1</translation>
     </message>
 </context>
 <context>
@@ -14,87 +14,87 @@
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="478"/>
         <source>Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplikasi</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="512"/>
         <source>To &amp;Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk &amp;Desktop</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="514"/>
         <source>&amp;All Desktops</source>
-        <translation type="unfinished"></translation>
+        <translation>Semua Desktop</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="522"/>
         <source>Desktop &amp;%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Desktop &amp;%1</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="529"/>
         <source>&amp;To Current Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk Desktop Saat Ini</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="538"/>
         <source>Ma&amp;ximize</source>
-        <translation type="unfinished"></translation>
+        <translation>Maksimalkan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="545"/>
         <source>Maximize vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Maksimalkan secara vertikal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="550"/>
         <source>Maximize horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Maksimalkan secara horizontal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="556"/>
         <source>&amp;Restore</source>
-        <translation type="unfinished"></translation>
+        <translation>Pulihkan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="560"/>
         <source>Mi&amp;nimize</source>
-        <translation type="unfinished"></translation>
+        <translation>Minimalkan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="566"/>
         <source>Roll down</source>
-        <translation type="unfinished"></translation>
+        <translation>Gulung kebawah</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="572"/>
         <source>Roll up</source>
-        <translation type="unfinished"></translation>
+        <translation>Gulung keatas</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="580"/>
         <source>&amp;Layer</source>
-        <translation type="unfinished"></translation>
+        <translation>Lapisan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="582"/>
         <source>Always on &amp;top</source>
-        <translation type="unfinished"></translation>
+        <translation>Selalu diatas</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="588"/>
         <source>&amp;Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Normal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="594"/>
         <source>Always on &amp;bottom</source>
-        <translation type="unfinished"></translation>
+        <translation>Selalu dibawah</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbutton.cpp" line="602"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup</translation>
     </message>
 </context>
 <context>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../../../lxqttaskgroup.cpp" line="83"/>
         <source>Group</source>
-        <translation type="unfinished"></translation>
+        <translation>Grup</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskgroup.cpp" line="85"/>
         <source>Close group</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup Grup</translation>
     </message>
 </context>
 <context>
@@ -115,108 +115,108 @@
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="14"/>
         <source>Task Manager Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengaturan Pengelola Tugas</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="20"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Umum</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="83"/>
         <source>Cycle windows on wheel scrolling</source>
-        <translation type="unfinished"></translation>
+        <translation>Putar jendela saat gerigi digulirkan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="93"/>
         <source>Window &amp;grouping</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengelompokan jendela</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="112"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Penampilan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="131"/>
         <source>Maximum button width</source>
-        <translation type="unfinished"></translation>
+        <translation>Lebar tombol maksimal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="144"/>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="170"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="157"/>
         <source>Maximum button height</source>
-        <translation type="unfinished"></translation>
+        <translation>Tinggi tombol maksimal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="183"/>
         <source>Auto&amp;rotate buttons when the panel is vertical</source>
-        <translation type="unfinished"></translation>
+        <translation>Otomatis putar tombol saat panelnya vertikal</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="190"/>
         <source>Use icons by WindowClass, if available</source>
-        <translation type="unfinished"></translation>
+        <translation>Gunakan ikon oleh WindowClass, jika tersedia</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="76"/>
         <source>Close on middle-click</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup pada klik tengah</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="43"/>
         <source>Show only windows from desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Hanya tampilkan jendela dari desktop</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="55"/>
         <source>Show only windows from &amp;panel&apos;s screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Hanya tampilkan jendela dari layar panel</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="62"/>
         <source>Show only minimized windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Hanya tampilkan jendela yang diminimalkan</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="69"/>
         <source>Raise minimized windows on current desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Angkat jendela yang diminimalkan pada desktop saat ini</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="102"/>
         <source>Show popup on mouse hover</source>
-        <translation type="unfinished"></translation>
+        <translation>Tampilkan munculan saat tetikus diatasnya</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.ui" line="121"/>
         <source>Button style</source>
-        <translation type="unfinished"></translation>
+        <translation>Gaya tombol</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.cpp" line="45"/>
         <source>Icon and text</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikon dan teks</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.cpp" line="46"/>
         <source>Only icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikon saja</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.cpp" line="47"/>
         <source>Only text</source>
-        <translation type="unfinished"></translation>
+        <translation>Teks saja</translation>
     </message>
     <message>
         <location filename="../../../lxqttaskbarconfiguration.cpp" line="49"/>
         <source>Current</source>
-        <translation type="unfinished"></translation>
+        <translation>Saat ini</translation>
     </message>
 </context>
 </TS>
