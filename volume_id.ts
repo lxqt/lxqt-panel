2c68cb3287b60a74125858f2fd1e472c3b0124d7
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

---
+++ volume_id.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id">
+<context>
+    <name>LXQtVolume</name>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="63"/>
+        <source>Increase sound volume</source>
+        <translation>Naikkan volume suara</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="69"/>
+        <source>Decrease sound volume</source>
+        <translation>Turunkan volume suara</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="75"/>
+        <source>Mute/unmute sound volume</source>
+        <translation>Bisukan/hidupkan volume suara</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="136"/>
+        <source>Volume Control: The following shortcuts can not be registered: %1</source>
+        <translation>Kontrol Volume: Pintasan berikut ini tidak dapat didaftarkan: %1</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="284"/>
+        <source>Volume: muted</source>
+        <translation>Volume: bisu</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolume.cpp" line="286"/>
+        <source>Volume: %1</source>
+        <translation>Volume: %1</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtVolumeConfiguration</name>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="14"/>
+        <source>Volume Control Settings</source>
+        <translation>Pengaturan Kontrol Volume</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="20"/>
+        <source>Device to control</source>
+        <translation>Perangkat untuk dikontrol</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="28"/>
+        <source>Alsa</source>
+        <translation>Alsa</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="35"/>
+        <source>PulseAudio</source>
+        <translation>PulseAudio</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="42"/>
+        <source>OSS</source>
+        <translation>OSS</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="57"/>
+        <source>Behavior</source>
+        <translation>Perilaku</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="63"/>
+        <source>Mute on middle click</source>
+        <translation>Bisukan saat klik tengah</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="70"/>
+        <source>Show on mouse click</source>
+        <translation>Tampilkan saat klik tetikus</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="77"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation>Izinkan volume diatas 100% (0dB)</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="84"/>
+        <source>Always notify about volume changes</source>
+        <translation>Selalu beritahu tentang perubahan volume</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="99"/>
+        <source>Volume adjust step</source>
+        <translation>Langkah menyesuaikan volume</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtvolumeconfiguration.ui" line="118"/>
+        <source>External Mixer</source>
+        <translation>Mikser Eksternal</translation>
+    </message>
+</context>
+<context>
+    <name>VolumePopup</name>
+    <message>
+        <location filename="../../../volumepopup.cpp" line="54"/>
+        <source>Launch mixer</source>
+        <translation>Luncurkan mikser</translation>
+    </message>
+    <message>
+        <location filename="../../../volumepopup.cpp" line="55"/>
+        <source>Mi&amp;xer</source>
+        <translation>Mikser</translation>
+    </message>
+</context>
+</TS>
