2911ef2e356a853fb414b69d82b6370e5dffe8c2
welaq <welaq@users.noreply.github.com>
2017-10-03 11:09:03 +0300

    Update Lithuanian translation

--- kbindicator_lt.ts
+++ kbindicator_lt.ts
@@ -7,37 +7,37 @@
         <location filename="../../../src/content.cpp" line="43"/>
         <source>C</source>
         <comment>Label for CapsLock indicator</comment>
-        <translation>C</translation>
+        <translation>D</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="46"/>
         <source>CapsLock</source>
         <comment>Tooltip for CapsLock indicator</comment>
-        <translation>CapsLock (Didž)</translation>
+        <translation>Didž (CapsLock)</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="50"/>
         <source>N</source>
         <comment>Label for NumLock indicator</comment>
-        <translation>N</translation>
+        <translation>S</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="52"/>
         <source>NumLock</source>
         <comment>Tooltip for NumLock indicator</comment>
-        <translation>NumLock (Skaitm)</translation>
+        <translation>Skaitm (NumLock)</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="57"/>
         <source>S</source>
         <comment>Label for ScrollLock indicator</comment>
-        <translation>S</translation>
+        <translation>L</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="59"/>
         <source>ScrollLock</source>
         <comment>Tooltip for ScrollLock indicator</comment>
-        <translation>ScrollLock (Slinkti)</translation>
+        <translation>Slinkti (ScrollLock)</translation>
     </message>
     <message>
         <location filename="../../../src/content.cpp" line="90"/>
