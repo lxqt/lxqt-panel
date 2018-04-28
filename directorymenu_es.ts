fe7c135bc6fe30e15b36ab280449618d3f2d9030
yo <yo@ejemplo.com>
2018-04-28 16:01:42 +0200

    Complete Spanish translation

---
+++ directorymenu_es.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>DirectoryMenu</name>
+    <message>
+        <location filename="../../../directorymenu.cpp" line="121"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryMenuConfiguration</name>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="14"/>
+        <source>Directory Menu Settings</source>
+        <translation>Configuración del Menú de directorio</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Apariencia</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="26"/>
+        <source>Base directory:</source>
+        <translation>Directorio base:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="39"/>
+        <source>BaseDirectoryName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.ui" line="46"/>
+        <source>Icon:</source>
+        <translation>Icono:</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="92"/>
+        <source>Choose Base Directory</source>
+        <translation>Elija el directorio base</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="108"/>
+        <source>Choose Icon</source>
+        <translation>Elija el icono</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="108"/>
+        <source>Icons (*.png *.xpm *.jpg)</source>
+        <translation>Iconos (*.png *.xpm *.jpg)</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="117"/>
+        <source>Directory Menu</source>
+        <translation>Menú de directorio</translation>
+    </message>
+    <message>
+        <location filename="../../../directorymenuconfiguration.cpp" line="117"/>
+        <source>An error occurred while loading the icon.</source>
+        <translation>Ha ocurrido un error al intentar cargar el icono.</translation>
+    </message>
+</context>
+</TS>
