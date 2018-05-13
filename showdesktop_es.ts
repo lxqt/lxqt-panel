09445feb93a78482bea9542ef991b94c0a64164a
micrococo <micrococo@gmx.com>
2018-05-13 10:59:05 +0200

    Spanish translation review

--- showdesktop_es.ts
+++ showdesktop_es.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../../../showdesktop.cpp" line="44"/>
         <source>Show desktop</source>
-        <translation>Mostrar escritorio</translation>
+        <translation>Mostrar el escritorio</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="66"/>
         <source>Show Desktop: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation>Mostrar escritorio: el atajo global &apos;%1&apos; no puede ser registrado</translation>
+        <translation>Mostrar el escritorio: el atajo global &apos;%1&apos; no puede ser registrado</translation>
     </message>
     <message>
         <location filename="../../../showdesktop.cpp" line="51"/>
         <source>Show Desktop</source>
-        <translation>Mostrar escritorio</translation>
+        <translation>Mostrar el escritorio</translation>
     </message>
 </context>
 </TS>
