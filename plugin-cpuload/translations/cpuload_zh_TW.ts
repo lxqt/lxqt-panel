a49bacdbc92deb481404e83ee76bfd056200ec03
DanLuInTW <c26400@gmail.com>
2018-06-14 02:54:54 +0000

    Translated using Weblate (漢語（正體字）)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Cpuload)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-cpuload/zh_Hant/

--- plugin-cpuload/translations/cpuload_zh_TW.ts
+++ plugin-cpuload/translations/cpuload_zh_TW.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../lxqtcpuload.cpp" line="127"/>
         <source>CPU load %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>CPU 負載 %1%</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="14"/>
         <source>CPU Load Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>CPU 負載設定</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="20"/>
