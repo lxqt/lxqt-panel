82d07f30804d4e3ba02e434f0618db633f51780e
ButterflyOfFire <butterflyoffire+lxqt@protonmail.com>
2018-06-16 10:46:47 +0000

    Translated using Weblate (Arabic)
    Currently translated at 95.0% (19 of 20 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/ar/

--- plugin-volume/translations/volume_ar.ts
+++ plugin-volume/translations/volume_ar.ts
@@ -21,17 +21,17 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="136"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation>التّحكّم بالصّوت: تعذّر تسجيل الاختصارات الآتية: %1</translation>
+        <translation>التّحكّم بالصّوت : تعذّر تسجيل الاختصارات الآتية : %1</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="287"/>
         <source>Volume: muted</source>
-        <translation type="unfinished"></translation>
+        <translation>الصوت : مكتوم</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="289"/>
         <source>Volume: %1%</source>
-        <translation type="unfinished">المستوى: %1%</translation>
+        <translation>مستوى الصوت : %1%</translation>
     </message>
 </context>
 <context>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation>إعدادات التّحكّم بالصّوت</translation>
+        <translation>إعدادات التّحكّم في الصّوت</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation>الجهاز للتّحكّم به</translation>
+        <translation>الجهاز الذي تود التّحكّم فيه</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
@@ -69,17 +69,17 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation>اكتم بالنّقر بالوسط</translation>
+        <translation>اكتم عند النّقر بالوسط</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation>أظهر بالنّقر بالوسط</translation>
+        <translation>أظهر عند النّقر بالوسط</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation>اسمح بارتفاع المستوى عن 100% (0ديسيبل)</translation>
+        <translation>اسمح بارتفاع المستوى عن 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
@@ -107,7 +107,7 @@
     <message>
         <location filename="../volumepopup.cpp" line="55"/>
         <source>Mi&amp;xer</source>
-        <translation>الما&amp;زج</translation>
+        <translation>المازج</translation>
     </message>
 </context>
 </TS>
