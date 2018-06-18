cd03cce5e2883163ec49664d17e8c7e1ef332cc2
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-18 07:27:07 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/he/

--- plugin-sensors/translations/sensors_he.ts
+++ plugin-sensors/translations/sensors_he.ts
@@ -6,77 +6,77 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="14"/>
         <source>Sensors Settings</source>
-        <translation></translation>
+        <translation>הגדרות חיישנים</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="30"/>
         <source>Common</source>
-        <translation></translation>
+        <translation>משותף</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="42"/>
         <source>Update interval (seconds)</source>
-        <translation></translation>
+        <translation>מרווח בין עדכונים (שניות)</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="49"/>
         <source>Temperature bar width</source>
-        <translation></translation>
+        <translation>רוחב סרגל מעלות</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="104"/>
         <source>Temperature scale</source>
-        <translation></translation>
+        <translation>סולם מעלות</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="110"/>
         <source>Celsius</source>
-        <translation></translation>
+        <translation>צלזיוס</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="120"/>
         <source>Fahrenheit</source>
-        <translation></translation>
+        <translation>פרנהייט</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink progress bars when the temperature is too high</source>
-        <translation></translation>
+        <translation>הבהוב סרגלי ההתקדמות כאשר המעלות גבוהות מדי</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
         <source>Warning about high temperature</source>
-        <translation></translation>
+        <translation>אזהרה על מעלות גבוהות</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="152"/>
         <source>Sensors</source>
-        <translation></translation>
+        <translation>חיישנים</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="168"/>
         <source>Detected chips:</source>
-        <translation></translation>
+        <translation>שבבים שהתגלו:</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="175"/>
         <source>Chip features:</source>
-        <translation></translation>
+        <translation>תכונות שבב:</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Enabled</source>
-        <translation></translation>
+        <translation>מופעל</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Label</source>
-        <translation></translation>
+        <translation>תווית</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Color</source>
-        <translation></translation>
+        <translation>צבע</translation>
     </message>
 </context>
 </TS>
