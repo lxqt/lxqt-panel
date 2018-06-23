95e238f2e88380e4d47024521065fb2bce663329
Dimitrios Glentadakis <dglent@free.fr>
2018-06-23 15:37:16 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Mainmenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-mainmenu/el/

--- plugin-mainmenu/translations/mainmenu_el.ts
+++ plugin-mainmenu/translations/mainmenu_el.ts
@@ -64,7 +64,7 @@
     <message>
         <location filename="../lxqtmainmenuconfiguration.ui" line="162"/>
         <source>Max. item width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Μεγ. πλάτος αντικειμένου:</translation>
     </message>
     <message>
         <source>max. item width:</source>
