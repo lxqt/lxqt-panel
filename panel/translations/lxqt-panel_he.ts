e24ae17eb06a4dd3cdfb284bdef630213472e113
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-25 10:10:54 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (73 of 73 strings)
    
    Translation: LXQt/LXQt Panel
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel/he/

--- panel/translations/lxqt-panel_he.ts
+++ panel/translations/lxqt-panel_he.ts
@@ -160,7 +160,7 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="173"/>
         <source>Rows:</source>
-        <translation type="unfinished"></translation>
+        <translation>שורות:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="219"/>
