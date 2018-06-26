690d7a7f15d790391a0595566957c920819cd3d1
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-26 06:25:00 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Cpuload)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-cpuload/he/

--- plugin-cpuload/translations/cpuload_he.ts
+++ plugin-cpuload/translations/cpuload_he.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../lxqtcpuload.cpp" line="127"/>
         <source>CPU load %1%</source>
-        <translation></translation>
+        <translation>עומס על המעבד  %1%</translation>
     </message>
 </context>
 <context>
@@ -14,57 +14,57 @@
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="14"/>
         <source>CPU Load Settings</source>
-        <translation></translation>
+        <translation>הגדרות עומס מעבד</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="20"/>
         <source>General</source>
-        <translation></translation>
+        <translation>כללי</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="35"/>
         <source>Show text</source>
-        <translation></translation>
+        <translation>הצגת טקסט</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="42"/>
         <source>Update interval:</source>
-        <translation></translation>
+        <translation>הפרש בין עדכונים:</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="49"/>
         <source>sec</source>
-        <translation></translation>
+        <translation>שניות</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="71"/>
         <source>Bar orientation:</source>
-        <translation></translation>
+        <translation>כיוון סרגל:</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="81"/>
         <source>Bar width:</source>
-        <translation></translation>
+        <translation>רוחב סרגל:</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.cpp" line="68"/>
         <source>Bottom up</source>
-        <translation></translation>
+        <translation>מלמטה למעלה</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.cpp" line="69"/>
         <source>Top down</source>
-        <translation></translation>
+        <translation>מלמעלה למטה</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.cpp" line="70"/>
         <source>Left to right</source>
-        <translation></translation>
+        <translation>שמאל לימין</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.cpp" line="71"/>
         <source>Right to left</source>
-        <translation></translation>
+        <translation>ימין לשמאל</translation>
     </message>
 </context>
 </TS>
