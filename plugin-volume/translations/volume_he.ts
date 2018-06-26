b9c01f7cd3c3f2d0e2cc22ebcec5419209a33910
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-26 08:59:35 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/he/

--- plugin-volume/translations/volume_he.ts
+++ plugin-volume/translations/volume_he.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="63"/>
         <source>Increase sound volume</source>
-        <translation></translation>
+        <translation>הגברת עצמת השמע</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="69"/>
         <source>Decrease sound volume</source>
-        <translation></translation>
+        <translation>הנמכת עצמת השמע</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="75"/>
         <source>Mute/unmute sound volume</source>
-        <translation></translation>
+        <translation>השתקה/השמעה של צלילים</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="136"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation></translation>
+        <translation>בקרת עצמת שמע: אין אפשרות לרשום את קיצורי הדרך הבאים: %1</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="287"/>
         <source>Volume: muted</source>
-        <translation></translation>
+        <translation>עצמת שמע: השתקה</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="289"/>
         <source>Volume: %1%</source>
-        <translation></translation>
+        <translation>עצמת שמע: %1%</translation>
     </message>
 </context>
 <context>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation></translation>
+        <translation>הגדרות בקרת עצמת שמע</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation></translation>
+        <translation>התקן לבקרה</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
@@ -64,37 +64,37 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation></translation>
+        <translation>התנהגות</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation></translation>
+        <translation>השתקה בכפתור אמצעי</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation></translation>
+        <translation>הצגה בלחיצה עם העכבר</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation></translation>
+        <translation>לאפשר עצמת שמע מעל 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
         <source>Always notify about volume changes</source>
-        <translation></translation>
+        <translation>תמיד להודיע על שינויים בעצמת השמע</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="99"/>
         <source>Volume adjust step</source>
-        <translation></translation>
+        <translation>צעדי התאמת עצמת שמע</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="118"/>
         <source>External Mixer</source>
-        <translation></translation>
+        <translation>מערבל חיצוני</translation>
     </message>
 </context>
 <context>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../volumepopup.cpp" line="54"/>
         <source>Launch mixer</source>
-        <translation></translation>
+        <translation>הפעלת מערבל</translation>
     </message>
     <message>
         <location filename="../volumepopup.cpp" line="55"/>
         <source>Mi&amp;xer</source>
-        <translation></translation>
+        <translation>מ&amp;ערבל</translation>
     </message>
 </context>
 </TS>
