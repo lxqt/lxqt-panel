04be779dafc73a36e9078f84c0f944ff80283b09
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2018-07-01 13:51:17 +0000

    Translated using Weblate (català)
    Currently translated at 100,0% (26 of 26 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Mainmenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-mainmenu/ca/

--- plugin-mainmenu/translations/mainmenu_ca.ts
+++ plugin-mainmenu/translations/mainmenu_ca.ts
@@ -64,7 +64,7 @@
     <message>
         <location filename="../lxqtmainmenuconfiguration.ui" line="162"/>
         <source>Max. item width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Amplada màx. de l&apos;ítem:</translation>
     </message>
     <message>
         <source>max. item width:</source>
