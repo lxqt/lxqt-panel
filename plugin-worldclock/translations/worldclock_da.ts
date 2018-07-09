c25618f8dfff8a9633e07d7a937f3460c9d67baf
scootergrisen <scootergrisen@gmail.com>
2018-07-09 19:20:13 +0000

    Translated using Weblate (Dansk)
    Currently translated at 100,0% (54 of 54 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Worldclock)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-worldclock/da/

--- plugin-worldclock/translations/worldclock_da.ts
+++ plugin-worldclock/translations/worldclock_da.ts
@@ -284,7 +284,43 @@
 &lt;tr&gt;&lt;td&gt;TTTTTT&lt;/td&gt;&lt;td&gt;the timezone custom name. You can change it the &apos;Time zones&apos; tab of the configuration window&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;
 &lt;br /&gt;&lt;b&gt;Notes:&lt;/b&gt; &lt;ul&gt;&lt;li&gt;Any characters in the pattern that are not in the ranges of [&apos;a&apos;..&apos;z&apos;] and [&apos;A&apos;..&apos;Z&apos;] will be treated as quoted text. For instance, characters like &apos;:&apos;, &apos;.&apos;, &apos; &apos;, &apos;#&apos; and &apos;@&apos; will appear in the resulting time text even they are not enclosed within single quotes.The single quote is used to &apos;escape&apos; letters. Two single quotes in a row, whether inside or outside a quoted sequence, represent a &apos;real&apos; single quote.&lt;/li&gt;&lt;li&gt;Minimal update interval is 1 second. If z or zzz is configured time is shown with the milliseconds fraction, but not updated on millisecond basis (avoiding big performance hit).&lt;/li&gt;&lt;ul&gt;
 </source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h1&gt;Brugerdefineret syntaks til format med dato/klokkeslæt&lt;/h1&gt;
+&lt;p&gt;Et datomønster er en streng af tegn, hvor bestemte strenge af tegn erstattes af data om dato og klokkeslæt fra en kalender med formatering eller brugt til at generere data til en kalender når der parses.&lt;/p&gt;
+&lt;p&gt;Symboltabellen til datofeltet nedenfor indeholder tegnene som bruges i mønstre til at vise de rette formater for en given lokalitet, såsom yyyy for året. Tegnene kan bruges flere gange. F.eks. hvis y bruges til året, så kan &apos;yy&apos; måske give &apos;99&apos;, hvor &apos;yyyy&apos; giver &apos;1999&apos;. Til de fleste nummerfeltet, angiver antallet af tegn feltets bredde. F.eks. hvis h er timen, så kan &apos;h&apos; måske give &apos;5&apos;, men &apos;hh&apos; giver &apos;05&apos;. Ved nogle tegn angiver antallet om en forkortet eller fuld form skal bruges, men kan have andre valg, som givet nedenfor.&lt;/p&gt;
+&lt;p&gt;To enkelte citationstegn repræsenterer et literal enkelt citationstegn, enten inden i eller udenfor enkelte citationstegn. Tekst inden i enkelte citationstegn fortolkes ikke på nogen måde (undtagen to omgivende enkelte citationstegn). Ellers er alle ASCII-tegn fra a til z og A til Z reserveret som syntakstegn, og kræver citering hvis de skal repræsentere literal tegn. Derudover, kan bestemte ASCII-tegnsætningstegn blive variabler i fremtiden (f.eks. kan &quot;:&quot; fortolkes som klokkeslætsseparatoren og &apos;/&apos; som en datoseparator, og erstattes af respektive lokalitetsfølsomme tegn i visningen).&lt;br /&gt;&lt;/p&gt;
+&lt;table border=&quot;1&quot; width=&quot;100%&quot; cellpadding=&quot;4&quot; cellspacing=&quot;0&quot;&gt;
+&lt;tr&gt;&lt;th width=&quot;20%&quot;&gt;Kode&lt;/th&gt;&lt;th&gt;Betydning&lt;/th&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;d&lt;/td&gt;&lt;td&gt;dagen som nummer uden et foranstillet nul (1 til 31)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;dd&lt;/td&gt;&lt;td&gt;dagen som nummer med et foranstillet nul (01 til 31)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ddd&lt;/td&gt;&lt;td&gt;det forkortet oversatte navn på dagen (f.eks. &apos;Man&apos; til &apos;Søn&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;dddd&lt;/td&gt;&lt;td&gt;det lange oversatte navn på dagen (f.eks. &apos;Mandag&apos; til &apos;Søndag&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;M&lt;/td&gt;&lt;td&gt;måneden som nummer uden et foranstillet nul (1-12)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MM&lt;/td&gt;&lt;td&gt;måneden som nummer med et foranstillet nul (01-12)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MMM&lt;/td&gt;&lt;td&gt;det forkortet oversatte navn på måneden (f.eks. &apos;Jan&apos; til &apos;Dec&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MMMM&lt;/td&gt;&lt;td&gt;det lange oversatte navn på måneden (f.eks. &apos;Januar&apos; til &apos;December&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;yy&lt;/td&gt;&lt;td&gt;året som nummer på 2 tegn (00-99)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;yyyy&lt;/td&gt;&lt;td&gt;året som nummer på 4 tegn&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;h&lt;/td&gt;&lt;td&gt;timen uden et foranstillet nul (0 til 23 eller 1 til 12 hvis visning med AM/PM)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;hh&lt;/td&gt;&lt;td&gt;timen med et foranstillet nul (00 til 23 eller 01 til 12 hvis visning med AM/PM)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;H&lt;/td&gt;&lt;td&gt;timen uden et foranstillet nul (0 til 23, selv ved visning med AM/PM)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;HH&lt;/td&gt;&lt;td&gt;timen med et foranstillet nul (00 til 23, selv ved visning med AM/PM)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;m&lt;/td&gt;&lt;td&gt;minuttet uden et foranstillet nul (0 til 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;mm&lt;/td&gt;&lt;td&gt;minuttet med et foranstillet nul (00 til 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;s&lt;/td&gt;&lt;td&gt;sekundet uden et foranstillet nul (0 til 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ss&lt;/td&gt;&lt;td&gt;sekundet med et foranstillet nul (00 til 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;z&lt;/td&gt;&lt;td&gt;millisekundet uden foranstillede nuller (0 til 999)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;zzz&lt;/td&gt;&lt;td&gt;millisekundet med foranstillede nuller (000 til 999)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;AP &lt;i&gt;eller&lt;/i&gt; A&lt;/td&gt;&lt;td&gt;brug visning med AM/PM. &lt;b&gt;A/AP&lt;/b&gt; erstattes enten med &quot;AM&quot; eller &quot;PM&quot;.&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ap &lt;i&gt;eller&lt;/i&gt; a&lt;/td&gt;&lt;td&gt;brug visning med am/pm. &lt;b&gt;a/ap&lt;/b&gt; erstattes enten med &quot;am&quot; eller &quot;pm&quot;.&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;t&lt;/td&gt;&lt;td&gt;tidszonen (f.eks. &quot;CEST&quot;)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;T&lt;/td&gt;&lt;td&gt;forskydningen i forhold til UTC&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TT&lt;/td&gt;&lt;td&gt;tidszonen IANA-id&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTT&lt;/td&gt;&lt;td&gt;tidszonen forkortelse&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTT&lt;/td&gt;&lt;td&gt;tidszonen kort navn for visning&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTTT&lt;/td&gt;&lt;td&gt;tidszonen langt navn for visning&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTTTT&lt;/td&gt;&lt;td&gt;tidszonen tilpasset navn. Du kan ændre det med &apos;Tidszoner&apos;-fanebladet i konfigurationsvinduet&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;
+&lt;br /&gt;&lt;b&gt;Bemærkninger:&lt;/b&gt; &lt;ul&gt;&lt;li&gt;Tegn i mønsteret som ikke er i området [&apos;a&apos;..&apos;z&apos;] og [&apos;A&apos;..&apos;Z&apos;] behandles som citeret tekst. Tegn som f.eks. &apos;:&apos;, &apos;.&apos;, &apos; &apos;, &apos;#&apos; og &apos;@&apos; vises i den resulterende tidstekst, selv hvis de ikke er omsluttet af enkelte citationstegn. Det enkelte citationstegn bruges til &apos;undvige&apos; bogstaver. To enkelte citationstegn i træk, hvad enten de er inden i eller udenfor en citeret sætning, repræsenterer et &apos;ægte&apos; enkelt citationstegn.&lt;/li&gt;&lt;li&gt;Mindste opdateringsinterval er 1 sekund. Hvis z eller zzz er konfigureret, så vises tiden med brøkdele af millisekunder, men opdateres ikke på basis af millisekunder (for at undgå reduceret ydelse).&lt;/li&gt;&lt;ul&gt;
+</translation>
     </message>
 </context>
 <context>
