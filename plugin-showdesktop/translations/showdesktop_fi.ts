45a1d2ca782c84190545936eeff5d7fb084f80c9
Urho Hoikkanen <urho.hoikkanen@protonmail.com>
2018-07-11 13:46:54 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: LXQt/LXQt Panel (Plugin ShowDesktop)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-showdesktop/fi/

--- plugin-showdesktop/translations/showdesktop_fi.ts
+++ plugin-showdesktop/translations/showdesktop_fi.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../showdesktop.cpp" line="44"/>
         <source>Show desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytä työpöytä</translation>
     </message>
     <message>
         <location filename="../showdesktop.cpp" line="66"/>
