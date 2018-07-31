693c9fa60b9b9944900a89b4d4e90de89a1c1624
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-31 19:30:55 +0000

    Added translation using Weblate (Welsh)

---
+++ plugin-volume/translations/volume_cy.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cy">
+<context>
+    <name>LXQtVolume</name>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="63"/>
+        <source>Increase sound volume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="69"/>
+        <source>Decrease sound volume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="75"/>
+        <source>Mute/unmute sound volume</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="136"/>
+        <source>Volume Control: The following shortcuts can not be registered: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="287"/>
+        <source>Volume: muted</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="289"/>
+        <source>Volume: %1%</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LXQtVolumeConfiguration</name>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
+        <source>Volume Control Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
+        <source>Device to control</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
+        <source>Alsa</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="35"/>
+        <source>PulseAudio</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="42"/>
+        <source>OSS</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
+        <source>Behavior</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
+        <source>Mute on middle click</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
+        <source>Show on mouse click</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
+        <source>Always notify about volume changes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="99"/>
+        <source>Volume adjust step</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="118"/>
+        <source>External Mixer</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>VolumePopup</name>
+    <message>
+        <location filename="../volumepopup.cpp" line="54"/>
+        <source>Launch mixer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../volumepopup.cpp" line="55"/>
+        <source>Mi&amp;xer</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
