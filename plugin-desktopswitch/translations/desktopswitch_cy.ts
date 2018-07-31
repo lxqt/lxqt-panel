29a1c900f2e475430109772473625138fd50c7f7
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-31 19:25:53 +0000

    Added translation using Weblate (Welsh)

---
+++ plugin-desktopswitch/translations/desktopswitch_cy.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cy">
+<context>
+    <name>DesktopSwitch</name>
+    <message>
+        <location filename="../desktopswitch.cpp" line="84"/>
+        <source>Switch to desktop %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitch.cpp" line="141"/>
+        <location filename="../desktopswitch.cpp" line="151"/>
+        <source>Desktop %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DesktopSwitchConfiguration</name>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="14"/>
+        <source>DesktopSwitch settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="46"/>
+        <source>Number of rows:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="39"/>
+        <source>Desktop labels:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="54"/>
+        <source>Numbers</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="59"/>
+        <source>Names</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="64"/>
+        <source>None</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="75"/>
+        <source>Desktop names</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
