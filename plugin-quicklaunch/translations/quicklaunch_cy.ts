e841930495cc44b730012585995f501ab80c800d
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-31 19:30:19 +0000

    Added translation using Weblate (Welsh)

---
+++ plugin-quicklaunch/translations/quicklaunch_cy.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cy">
+<context>
+    <name>LXQtQuickLaunch</name>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="214"/>
+        <source>Drop Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="215"/>
+        <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="321"/>
+        <source>Drop application
+icons here</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QuickLaunchButton</name>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="55"/>
+        <source>Move left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="58"/>
+        <source>Move right</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="62"/>
+        <source>Remove from quicklaunch</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
