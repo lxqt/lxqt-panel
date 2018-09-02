29db23fd87d3b9fd468351db35c86e2a0d849484
Carmina16 <mistresssilvara@hotmail.com>
2018-09-02 12:55:31 +0000

    Translated using Weblate (Russian)
    Currently translated at 100,0% (20 of 20 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/ru/

--- plugin-volume/translations/volume_ru.ts
+++ plugin-volume/translations/volume_ru.ts
@@ -26,12 +26,12 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="287"/>
         <source>Volume: muted</source>
-        <translation type="unfinished"></translation>
+        <translation>Громкость: отключено</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="289"/>
         <source>Volume: %1%</source>
-        <translation type="unfinished">Громкость: %1%</translation>
+        <translation>Громкость: %1%</translation>
     </message>
 </context>
 <context>
@@ -84,7 +84,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
         <source>Always notify about volume changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Всегда уведомлять об изменении громкости</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="99"/>
