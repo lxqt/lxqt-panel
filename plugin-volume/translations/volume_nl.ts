a47a71a66b82d7c4b5797148a95ece19bdadf66d
Pjotr123 <pjotrvertaalt@gmail.com>
2018-09-03 09:45:34 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100,0% (22 of 22 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/nl/

--- plugin-volume/translations/volume_nl.ts
+++ plugin-volume/translations/volume_nl.ts
@@ -26,12 +26,12 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>Volume: %1%%2</source>
-        <translation type="unfinished">Geluidssterkte: %1% {1%%2?}</translation>
+        <translation>Geluidssterkte: %1%%2</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation type="unfinished"></translation>
+        <translation>(gedempt)</translation>
     </message>
     <message>
         <source>Volume: muted</source>
