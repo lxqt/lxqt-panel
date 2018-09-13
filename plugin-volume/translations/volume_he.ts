b8308f12ba62b1c9b2638d8133b2faf16bb7bc6a
Yaron Shahrabani <sh.yaron@gmail.com>
2018-09-13 06:35:15 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/he/

--- plugin-volume/translations/volume_he.ts
+++ plugin-volume/translations/volume_he.ts
@@ -26,12 +26,12 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>Volume: %1%%2</source>
-        <translation type="unfinished">עצמת שמע: %1% {1%%2?}</translation>
+        <translation>עצמת שמע: %1%%2</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation type="unfinished"></translation>
+        <translation>(מושתק)</translation>
     </message>
     <message>
         <source>Volume: muted</source>
