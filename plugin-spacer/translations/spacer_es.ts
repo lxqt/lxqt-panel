a81bf84545254cebd6ec6e1257c266fe8fb8eb02
micrococo <micrococo@gmx.com>
2018-09-21 09:09:04 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100,0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/es/

--- plugin-spacer/translations/spacer_es.ts
+++ plugin-spacer/translations/spacer_es.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>fijo</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>expandible</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
