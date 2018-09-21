f6952f51c0196f9832d63f25a1cd5ca7478a9dae
Takefumi Nagata <take1975@kumamoto.email.ne.jp>
2018-09-21 14:40:35 +0000

    Translated using Weblate (Japanese)
    Currently translated at 36.8% (7 of 19 strings)
    
    Translation: LXQt/LXQt Panel (Plugin KBIndicator)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-kbindicator/ja/

--- plugin-kbindicator/translations/kbindicator_ja.ts
+++ plugin-kbindicator/translations/kbindicator_ja.ts
@@ -7,42 +7,42 @@
         <location filename="../src/content.cpp" line="43"/>
         <source>C</source>
         <comment>Label for CapsLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>C</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="46"/>
         <source>CapsLock</source>
         <comment>Tooltip for CapsLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>CapsLock</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="50"/>
         <source>N</source>
         <comment>Label for NumLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>N</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="52"/>
         <source>NumLock</source>
         <comment>Tooltip for NumLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>NumLock</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="57"/>
         <source>S</source>
         <comment>Label for ScrollLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>S</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="59"/>
         <source>ScrollLock</source>
         <comment>Tooltip for ScrollLock indicator</comment>
-        <translation type="unfinished"></translation>
+        <translation>ScrollLock</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="90"/>
         <source>Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>レイアウト</translation>
     </message>
     <message>
         <location filename="../src/content.cpp" line="90"/>
