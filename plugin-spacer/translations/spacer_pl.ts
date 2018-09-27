05a0da77db70229f628cd71d5e2e1c35bdcdae47
Marcin Mikołajczak <marcin123407@vivaldi.net>
2018-09-27 10:43:52 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/pl/

--- plugin-spacer/translations/spacer_pl.ts
+++ plugin-spacer/translations/spacer_pl.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>stały</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>rozszerzalny</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
