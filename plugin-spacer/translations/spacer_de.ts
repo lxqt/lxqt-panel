2159246215029e36d43a83bb6a01c58243d3da7e
Ettore Atalan <atalanttore@googlemail.com>
2018-09-29 22:16:56 +0000

    Translated using Weblate (German)
    Currently translated at 100,0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/de/

--- plugin-spacer/translations/spacer_de.ts
+++ plugin-spacer/translations/spacer_de.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>fest</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>erweiterbar</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
