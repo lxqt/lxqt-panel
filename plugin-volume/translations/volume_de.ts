62cd3307473c63f6c9334919da90c4dbd11693b9
Ettore Atalan <atalanttore@googlemail.com>
2018-09-29 22:17:45 +0000

    Translated using Weblate (German)
    Currently translated at 100,0% (22 of 22 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/de/

--- plugin-volume/translations/volume_de.ts
+++ plugin-volume/translations/volume_de.ts
@@ -26,12 +26,12 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>Volume: %1%%2</source>
-        <translation type="unfinished">Lautstärke: %1% {1%%2?}</translation>
+        <translation>Lautstärke: %1%%2</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation type="unfinished"></translation>
+        <translation>(stumm)</translation>
     </message>
     <message>
         <source>Volume: muted</source>
