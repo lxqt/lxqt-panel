4ccbb32844bf91ea6ecdaae746320d96093fc7ed
eltonfabricio10 <eltonfabricio10@gmail.com>
2018-10-04 03:49:33 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Networkmonitor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-networkmonitor/pt_BR/

--- plugin-networkmonitor/translations/networkmonitor_pt_BR.ts
+++ plugin-networkmonitor/translations/networkmonitor_pt_BR.ts
@@ -26,27 +26,27 @@
     <message>
         <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
         <source>KiB</source>
-        <translation>KiB</translation>
+        <translation>KB</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
         <source>MiB</source>
-        <translation>MByte</translation>
+        <translation>MB</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
         <source>GiB</source>
-        <translation>GByte</translation>
+        <translation>GB</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
         <source>TiB</source>
-        <translation>TByte</translation>
+        <translation>TB</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitor.cpp" line="209"/>
         <source>PiB</source>
-        <translation>PByte</translation>
+        <translation>PB</translation>
     </message>
 </context>
 <context>
@@ -74,7 +74,7 @@
     <message>
         <location filename="../lxqtnetworkmonitorconfiguration.ui" line="65"/>
         <source>Monitor</source>
-        <translation>Monitoramento</translation>
+        <translation>Monitor</translation>
     </message>
     <message>
         <location filename="../lxqtnetworkmonitorconfiguration.ui" line="74"/>
