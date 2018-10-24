e0f63f2f40c358463821aecef74ae9feb5b51eae
p-bo <pavel.borecki@gmail.com>
2018-10-24 17:46:31 +0000

    Translated using Weblate (Czech)
    Currently translated at 100,0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/cs/

--- plugin-spacer/translations/spacer_cs.ts
+++ plugin-spacer/translations/spacer_cs.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>pevné</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>zvětšovatelné</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
