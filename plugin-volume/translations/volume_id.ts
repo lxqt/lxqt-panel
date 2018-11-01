ac4fa4804d4bfbb4ed64111f2f929d30a245a370
Arif Budiman <arifpedia@gmail.com>
2018-11-01 13:24:06 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100,0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/id/

--- plugin-volume/translations/volume_id.ts
+++ plugin-volume/translations/volume_id.ts
@@ -26,12 +26,12 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>Volume: %1%%2</source>
-        <translation type="unfinished">Volume: %1% {1%%2?}</translation>
+        <translation>Volume: %1%%2</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation type="unfinished"></translation>
+        <translation>(bisu)</translation>
     </message>
     <message>
         <source>Volume: muted</source>
