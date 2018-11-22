508f81df13260379f737571d54e67494f02b89a7
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-22 16:18:29 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/gl/

--- plugin-volume/translations/volume_gl.ts
+++ plugin-volume/translations/volume_gl.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="63"/>
         <source>Increase sound volume</source>
-        <translation></translation>
+        <translation>Aumentar o volume do son</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="69"/>
         <source>Decrease sound volume</source>
-        <translation></translation>
+        <translation>Reducir o volume do son</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="75"/>
         <source>Mute/unmute sound volume</source>
-        <translation></translation>
+        <translation>Silenciar/activar o volume do son</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="136"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation></translation>
+        <translation>Control de volume: Non foi posíbel rexistrar os seguintes atallos: %1</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation></translation>
+        <translation>(silenciado)</translation>
     </message>
 </context>
 <context>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation></translation>
+        <translation>Axustes do control do volume</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation></translation>
+        <translation>Dispositivo a controlar</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
@@ -64,37 +64,37 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation></translation>
+        <translation>Comportamento</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation></translation>
+        <translation>Silenciar premendo co botón central do rato</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation></translation>
+        <translation>Amosar ao premer co rato</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation></translation>
+        <translation>Permitir que o volume exceda o 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
         <source>Always notify about volume changes</source>
-        <translation></translation>
+        <translation>Notificar sempre os cambios de volume</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="99"/>
         <source>Volume adjust step</source>
-        <translation></translation>
+        <translation>Paso do axuste do volume</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="118"/>
         <source>External Mixer</source>
-        <translation></translation>
+        <translation>Mesturador externo</translation>
     </message>
 </context>
 <context>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../volumepopup.cpp" line="54"/>
         <source>Launch mixer</source>
-        <translation></translation>
+        <translation>Iniciar o mesturador</translation>
     </message>
     <message>
         <location filename="../volumepopup.cpp" line="55"/>
         <source>Mi&amp;xer</source>
-        <translation></translation>
+        <translation>&amp;Mesturador</translation>
     </message>
 </context>
 </TS>
