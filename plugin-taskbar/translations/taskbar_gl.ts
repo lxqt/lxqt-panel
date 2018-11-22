5eee5e172ceef53fb93c6a8b23309a2ebe0616d2
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-22 18:34:24 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (43 of 43 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Taskbar)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-taskbar/gl/

--- plugin-taskbar/translations/taskbar_gl.ts
+++ plugin-taskbar/translations/taskbar_gl.ts
@@ -99,7 +99,7 @@
     <message>
         <location filename="../lxqttaskbutton.cpp" line="638"/>
         <source>Always on &amp;bottom</source>
-        <translation>Sempre &amp;embaixo</translation>
+        <translation>Sempre em&amp;baixo</translation>
     </message>
     <message>
         <location filename="../lxqttaskbutton.cpp" line="646"/>
@@ -112,7 +112,7 @@
     <message>
         <location filename="../lxqttaskgroup.cpp" line="82"/>
         <source>Group</source>
-        <translation>Groupo</translation>
+        <translation>Grupo</translation>
     </message>
     <message>
         <location filename="../lxqttaskgroup.cpp" line="84"/>
@@ -135,57 +135,57 @@
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="43"/>
         <source>Show only windows from desktop</source>
-        <translation></translation>
+        <translation>Amosar só as xanelas do escritorio</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="55"/>
         <source>Show only windows from &amp;panel&apos;s screen</source>
-        <translation></translation>
+        <translation>Amosar só as xanelas da pantalla do &amp;panel</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="62"/>
         <source>Show only minimized windows</source>
-        <translation></translation>
+        <translation>Amosar só as xanelas minimizadas</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="69"/>
         <source>Raise minimized windows on current desktop</source>
-        <translation></translation>
+        <translation>Elevar as xanelas minimizadas do escritorio actual</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="76"/>
         <source>Close on middle-click</source>
-        <translation></translation>
+        <translation>Pechar co botón central do rato</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="83"/>
         <source>Cycle windows on wheel scrolling</source>
-        <translation></translation>
+        <translation>Percorrer as xanelas coa roda do rato</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="93"/>
         <source>Window &amp;grouping</source>
-        <translation></translation>
+        <translation>A&amp;grupamento de xanelas</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="102"/>
         <source>Show popup on mouse hover</source>
-        <translation></translation>
+        <translation>Amosar a mensaxe emerxente ao pasar o rato por riba</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="112"/>
         <source>Appearance</source>
-        <translation></translation>
+        <translation>Aparencia</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="121"/>
         <source>Button style</source>
-        <translation></translation>
+        <translation>Estilo do botón</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="131"/>
         <source>Maximum button width</source>
-        <translation></translation>
+        <translation>Largura máxima do botón</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="144"/>
@@ -196,37 +196,37 @@
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="157"/>
         <source>Maximum button height</source>
-        <translation></translation>
+        <translation>Altura máxima do botón</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="183"/>
         <source>Auto&amp;rotate buttons when the panel is vertical</source>
-        <translation></translation>
+        <translation>&amp;Rotar automaticamente os botóns cando o panel estea en vertical</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="190"/>
         <source>Use icons by WindowClass, if available</source>
-        <translation></translation>
+        <translation>Usar as iconas de clase de xanela, se están dispoñíbeis</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="44"/>
         <source>Icon and text</source>
-        <translation></translation>
+        <translation>Icona e texto</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="45"/>
         <source>Only icon</source>
-        <translation></translation>
+        <translation>Só a icona</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="46"/>
         <source>Only text</source>
-        <translation></translation>
+        <translation>Só o texto</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="48"/>
         <source>Current</source>
-        <translation></translation>
+        <translation>Actual</translation>
     </message>
 </context>
 </TS>
