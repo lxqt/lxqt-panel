40b5e0186e6906896be8586bd0e713bce19cd1d0
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-22 15:20:33 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Mount)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-mount/gl/

--- plugin-mount/translations/mount_gl.ts
+++ plugin-mount/translations/mount_gl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../button.cpp" line="38"/>
         <source>Removable media/devices manager</source>
-        <translation></translation>
+        <translation>Administrador de medios/dispositivos extraíbeis</translation>
     </message>
 </context>
 <context>
@@ -14,32 +14,32 @@
     <message>
         <location filename="../configuration.ui" line="14"/>
         <source>Removable Media Settings</source>
-        <translation></translation>
+        <translation>Axustes de medios extraíbeis</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="20"/>
         <source>Behaviour</source>
-        <translation></translation>
+        <translation>Comportamento</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="26"/>
         <source>When a device is connected :</source>
-        <translation></translation>
+        <translation>Cando se conecta un dispositivo:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="40"/>
         <source>Popup menu</source>
-        <translation></translation>
+        <translation>Menú emerxente</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="41"/>
         <source>Show info</source>
-        <translation></translation>
+        <translation>Amosar información</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="42"/>
         <source>Do nothing</source>
-        <translation></translation>
+        <translation>Non facer nada</translation>
     </message>
 </context>
 <context>
@@ -47,17 +47,17 @@
     <message>
         <location filename="../actions/deviceaction_info.cpp" line="40"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
-        <translation></translation>
+        <translation>Conectouse o dispositivo &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <location filename="../actions/deviceaction_info.cpp" line="45"/>
         <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
-        <translation></translation>
+        <translation>Retirouse o dispositivo &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt;.</translation>
     </message>
     <message>
         <location filename="../actions/deviceaction_info.cpp" line="50"/>
         <source>Removable media/devices manager</source>
-        <translation></translation>
+        <translation>Administrador de medios/dispositivos extraíbeis</translation>
     </message>
 </context>
 <context>
@@ -65,18 +65,18 @@
     <message>
         <location filename="../menudiskitem.cpp" line="155"/>
         <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
-        <translation></translation>
+        <translation>Produciuse un fallo na montaxe de &lt;b&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/b&gt;: %2</translation>
     </message>
     <message>
         <location filename="../menudiskitem.cpp" line="157"/>
         <location filename="../menudiskitem.cpp" line="178"/>
         <source>Removable media/devices manager</source>
-        <translation></translation>
+        <translation>Administrador de medios/dispositivos extraíbeis</translation>
     </message>
     <message>
         <location filename="../menudiskitem.cpp" line="176"/>
         <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
-        <translation></translation>
+        <translation>Produciuse un fallo na desmontaxe de &lt;strong&gt;&lt;nobr&gt;«%1»&lt;/nobr&gt;&lt;/strong&gt;: %2</translation>
     </message>
 </context>
 <context>
@@ -84,7 +84,7 @@
     <message>
         <location filename="../popup.cpp" line="68"/>
         <source>No devices are available</source>
-        <translation></translation>
+        <translation>Non hai dispositivos dispoñíbeis</translation>
     </message>
 </context>
 </TS>
