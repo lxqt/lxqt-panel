c7aa962cfadaebe8abffc77d91b64420fe055853
Einar Mostad <einar@mostad.eu>
2018-11-24 22:59:11 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Desktopswitch)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-desktopswitch/nb_NO/

--- plugin-desktopswitch/translations/desktopswitch_nb_NO.ts
+++ plugin-desktopswitch/translations/desktopswitch_nb_NO.ts
@@ -6,13 +6,13 @@
     <message>
         <location filename="../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
-        <translation></translation>
+        <translation>Bytt til skrivebord %1</translation>
     </message>
     <message>
         <location filename="../desktopswitch.cpp" line="141"/>
         <location filename="../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
-        <translation></translation>
+        <translation>Skrivebord %1</translation>
     </message>
 </context>
 <context>
@@ -20,42 +20,42 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
-        <translation></translation>
+        <translation>Skrivebordsbytteinnstillinger</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
-        <translation></translation>
+        <translation>Antall rader:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
-        <translation></translation>
+        <translation>Skrivebordsnavn:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation></translation>
+        <translation>Utseende</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
-        <translation></translation>
+        <translation>Tall</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
-        <translation></translation>
+        <translation>Navn</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="64"/>
         <source>None</source>
-        <translation></translation>
+        <translation>Ingen</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="75"/>
         <source>Desktop names</source>
-        <translation></translation>
+        <translation>Skrivebordsnavn</translation>
     </message>
 </context>
 </TS>
