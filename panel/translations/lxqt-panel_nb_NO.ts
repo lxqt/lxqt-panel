4bd8f3b3d1cc366cdcd43e312a3d3fa5461a9249
Einar Mostad <einar@mostad.eu>
2018-11-24 21:56:04 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (75 of 75 strings)
    
    Translation: LXQt/LXQt Panel
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel/nb_NO/

--- panel/translations/lxqt-panel_nb_NO.ts
+++ panel/translations/lxqt-panel_nb_NO.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../config/addplugindialog.ui" line="14"/>
         <source>Add Plugins</source>
-        <translation></translation>
+        <translation>Legg til tillegg</translation>
     </message>
     <message>
         <location filename="../config/addplugindialog.ui" line="22"/>
         <source>Search:</source>
-        <translation></translation>
+        <translation>Søk:</translation>
     </message>
     <message>
         <location filename="../config/addplugindialog.ui" line="98"/>
         <source>Add Widget</source>
-        <translation></translation>
+        <translation>Legg til widget</translation>
     </message>
     <message>
         <location filename="../config/addplugindialog.ui" line="105"/>
         <source>Close</source>
-        <translation></translation>
+        <translation>Lukk</translation>
     </message>
     <message>
         <location filename="../config/addplugindialog.cpp" line="115"/>
         <source>(only one instance can run at a time)</source>
-        <translation></translation>
+        <translation>(et tillegg kan ikke kjøres mer enn en gang samtidig)</translation>
     </message>
 </context>
 <context>
@@ -34,17 +34,17 @@
     <message>
         <location filename="../config/configpaneldialog.cpp" line="31"/>
         <source>Configure Panel</source>
-        <translation></translation>
+        <translation>Konfigurer panelet</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="38"/>
         <source>Panel</source>
-        <translation></translation>
+        <translation>Panelet</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="42"/>
         <source>Widgets</source>
-        <translation></translation>
+        <translation>Widgeter</translation>
     </message>
 </context>
 <context>
@@ -52,27 +52,27 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="20"/>
         <source>Configure panel</source>
-        <translation></translation>
+        <translation>Konfigurer panelet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="44"/>
         <source>Size</source>
-        <translation></translation>
+        <translation>Størrelse</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="68"/>
         <source>&lt;p&gt;Negative pixel value sets the panel length to that many pixels less than available screen space.&lt;/p&gt;&lt;p/&gt;&lt;p&gt;&lt;i&gt;E.g. &quot;Length&quot; set to -100px, screen size is 1000px, then real panel length will be 900 px.&lt;/i&gt;&lt;/p&gt;</source>
-        <translation></translation>
+        <translation>&lt;p&gt;En negativ pikselverdi gjør panelet så mange piksler mindre enn skjermstørrelsen.&lt;/p&gt;&lt;p/&gt;&lt;p&gt;&lt;i&gt;Hvis f.eks. lengden settes til -100 piksler og skjermstørrelsen er 1000 piksler vil panelet bli 900 piksler langt.&lt;/i&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="81"/>
         <source>Size:</source>
-        <translation></translation>
+        <translation>Størrelse:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="88"/>
         <source>Length:</source>
-        <translation></translation>
+        <translation>Lengde:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="96"/>
@@ -82,34 +82,34 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="101"/>
         <source>px</source>
-        <translation></translation>
+        <translation>piksler</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="109"/>
         <location filename="../config/configpanelwidget.ui" line="153"/>
         <source> px</source>
-        <translation></translation>
+        <translation> piksler</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="166"/>
         <source>Icon size:</source>
-        <translation></translation>
+        <translation>Symbolstørrelse:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="202"/>
         <source>Alignment &amp;&amp; position</source>
-        <translation></translation>
+        <translation>Justering &amp;og posisjon</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="250"/>
         <source>A&amp;uto-hide</source>
-        <translation></translation>
+        <translation>S&amp;kjul automatisk</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="262"/>
         <location filename="../config/configpanelwidget.ui" line="285"/>
         <source>Zero means no animation</source>
-        <translation></translation>
+        <translation>Null betyr ingen animasjon</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="288"/>
@@ -121,22 +121,22 @@
         <location filename="../config/configpanelwidget.ui" line="301"/>
         <location filename="../config/configpanelwidget.ui" line="311"/>
         <source>Zero means no delay</source>
-        <translation></translation>
+        <translation>Null betyr ingen forsinkelse</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="304"/>
         <source>Show with delay:</source>
-        <translation></translation>
+        <translation>Vis med forsinkelse:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="330"/>
         <source>Don&apos;t allow maximized windows go under the panel window</source>
-        <translation></translation>
+        <translation>Ikke tillat maksimerte vinduer å ligge under panelet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="333"/>
         <source>Reserve space on display</source>
-        <translation></translation>
+        <translation>Reserver plass på skjermen</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="561"/>
@@ -148,152 +148,159 @@ You might also want to disable:
 LXQt Appearance Configuration →
 Icons Theme →
 Colorize icons based on widget style (palette)</source>
-        <translation></translation>
+        <translation>En delvis fiks for widget-stiler som ikke
+kan gi et eget tema til panelet.
+
+Du ønsker kanskje også å skru av:
+
+LXQt Utseendeinnstillinger →
+Symboltema →
+Fargelegg symboler basert på widget-stil (pallett)</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="571"/>
         <source>Override icon &amp;theme</source>
-        <translation></translation>
+        <translation>Overkjør symbo&amp;ltema</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="586"/>
         <source>Icon theme for panels:</source>
-        <translation></translation>
+        <translation>Symboltema for paneler:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="237"/>
         <source>Position:</source>
-        <translation></translation>
+        <translation>Plassering:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="265"/>
         <source>Animation duration:</source>
-        <translation></translation>
+        <translation>Lengde på animasjon:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="208"/>
         <source>Alignment:</source>
-        <translation></translation>
+        <translation>Justering:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="173"/>
         <source>Rows:</source>
-        <translation></translation>
+        <translation>Rader:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="219"/>
         <location filename="../config/configpanelwidget.cpp" line="206"/>
         <source>Left</source>
-        <translation></translation>
+        <translation>Venstre</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="224"/>
         <location filename="../config/configpanelwidget.cpp" line="207"/>
         <location filename="../config/configpanelwidget.cpp" line="213"/>
         <source>Center</source>
-        <translation></translation>
+        <translation>I midten</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="229"/>
         <location filename="../config/configpanelwidget.cpp" line="208"/>
         <source>Right</source>
-        <translation></translation>
+        <translation>Høyre</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="349"/>
         <source>Custom styling</source>
-        <translation></translation>
+        <translation>Egen stil</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="370"/>
         <source>Font color:</source>
-        <translation></translation>
+        <translation>Skriftfarge:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="410"/>
         <source>Background color:</source>
-        <translation></translation>
+        <translation>Bakgrunnsfarge:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="449"/>
         <source>Background opacity:</source>
-        <translation></translation>
+        <translation>Bakgrunnsgjennomsiktighet:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="481"/>
         <source>&lt;small&gt;Compositing is required for panel transparency.&lt;/small&gt;</source>
-        <translation></translation>
+        <translation>&lt;small&gt;Lagsammenføying (compositing) er nødvendig for panelets gjennomsiktighet.&lt;/small&gt;</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="509"/>
         <source>Background image:</source>
-        <translation></translation>
+        <translation>Bakgrunnsbilde:</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="172"/>
         <source>Top of desktop</source>
-        <translation></translation>
+        <translation>Øverst på skrivebordet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="173"/>
         <source>Left of desktop</source>
-        <translation></translation>
+        <translation>Venstre på skrivebordet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="174"/>
         <source>Right of desktop</source>
-        <translation></translation>
+        <translation>Høyre på skrivebordet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="175"/>
         <source>Bottom of desktop</source>
-        <translation></translation>
+        <translation>Nederst på skrivebordet</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="184"/>
         <source>Top of desktop %1</source>
-        <translation></translation>
+        <translation>Øverst på skrivebordet %1</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="185"/>
         <source>Left of desktop %1</source>
-        <translation></translation>
+        <translation>Venstre på skrivebordet %1</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="186"/>
         <source>Right of desktop %1</source>
-        <translation></translation>
+        <translation>Høyre på skrivebordet %1</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="187"/>
         <source>Bottom of desktop %1</source>
-        <translation></translation>
+        <translation>Nederst på skrivebordet %1</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="212"/>
         <source>Top</source>
-        <translation></translation>
+        <translation>Øverst</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="214"/>
         <source>Bottom</source>
-        <translation></translation>
+        <translation>Nederst</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="430"/>
         <location filename="../config/configpanelwidget.cpp" line="446"/>
         <source>Pick color</source>
-        <translation></translation>
+        <translation>Velg farge</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="464"/>
         <source>Pick image</source>
-        <translation></translation>
+        <translation>Velg bilde</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.cpp" line="464"/>
         <source>Images (*.png *.gif *.jpg)</source>
-        <translation></translation>
+        <translation>Bilder (*.png *.gif *.jpg)</translation>
     </message>
 </context>
 <context>
@@ -301,17 +308,17 @@ Colorize icons based on widget style (palette)</source>
     <message>
         <location filename="../config/configpluginswidget.ui" line="14"/>
         <source>Configure Plugins</source>
-        <translation></translation>
+        <translation>Konfigurer tillegg</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="84"/>
         <source>Note: changes made in this page cannot be reset.</source>
-        <translation></translation>
+        <translation>Obs: Forandringer gjort på denne siden kan ikke stilles om.</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="112"/>
         <source>Move up</source>
-        <translation></translation>
+        <translation>Flytt oppover</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="115"/>
@@ -325,22 +332,22 @@ Colorize icons based on widget style (palette)</source>
     <message>
         <location filename="../config/configpluginswidget.ui" line="126"/>
         <source>Move down</source>
-        <translation></translation>
+        <translation>Flytt nedover</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="147"/>
         <source>Add</source>
-        <translation></translation>
+        <translation>Legg til</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="161"/>
         <source>Remove</source>
-        <translation></translation>
+        <translation>Fjern</translation>
     </message>
     <message>
         <location filename="../config/configpluginswidget.ui" line="182"/>
         <source>Configure</source>
-        <translation></translation>
+        <translation>Konfigurer</translation>
     </message>
 </context>
 <context>
@@ -349,45 +356,46 @@ Colorize icons based on widget style (palette)</source>
         <location filename="../lxqtpanel.cpp" line="1068"/>
         <location filename="../lxqtpanel.cpp" line="1094"/>
         <source>Panel</source>
-        <translation></translation>
+        <translation>Panelet</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1097"/>
         <source>Configure Panel</source>
-        <translation></translation>
+        <translation>Konfigurer panelet</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1102"/>
         <source>Manage Widgets</source>
-        <translation></translation>
+        <translation>Styr Widgeter</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1108"/>
         <source>Add New Panel</source>
-        <translation></translation>
+        <translation>Legg til nytt panel</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1115"/>
         <source>Remove Panel</source>
         <comment>Menu Item</comment>
-        <translation></translation>
+        <translation>Fjern panel</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1120"/>
         <source>Lock This Panel</source>
-        <translation></translation>
+        <translation>Lås dette panelet</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1273"/>
         <source>Remove Panel</source>
         <comment>Dialog Title</comment>
-        <translation></translation>
+        <translation>Fjern panel</translation>
     </message>
     <message>
         <location filename="../lxqtpanel.cpp" line="1274"/>
         <source>Removing a panel can not be undone.
 Do you want to remove this panel?</source>
-        <translation></translation>
+        <translation>Å fjerne panelet kan ikke omgjøres.
+Ønsker du likevel å fjerne panelet?</translation>
     </message>
 </context>
 <context>
@@ -395,17 +403,17 @@ Do you want to remove this panel?</source>
     <message>
         <location filename="../plugin.cpp" line="411"/>
         <source>Configure &quot;%1&quot;</source>
-        <translation></translation>
+        <translation>Konfigurer &quot;%1&quot;</translation>
     </message>
     <message>
         <location filename="../plugin.cpp" line="416"/>
         <source>Move &quot;%1&quot;</source>
-        <translation></translation>
+        <translation>Flytt &quot;%1&quot;</translation>
     </message>
     <message>
         <location filename="../plugin.cpp" line="424"/>
         <source>Remove &quot;%1&quot;</source>
-        <translation></translation>
+        <translation>Fjern &quot;%1&quot;</translation>
     </message>
 </context>
 <context>
@@ -413,12 +421,12 @@ Do you want to remove this panel?</source>
     <message>
         <location filename="../lxqtpanelapplication.cpp" line="95"/>
         <source>Use alternate configuration file.</source>
-        <translation></translation>
+        <translation>Bruk en alternativ konfigurasjonsfil.</translation>
     </message>
     <message>
         <location filename="../lxqtpanelapplication.cpp" line="96"/>
         <source>Configuration file</source>
-        <translation></translation>
+        <translation>Konfigurasjonsfil</translation>
     </message>
 </context>
 </TS>
