8ec1fa0c5178e448d4b258c21ab9096d98542693
Einar Mostad <einar@mostad.eu>
2018-11-25 00:20:37 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/nb_NO/

--- plugin-volume/translations/volume_nb_NO.ts
+++ plugin-volume/translations/volume_nb_NO.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../lxqtvolume.cpp" line="63"/>
         <source>Increase sound volume</source>
-        <translation></translation>
+        <translation>Øk lydvolum</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="69"/>
         <source>Decrease sound volume</source>
-        <translation></translation>
+        <translation>Senk lydvolum</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="75"/>
         <source>Mute/unmute sound volume</source>
-        <translation></translation>
+        <translation>Skru av eller på lydvolum</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="136"/>
         <source>Volume Control: The following shortcuts can not be registered: %1</source>
-        <translation></translation>
+        <translation>Volumkontroll: Den følgende tastaturkommandoen kan ikke registreres: %1</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>Volume: %1%%2</source>
-        <translation></translation>
+        <translation>Volum: %1%%2</translation>
     </message>
     <message>
         <location filename="../lxqtvolume.cpp" line="286"/>
         <source>(muted)</source>
-        <translation></translation>
+        <translation>(skrudd av)</translation>
     </message>
 </context>
 <context>
@@ -39,12 +39,12 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
         <source>Volume Control Settings</source>
-        <translation></translation>
+        <translation>Volumkontrollinnstillinger</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
         <source>Device to control</source>
-        <translation></translation>
+        <translation>Enhet å kontrollere</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
@@ -64,37 +64,37 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
         <source>Behavior</source>
-        <translation></translation>
+        <translation>Oppførsel</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
         <source>Mute on middle click</source>
-        <translation></translation>
+        <translation>Skru av ved midtklikk</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
         <source>Show on mouse click</source>
-        <translation></translation>
+        <translation>Vis ved musklikk</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
         <source>Allow volume beyond 100% (0dB)</source>
-        <translation></translation>
+        <translation>Tillat volum over 100% (0dB)</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
         <source>Always notify about volume changes</source>
-        <translation></translation>
+        <translation>Alltid gi beskjed om volumforandringer</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="99"/>
         <source>Volume adjust step</source>
-        <translation></translation>
+        <translation>Volumjusteringssteg</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="118"/>
         <source>External Mixer</source>
-        <translation></translation>
+        <translation>Ekstern mikser</translation>
     </message>
 </context>
 <context>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../volumepopup.cpp" line="54"/>
         <source>Launch mixer</source>
-        <translation></translation>
+        <translation>Åpne mikser</translation>
     </message>
     <message>
         <location filename="../volumepopup.cpp" line="55"/>
         <source>Mi&amp;xer</source>
-        <translation></translation>
+        <translation>Mi&amp;kser</translation>
     </message>
 </context>
 </TS>
