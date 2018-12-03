32e597e1990ea8ab643e73e58d2db3df638dfa3d
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-12-03 16:12:56 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/ru/

--- plugin-spacer/translations/spacer_ru.ts
+++ plugin-spacer/translations/spacer_ru.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>фиксированный</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>расширяемый</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
