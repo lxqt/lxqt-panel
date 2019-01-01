ef699441267dde7e9d2c49619193a60ec5294dad
Ettore Atalan <atalanttore@googlemail.com>
2019-01-01 22:47:00 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (43 of 43 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Taskbar)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-taskbar/de/

--- plugin-taskbar/translations/taskbar_de.ts
+++ plugin-taskbar/translations/taskbar_de.ts
@@ -125,7 +125,7 @@
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="14"/>
         <source>Task Manager Settings</source>
-        <translation>Anwendungsverwalter - Einstellungen</translation>
+        <translation>Anwendungsverwaltungseinstellungen</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="20"/>
