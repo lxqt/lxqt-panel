4059e05201fcfa97fa6e320e9314594fb54c30f4
Jung-Kyu Park <bagjunggyu@gmail.com>
2019-01-05 07:43:40 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Desktopswitch)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-desktopswitch/ko/

--- plugin-desktopswitch/translations/desktopswitch_ko.ts
+++ plugin-desktopswitch/translations/desktopswitch_ko.ts
@@ -6,13 +6,13 @@
     <message>
         <location filename="../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>바탕화면 바꾸기 %1</translation>
     </message>
     <message>
         <location filename="../desktopswitch.cpp" line="141"/>
         <location filename="../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>바탕화면 %1</translation>
     </message>
 </context>
 <context>
@@ -20,42 +20,42 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
-        <translation type="unfinished"></translation>
+        <translation>바탕화면 바꾸기 설정</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
-        <translation type="unfinished"></translation>
+        <translation>줄 수:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
-        <translation type="unfinished"></translation>
+        <translation>바탕화면 이름표:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>꾸미기</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>번호</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
-        <translation type="unfinished"></translation>
+        <translation>이름</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="64"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>없음</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="75"/>
         <source>Desktop names</source>
-        <translation type="unfinished"></translation>
+        <translation>바탕화면 이름</translation>
     </message>
 </context>
 </TS>
