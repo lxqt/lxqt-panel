9d0fd14b3b14ab9e4038420602d7772a8d4fad09
stefonarch <stef.binde@riseup.net>
2019-01-06 21:31:17 +0000

    Translated using Weblate (Italian)
    Currently translated at 100,0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Spacer)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-spacer/it/

--- plugin-spacer/translations/spacer_it.ts
+++ plugin-spacer/translations/spacer_it.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>Fisso</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>espandibile</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
