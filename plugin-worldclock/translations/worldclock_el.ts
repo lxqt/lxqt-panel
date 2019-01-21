13add972fb85f9aaf7c853686467d0dd8fad01b5
Dimitrios Glentadakis <dglent@free.fr>
2019-01-21 05:50:34 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (54 of 54 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Worldclock)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-worldclock/el/

--- plugin-worldclock/translations/worldclock_el.ts
+++ plugin-worldclock/translations/worldclock_el.ts
@@ -243,7 +243,7 @@
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="14"/>
         <source>World Clock Manual Format Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Χειροκίνητη διαμόρφωση της μορφοποίησης του παγκόσμιου ρολογιού</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="83"/>
