d86015349d421d49a4c97b1740aa1449c56f71eb
Miguel A. Bouzada <mbouzada@gmail.com>
2019-01-24 10:15:52 +0000

    Translated using Weblate (Galician)
    Currently translated at 100,0% (54 of 54 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Worldclock)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-worldclock/gl/

--- plugin-worldclock/translations/worldclock_gl.ts
+++ plugin-worldclock/translations/worldclock_gl.ts
@@ -243,7 +243,7 @@
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="14"/>
         <source>World Clock Manual Format Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Configuración do formato manual do reloxo mundial</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="83"/>
