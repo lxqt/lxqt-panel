b257c5f9b7888fabfa4705575f90f0ca9719b363
PavelFric <pavelfric@yahoo.com>
2019-01-26 22:11:42 +0000

    Translated using Weblate (Czech)
    Currently translated at 98.1% (53 of 54 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Worldclock)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-worldclock/cs/

--- plugin-worldclock/translations/worldclock_cs.ts
+++ plugin-worldclock/translations/worldclock_cs.ts
@@ -243,7 +243,7 @@
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="14"/>
         <source>World Clock Manual Format Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Ruční nastavení formátu světových hodin</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfigurationmanualformat.ui" line="83"/>
