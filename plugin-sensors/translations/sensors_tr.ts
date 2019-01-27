ba3d2f7ad57b352e18cef4f26ea463a54b1c0aa5
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2019-01-27 03:59:23 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100,0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/tr/

--- plugin-sensors/translations/sensors_tr.ts
+++ plugin-sensors/translations/sensors_tr.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink status bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>Sıcaklık çok yüksek olduğunda durum çubukları yanıp söner</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
