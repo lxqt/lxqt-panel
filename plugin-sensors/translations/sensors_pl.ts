0d21f3936d667bd3aba0e312270340450360cf88
Marcin Mikołajczak <marcin123407@vivaldi.net>
2019-01-27 21:57:35 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/pl/

--- plugin-sensors/translations/sensors_pl.ts
+++ plugin-sensors/translations/sensors_pl.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink status bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>Migaj paskiem stanu, jeżeli temperatura jest zbyt wysoka</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
