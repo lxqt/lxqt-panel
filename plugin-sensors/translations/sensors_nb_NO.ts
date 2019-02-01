d42f0336fbd69f2c81d046105850ea6d53bc972e
Einar Mostad <einar@mostad.eu>
2019-02-01 22:42:09 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/nb_NO/

--- plugin-sensors/translations/sensors_nb_NO.ts
+++ plugin-sensors/translations/sensors_nb_NO.ts
@@ -46,7 +46,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
         <source>Warning about high temperature</source>
-        <translation>Varsel ved høy temperatur</translation>
+        <translation>Advarsel ved høy temperatur</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="152"/>
@@ -71,7 +71,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.cpp" line="195"/>
         <source>Label</source>
-        <translation>Navn</translation>
+        <translation>Etikett</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.cpp" line="195"/>
