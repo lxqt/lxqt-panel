0c683f62f0ccedde87f93f7fbbc0c6bb6825742a
Gema Aji Wardian <gema_wardian@hotmail.com>
2019-02-21 08:07:21 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/id/

--- plugin-sensors/translations/sensors_id.ts
+++ plugin-sensors/translations/sensors_id.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink status bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>Kedip status bar apabila suhunya terlalu tinggi</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
