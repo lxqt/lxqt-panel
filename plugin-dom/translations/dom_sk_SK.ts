8b74ae71acfd7902b8167c20934d25105fa1d210
Paolo Vigoroso <xpaolox@zoznam.sk>
2019-05-27 12:09:22 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Dom)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-dom/sk_SK/

--- plugin-dom/translations/dom_sk_SK.ts
+++ plugin-dom/translations/dom_sk_SK.ts
@@ -6,33 +6,33 @@
     <message>
         <location filename="../treewindow.ui" line="14"/>
         <source>Panel DOM tree</source>
-        <translation type="unfinished"></translation>
+        <translation>DOM strom panelu</translation>
     </message>
     <message>
         <location filename="../treewindow.ui" line="63"/>
         <location filename="../treewindow.ui" line="96"/>
         <source>Property</source>
-        <translation type="unfinished"></translation>
+        <translation>Vlastníctvo</translation>
     </message>
     <message>
         <location filename="../treewindow.ui" line="68"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Hodnota</translation>
     </message>
     <message>
         <location filename="../treewindow.ui" line="76"/>
         <source>All properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Všetky vlastnosti</translation>
     </message>
     <message>
         <location filename="../treewindow.ui" line="101"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Typ</translation>
     </message>
     <message>
         <location filename="../treewindow.ui" line="106"/>
         <source>String value</source>
-        <translation type="unfinished"></translation>
+        <translation>Hodnota reťazca</translation>
     </message>
 </context>
 </TS>
