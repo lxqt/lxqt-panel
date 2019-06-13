1e62b3619039b549bfb7b10ba3325da3bc27054f
BavonChao <chaochinghua@outlook.com>
2019-06-13 07:45:13 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/zh_TW/

--- plugin-sensors/translations/sensors_zh_TW.ts
+++ plugin-sensors/translations/sensors_zh_TW.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink status bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>當溫度過高時閃爍狀態欄</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
