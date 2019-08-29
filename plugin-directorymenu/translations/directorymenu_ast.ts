a6817f99a56cdf26f71fd8a142adc0308af64e3c
Enol P <enolp@softastur.org>
2019-08-29 11:45:44 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Directorymenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-directorymenu/ast/

--- plugin-directorymenu/translations/directorymenu_ast.ts
+++ plugin-directorymenu/translations/directorymenu_ast.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../directorymenu.cpp" line="120"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir</translation>
     </message>
 </context>
 <context>
@@ -14,52 +14,52 @@
     <message>
         <location filename="../directorymenuconfiguration.ui" line="14"/>
         <source>Directory Menu Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Axustes del menú de direutorios</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Aspeutu</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="26"/>
         <source>Base directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Direutoriu base:</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="39"/>
         <source>BaseDirectoryName</source>
-        <translation type="unfinished"></translation>
+        <translation>BaseDirectoryName</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="46"/>
         <source>Icon:</source>
-        <translation type="unfinished"></translation>
+        <translation>Iconu:</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="91"/>
         <source>Choose Base Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Escoyeta d&apos;un direutoriu base</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="107"/>
         <source>Choose Icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Escoyeta d&apos;un iconu</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="107"/>
         <source>Icons (*.png *.xpm *.jpg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Iconos (*.png *.xpm *.jpg)</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="116"/>
         <source>Directory Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Menú de direutorios</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="116"/>
         <source>An error occurred while loading the icon.</source>
-        <translation type="unfinished"></translation>
+        <translation>Asocedió un fallu mentanto se cargaba l&apos;iconu.</translation>
     </message>
 </context>
 </TS>
