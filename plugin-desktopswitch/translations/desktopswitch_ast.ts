e067b0576173074c5ec73cb82abf98351c583368
Enol P <enolp@softastur.org>
2019-08-29 11:51:13 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Desktopswitch)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-desktopswitch/ast/

--- plugin-desktopswitch/translations/desktopswitch_ast.ts
+++ plugin-desktopswitch/translations/desktopswitch_ast.ts
@@ -6,13 +6,13 @@
     <message>
         <location filename="../desktopswitch.cpp" line="84"/>
         <source>Switch to desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar al escritoriu %1</translation>
     </message>
     <message>
         <location filename="../desktopswitch.cpp" line="141"/>
         <location filename="../desktopswitch.cpp" line="151"/>
         <source>Desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Escritoriu %1</translation>
     </message>
 </context>
 <context>
@@ -20,42 +20,42 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="14"/>
         <source>DesktopSwitch settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Axustes del conmutador d&apos;escritorios</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="46"/>
         <source>Number of rows:</source>
-        <translation type="unfinished"></translation>
+        <translation>Númberu de fileres:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="39"/>
         <source>Desktop labels:</source>
-        <translation type="unfinished"></translation>
+        <translation>Etiquetes:</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Aspeutu</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="54"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Númberos</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="59"/>
         <source>Names</source>
-        <translation type="unfinished"></translation>
+        <translation>Nomes</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="64"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Nada</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="75"/>
         <source>Desktop names</source>
-        <translation type="unfinished"></translation>
+        <translation>Nomes de los escritorios</translation>
     </message>
 </context>
 </TS>
