fa1169535b9c580b0f0adb1bd7a2f39d4aa854b7
Alf Gaida <agaida@siduction.org>
2019-09-22 22:48:15 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Tray)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-tray/de/

--- plugin-tray/translations/tray_de.ts
+++ plugin-tray/translations/tray_de.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="6"/>
         <source>System Tray Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen des Infobereichs</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="12"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Allgemein</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="18"/>
         <source>Spacing between icons:</source>
-        <translation type="unfinished"></translation>
+        <translation>Abstand zwischen den Symbolen:</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="25"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="32"/>
         <source>Sort icons by window class</source>
-        <translation type="unfinished"></translation>
+        <translation>Symbole nach Fensterklasse sortieren</translation>
     </message>
 </context>
 </TS>
