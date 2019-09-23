4ef2ec3eb7dac1c4f91a0b60f160438e0d669a7d
Alf Gaida <agaida@siduction.org>
2019-09-23 09:18:44 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Directorymenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-directorymenu/de/

--- plugin-directorymenu/translations/directorymenu_de.ts
+++ plugin-directorymenu/translations/directorymenu_de.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../directorymenu.cpp" line="129"/>
         <source>Open in terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>In Terminal öffnen</translation>
     </message>
 </context>
 <context>
@@ -44,13 +44,13 @@
     <message>
         <location filename="../directorymenuconfiguration.ui" line="66"/>
         <source>Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="73"/>
         <location filename="../directorymenuconfiguration.cpp" line="112"/>
         <source>Choose Default Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Wählen Sie Standardterminal</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="96"/>
