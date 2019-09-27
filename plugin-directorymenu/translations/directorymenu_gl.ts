bc1e20a2731491bdee32bdc9c9a3e27b099bfe8b
Miguel A. Bouzada <mbouzada@gmail.com>
2019-09-27 09:25:41 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Directorymenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-directorymenu/gl/

--- plugin-directorymenu/translations/directorymenu_gl.ts
+++ plugin-directorymenu/translations/directorymenu_gl.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../directorymenu.cpp" line="129"/>
         <source>Open in terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir nun terminal</translation>
     </message>
 </context>
 <context>
@@ -44,13 +44,13 @@
     <message>
         <location filename="../directorymenuconfiguration.ui" line="66"/>
         <source>Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="73"/>
         <location filename="../directorymenuconfiguration.cpp" line="112"/>
         <source>Choose Default Terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Escolla o terminal predeterminado</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="96"/>
