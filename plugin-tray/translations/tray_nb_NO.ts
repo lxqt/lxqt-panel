94720134b14354af8e6a8f6c421ee41318dd5da5
Einar Mostad <einar@mostad.eu>
2019-09-28 11:04:37 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Tray)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-tray/nb_NO/

--- plugin-tray/translations/tray_nb_NO.ts
+++ plugin-tray/translations/tray_nb_NO.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="6"/>
         <source>System Tray Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Systemtrauinnstillinger</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="12"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Generelt</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="18"/>
         <source>Spacing between icons:</source>
-        <translation type="unfinished"></translation>
+        <translation>Avstand mellom symboler:</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="25"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> pk</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="32"/>
         <source>Sort icons by window class</source>
-        <translation type="unfinished"></translation>
+        <translation>Sorter symboler etter type vindu</translation>
     </message>
 </context>
 </TS>
