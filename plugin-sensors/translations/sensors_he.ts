837fd8a56ffd7f1ec9f3684271d3b09ed79be2f4
Yaron Shahrabani <sh.yaron@gmail.com>
2019-10-12 16:54:23 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Sensors)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-sensors/he/

--- plugin-sensors/translations/sensors_he.ts
+++ plugin-sensors/translations/sensors_he.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="132"/>
         <source>Blink status bars when the temperature is too high</source>
-        <translation type="unfinished"></translation>
+        <translation>להבהב את שורות המצב כשהטמפרטורה גבוהה מדי</translation>
     </message>
     <message>
         <location filename="../lxqtsensorsconfiguration.ui" line="138"/>
