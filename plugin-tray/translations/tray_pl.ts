73655672e720946e45250292b9c5a393195edca3
Marcin Mikołajczak <marcin123407@vivaldi.net>
2019-10-30 09:32:57 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Tray)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-tray/pl/

--- plugin-tray/translations/tray_pl.ts
+++ plugin-tray/translations/tray_pl.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="6"/>
         <source>System Tray Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia zasobnika systemowego</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="12"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Ogólne</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="18"/>
         <source>Spacing between icons:</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstęp między ikonami:</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="25"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="32"/>
         <source>Sort icons by window class</source>
-        <translation type="unfinished"></translation>
+        <translation>Sortuj ikony według klasy okna</translation>
     </message>
 </context>
 </TS>
