e4d672981078be1a8829be2d194e555b058e4690
LothaireCliquennois <cliqloth64@gnous.eu>
2019-11-11 17:13:22 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/fr/

--- plugin-volume/translations/volume_fr.ts
+++ plugin-volume/translations/volume_fr.ts
@@ -89,7 +89,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="91"/>
         <source>Notify about volume changes with keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Notification lorsque le volume est changé avec un raccourci clavier</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="106"/>
