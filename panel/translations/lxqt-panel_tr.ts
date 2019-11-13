a8f749beb32838878622a6cef626a312520cd5a2
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2019-11-13 12:09:59 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (76 of 76 strings)
    
    Translation: LXQt/LXQt Panel
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel/tr/

--- panel/translations/lxqt-panel_tr.ts
+++ panel/translations/lxqt-panel_tr.ts
@@ -160,7 +160,7 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="327"/>
         <source>Visible thin margin for hidden panel</source>
-        <translation type="unfinished"></translation>
+        <translation>Gizli panel için görünür ince kenar boşluğu</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="337"/>
