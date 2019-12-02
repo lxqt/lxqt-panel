bf56400fd1373e470205a28aed675301bf3d1ecd
eltonfabricio10 <eltonfabricio10@gmail.com>
2019-12-02 14:44:54 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Desktopswitch)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-desktopswitch/pt_BR/

--- plugin-desktopswitch/translations/desktopswitch_pt_BR.ts
+++ plugin-desktopswitch/translations/desktopswitch_pt_BR.ts
@@ -55,12 +55,12 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="72"/>
         <source>If checked, widget will be just an indicator</source>
-        <translation type="unfinished"></translation>
+        <translation>Se marcado, o widget será apenas um indicador</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="75"/>
         <source>Show only active desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Exibir somente a área de trabalho ativa</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="85"/>
