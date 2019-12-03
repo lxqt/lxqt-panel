69d69c525f33372828047d0ba1fedf4a5c1bb2e5
eltonfabricio10 <eltonfabricio10@gmail.com>
2019-12-03 12:46:26 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Tray)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-tray/pt_BR/

--- plugin-tray/translations/tray_pt_BR.ts
+++ plugin-tray/translations/tray_pt_BR.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="6"/>
         <source>System Tray Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurações da Bandeja do Sistema</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="12"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Geral</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="18"/>
         <source>Spacing between icons:</source>
-        <translation type="unfinished"></translation>
+        <translation>Espaçamento entre ícones:</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="25"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="32"/>
         <source>Sort icons by window class</source>
-        <translation type="unfinished"></translation>
+        <translation>Classificar ícones por classe de janela</translation>
     </message>
 </context>
 </TS>
