3751e812c0b71b3bd3ceaf7f61e82ce5f51fbf80
Yu Zhong <theyearthewas@gmail.com>
2019-12-12 07:22:05 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/zh_CN/

--- plugin-volume/translations/volume_zh_CN.ts
+++ plugin-volume/translations/volume_zh_CN.ts
@@ -89,7 +89,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="91"/>
         <source>Notify about volume changes with keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>使用键盘更改音量时显示通知</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="106"/>
