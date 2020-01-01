bf79b72f18ae530609a62ca16b68adf3ca95d801
Paolo Vigoroso <xpaolox@zoznam.sk>
2020-01-01 13:32:33 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Tray)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-tray/sk_SK/

--- plugin-tray/translations/tray_sk_SK.ts
+++ plugin-tray/translations/tray_sk_SK.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="6"/>
         <source>System Tray Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavenie oznamovacích oblastí panelu</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="12"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Všeobecné</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="18"/>
         <source>Spacing between icons:</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstup medzi ikonami:</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="25"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> Pixely</translation>
     </message>
     <message>
         <location filename="../lxqttrayconfiguration.ui" line="32"/>
         <source>Sort icons by window class</source>
-        <translation type="unfinished"></translation>
+        <translation>Riadiť ikony podľa triedy okna</translation>
     </message>
 </context>
 </TS>
