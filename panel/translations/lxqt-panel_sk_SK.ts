3307b3be6b8d0327c6832ae6495a3db856a9cb21
Paolo Vigoroso <xpaolox@zoznam.sk>
2020-01-01 12:33:36 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (76 of 76 strings)
    
    Translation: LXQt/LXQt Panel
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel/sk_SK/

--- panel/translations/lxqt-panel_sk_SK.ts
+++ panel/translations/lxqt-panel_sk_SK.ts
@@ -160,7 +160,7 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="327"/>
         <source>Visible thin margin for hidden panel</source>
-        <translation type="unfinished"></translation>
+        <translation>Viditeľný tenký okraj pre skrytý panel</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="337"/>
