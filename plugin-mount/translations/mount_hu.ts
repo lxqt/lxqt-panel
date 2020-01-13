8a879683444f9a1539ba031a712cdd9c59767899
Ács Zoltán <acszoltan111@gmail.com>
2020-01-13 15:42:29 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 92.9% (13 of 14 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Mount)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-mount/hu/

--- plugin-mount/translations/mount_hu.ts
+++ plugin-mount/translations/mount_hu.ts
@@ -24,7 +24,7 @@
     <message>
         <location filename="../configuration.ui" line="26"/>
         <source>When a device is connected:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ha egy eszköz csatlakoztatva van:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="40"/>
