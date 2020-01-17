b1e955f2d2c69a630c6c5cbe2763c6ecad5a8309
Ács Zoltán <acszoltan111@gmail.com>
2020-01-17 17:02:07 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 90.5% (19 of 21 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Volume)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-volume/hu/

--- plugin-volume/translations/volume_hu.ts
+++ plugin-volume/translations/volume_hu.ts
@@ -89,7 +89,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="91"/>
         <source>Notify about volume changes with keyboard</source>
-        <translation type="unfinished">Értesítsen amikor a billentyűzetről változtatom a hangerőt</translation>
+        <translation>Értesítsen amikor a billentyűzetről változtatom a hangerőt</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="106"/>
