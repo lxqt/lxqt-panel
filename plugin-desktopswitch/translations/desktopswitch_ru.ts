1b4978c66e98f22b761f9550f5032a9b9f6cea86
Golubev Alexander <fatzer2@gmail.com>
2020-01-19 23:33:52 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Desktopswitch)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-desktopswitch/ru/

--- plugin-desktopswitch/translations/desktopswitch_ru.ts
+++ plugin-desktopswitch/translations/desktopswitch_ru.ts
@@ -55,12 +55,12 @@
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="72"/>
         <source>If checked, widget will be just an indicator</source>
-        <translation type="unfinished"></translation>
+        <translation>Если отмечено, виджет будет работать как простой индикатор</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="75"/>
         <source>Show only active desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать только активный рабочий стол</translation>
     </message>
     <message>
         <location filename="../desktopswitchconfiguration.ui" line="85"/>
