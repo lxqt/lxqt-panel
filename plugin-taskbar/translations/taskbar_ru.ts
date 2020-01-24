2e9b732d645b0bc14d4595f79af8dd9a2c9a4192
Golubev Alexander <fatzer2@gmail.com>
2020-01-24 15:31:22 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (48 of 48 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Taskbar)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-taskbar/ru/

--- plugin-taskbar/translations/taskbar_ru.ts
+++ plugin-taskbar/translations/taskbar_ru.ts
@@ -197,12 +197,12 @@
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="86"/>
         <source>Mouse Wheel</source>
-        <translation type="unfinished"></translation>
+        <translation>Колёсико мыши</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="112"/>
         <source>Wheel Delta Threshold</source>
-        <translation type="unfinished"></translation>
+        <translation>Порог чувствительности колёсика мыши</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.ui" line="152"/>
@@ -237,17 +237,17 @@
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="48"/>
         <source>Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет действий</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="50"/>
         <source>Scroll up to raise, down to minimize</source>
-        <translation type="unfinished"></translation>
+        <translation>Прокрутка вверх разворачивает окно, вниз — сворачивает</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="51"/>
         <source>Scroll up to minimize, down to raise</source>
-        <translation type="unfinished"></translation>
+        <translation>Прокрутка вверх сворачивает окно, вниз — разворачивает</translation>
     </message>
     <message>
         <location filename="../lxqttaskbarconfiguration.cpp" line="53"/>
