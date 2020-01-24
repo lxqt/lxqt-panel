0ed58982f315632667a8f7efb1558097398382a0
Golubev Alexander <fatzer2@gmail.com>
2020-01-24 11:31:23 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (55 of 55 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Worldclock)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-worldclock/ru/

--- plugin-worldclock/translations/worldclock_ru.ts
+++ plugin-worldclock/translations/worldclock_ru.ts
@@ -148,7 +148,7 @@
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="535"/>
         <source>Show &amp;week number</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать номера недель</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="201"/>
