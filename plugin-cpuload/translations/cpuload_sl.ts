6c1020f4c2b9e0c92fa1f4ee071825c85236adbd
Nejc Bertoncelj <nbert@siol.net>
2020-01-26 14:39:35 +0000

    Translated using Weblate (Slovenian)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Cpuload)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-cpuload/sl/

--- plugin-cpuload/translations/cpuload_sl.ts
+++ plugin-cpuload/translations/cpuload_sl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../lxqtcpuload.cpp" line="127"/>
         <source>CPU load %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Obremenitev CPE %1%</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="14"/>
         <source>CPU Load Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavitve CPU-gradnika</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="20"/>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="35"/>
         <source>Show text</source>
-        <translation>Pokaži besedilo</translation>
+        <translation>Prikaži porabo v odstotkih</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="42"/>
@@ -44,7 +44,7 @@
     <message>
         <location filename="../lxqtcpuloadconfiguration.ui" line="81"/>
         <source>Bar width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Širina vrstice:</translation>
     </message>
     <message>
         <location filename="../lxqtcpuloadconfiguration.cpp" line="68"/>
