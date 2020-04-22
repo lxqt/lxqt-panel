401dce013a1d74546d735f978d936749f9b76164
Helix <HelixBot@users.noreply.github.com>
2020-04-22 19:10:13 +0200

    Translations update from Weblate (#1375)
    * Translated using Weblate (Hebrew)
    
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: LXQt/LXQt Panel (Plugin Directorymenu)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-panel-plugin-directorymenu/he/
    
    * Added translation using Weblate (Korean)
    
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Co-authored-by: joonyoung choi <okjych@gmail.com>

--- plugin-directorymenu/translations/directorymenu_ko.ts
+++ plugin-directorymenu/translations/directorymenu_ko.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="he">
+<TS version="2.1" language="ko">
 <context>
     <name>DirectoryMenu</name>
     <message>
         <location filename="../directorymenu.cpp" line="125"/>
         <source>Open</source>
-        <translation>פתיחה</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenu.cpp" line="129"/>
         <source>Open in terminal</source>
-        <translation>פתיחה במסוף</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -19,63 +19,63 @@
     <message>
         <location filename="../directorymenuconfiguration.ui" line="14"/>
         <source>Directory Menu Settings</source>
-        <translation>הגדרות תפריט תיקיות</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="20"/>
         <source>Appearance</source>
-        <translation>מראה</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="26"/>
         <source>Base directory:</source>
-        <translation>תיקיית בסיס:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="39"/>
         <source>BaseDirectoryName</source>
-        <translation>שם תיקיית בסיס</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="46"/>
         <source>Icon:</source>
-        <translation>סמל:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="66"/>
         <source>Terminal</source>
-        <translation>מסוף</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="73"/>
         <location filename="../directorymenuconfiguration.cpp" line="112"/>
         <source>Choose Default Terminal</source>
-        <translation>בחירת מסוף בררת המחדל</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="96"/>
         <source>Choose Base Directory</source>
-        <translation>בחירת תיקיית בסיס</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="126"/>
         <source>Choose Icon</source>
-        <translation>בחירת סמל</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="126"/>
         <source>Icons (*.png *.xpm *.jpg)</source>
-        <translation>סמלים ‎(*.png *.xpm *.jpg)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="135"/>
         <source>Directory Menu</source>
-        <translation>תפריט תיקיות</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="135"/>
         <source>An error occurred while loading the icon.</source>
-        <translation>אירעה שגיאה בעת טעינת הסמל.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
