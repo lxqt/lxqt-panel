7121993a098c6b6ab838bf6de0e5f951d4d8a90e
milotype <43657314+milotype@users.noreply.github.com>
2020-05-30 22:29:23 +0200

    Croatian translation new/update

--- plugin-spacer/translations/spacer_hr.ts
+++ plugin-spacer/translations/spacer_hr.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../spacerconfiguration.ui" line="14"/>
         <source>Spacer Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Razmaci – Postavke</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="20"/>
         <source>Space width:</source>
-        <translation type="unfinished">Duljina razmaka:</translation>
+        <translation>Širina razmaka:</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="40"/>
         <source>Space type:</source>
-        <translation type="unfinished">Vrsta razmaka:</translation>
+        <translation>Vrsta razmaka:</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="57"/>
         <source>fixed</source>
-        <translation type="unfinished"></translation>
+        <translation>fiksna</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.ui" line="71"/>
         <source>expandable</source>
-        <translation type="unfinished"></translation>
+        <translation>proširiva</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="34"/>
         <source>lined</source>
-        <translation type="unfinished"></translation>
+        <translation>crta</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="35"/>
         <source>dotted</source>
-        <translation type="unfinished"></translation>
+        <translation>točkast</translation>
     </message>
     <message>
         <location filename="../spacerconfiguration.cpp" line="36"/>
         <source>invisible</source>
-        <translation type="unfinished">nevidljiv</translation>
+        <translation>nevidljiv</translation>
     </message>
 </context>
 </TS>
