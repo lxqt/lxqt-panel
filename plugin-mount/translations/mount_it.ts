d70d53171083f50a5a018acf34a7c0ea569619b8
Stefonarch <steff@vogliaditerra.com>
2020-11-01 01:06:41 +0100

    Weblate commits
    Co-authored-by: Stefonarch <steff@vogliaditerra.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/it/
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-mount

--- plugin-mount/translations/mount_it.ts
+++ plugin-mount/translations/mount_it.ts
@@ -29,12 +29,12 @@
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Trigger the following action eject shortcut is pressed (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Esegui sempre la seguente azione alla pressione del pulsante di espulsione (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="45"/>
         <source>When eject button is pressed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Se pressato pulsante di espulsione:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="43"/>
@@ -55,7 +55,7 @@
     <message>
         <location filename="../configuration.cpp" line="51"/>
         <source>Eject All Optical Drives</source>
-        <translation type="unfinished"></translation>
+        <translation>Espelli tutti dispositivi ottici</translation>
     </message>
 </context>
 <context>
@@ -86,7 +86,7 @@
     <message>
         <location filename="../actions/ejectaction_optical.cpp" line="59"/>
         <source>Ejected all optical drives</source>
-        <translation type="unfinished"></translation>
+        <translation>Espelli tutti dischi ottici</translation>
     </message>
 </context>
 <context>
@@ -94,12 +94,12 @@
     <message>
         <location filename="../lxqtmountplugin.cpp" line="79"/>
         <source>Removable media/devices manager: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestore dispositivi rimovibili: Impossibile registrare scorciatoia globale &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../lxqtmountplugin.cpp" line="116"/>
         <source>Eject removable media</source>
-        <translation type="unfinished"></translation>
+        <translation>Espelli dispositivo rimovibile</translation>
     </message>
 </context>
 <context>
