c90b40b8d0212f4687e9b3732e5ffe137f8f23c7
Yaron Shahrabani <sh.yaron@gmail.com>
2020-11-09 08:06:13 +0100

    Weblate commits
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/he/
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-mount

--- plugin-mainmenu/translations/mainmenu_he.ts
+++ plugin-mainmenu/translations/mainmenu_he.ts
@@ -16,33 +16,34 @@
     <message>
         <location filename="../lxqtmainmenu.cpp" line="496"/>
         <source>Add to desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>הוספה לשולחן העבודה</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="504"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>שאלה</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="505"/>
         <source>A file with the same name already exists.
 Do you want to overwrite it?</source>
-        <translation type="unfinished"></translation>
+        <translation>כבר קיים קובץ בשם הזה.
+לשכתב אותו?</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="511"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>אזהרה</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="512"/>
         <source>The file cannot be overwritten.</source>
-        <translation type="unfinished"></translation>
+        <translation>לא ניתן לשכתב את הקובץ.</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="518"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>העתקה</translation>
     </message>
 </context>
 <context>
--- plugin-mount/translations/mount_he.ts
+++ plugin-mount/translations/mount_he.ts
@@ -29,12 +29,12 @@
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Trigger the following action eject shortcut is pressed (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;להריץ את הפעולה הבאה בלחיצה על כפתור השליפה (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; כבררת מחדל)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="45"/>
         <source>When eject button is pressed:</source>
-        <translation type="unfinished"></translation>
+        <translation>כאשר כפתור השליפה נלחץ:</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="43"/>
@@ -55,7 +55,7 @@
     <message>
         <location filename="../configuration.cpp" line="51"/>
         <source>Eject All Optical Drives</source>
-        <translation type="unfinished"></translation>
+        <translation>לשלוף את כל הכוננים האופטיים</translation>
     </message>
 </context>
 <context>
@@ -81,12 +81,12 @@
     <message>
         <location filename="../actions/ejectaction_optical.cpp" line="59"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">מנהל התקנים/מדיה נתיקים</translation>
+        <translation>מנהל התקנים/אמצעים נתיקים</translation>
     </message>
     <message>
         <location filename="../actions/ejectaction_optical.cpp" line="59"/>
         <source>Ejected all optical drives</source>
-        <translation type="unfinished"></translation>
+        <translation>שליפת כל הכוננים האופטיים</translation>
     </message>
 </context>
 <context>
@@ -94,12 +94,12 @@
     <message>
         <location filename="../lxqtmountplugin.cpp" line="79"/>
         <source>Removable media/devices manager: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="unfinished"></translation>
+        <translation>מנהל התקנים/אמצעים נתיקים: לא ניתן לרשום את קיצור הדרך הגלובלי ‚%1’</translation>
     </message>
     <message>
         <location filename="../lxqtmountplugin.cpp" line="116"/>
         <source>Eject removable media</source>
-        <translation type="unfinished"></translation>
+        <translation>שליפת אמצעי נתיק</translation>
     </message>
 </context>
 <context>
