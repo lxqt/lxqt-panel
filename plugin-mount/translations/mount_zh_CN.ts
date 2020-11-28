4b376b196107c755c2052eb023e4537cb4766d4f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-11-28 08:38:38 +0100

    Weblate commits (#1488)
    Co-authored-by: Kaijie Chen <chen@kaijie.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/zh_Hans/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat/zh_Hans/
    Translation: LXQt Panel/plugin-mount
    Translation: LXQt Panel/plugin-sysstat
    
    Co-authored-by: Kaijie Chen <chen@kaijie.org>

--- plugin-mount/translations/mount_zh_CN.ts
+++ plugin-mount/translations/mount_zh_CN.ts
@@ -29,12 +29,12 @@
     <message>
         <location filename="../configuration.ui" line="42"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Trigger the following action eject shortcut is pressed (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;按下弹出快捷键 (默认为 &lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt;) 触发以下操作&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../configuration.ui" line="45"/>
         <source>When eject button is pressed:</source>
-        <translation type="unfinished"></translation>
+        <translation>当弹出按钮被按下时：</translation>
     </message>
     <message>
         <location filename="../configuration.cpp" line="43"/>
@@ -55,7 +55,7 @@
     <message>
         <location filename="../configuration.cpp" line="51"/>
         <source>Eject All Optical Drives</source>
-        <translation type="unfinished"></translation>
+        <translation>弹出所有光盘驱动器</translation>
     </message>
 </context>
 <context>
@@ -81,12 +81,12 @@
     <message>
         <location filename="../actions/ejectaction_optical.cpp" line="59"/>
         <source>Removable media/devices manager</source>
-        <translation type="unfinished">可移动媒体管理器</translation>
+        <translation>可移动媒体/设备管理器</translation>
     </message>
     <message>
         <location filename="../actions/ejectaction_optical.cpp" line="59"/>
         <source>Ejected all optical drives</source>
-        <translation type="unfinished"></translation>
+        <translation>被弹出的所有光盘驱动器</translation>
     </message>
 </context>
 <context>
@@ -94,12 +94,12 @@
     <message>
         <location filename="../lxqtmountplugin.cpp" line="79"/>
         <source>Removable media/devices manager: Global shortcut &apos;%1&apos; cannot be registered</source>
-        <translation type="unfinished"></translation>
+        <translation>可移动媒体/设备管理器：全局快捷键 &apos;%1&apos; 无法被注册</translation>
     </message>
     <message>
         <location filename="../lxqtmountplugin.cpp" line="116"/>
         <source>Eject removable media</source>
-        <translation type="unfinished"></translation>
+        <translation>弹出可移动媒体</translation>
     </message>
 </context>
 <context>
