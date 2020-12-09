82ebdfd47d1bb2018eb10dfd9c4b2a5a81074b80
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-09 18:53:13 +0100

    Translations update from Weblate (#1505)
    * Weblate commits
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/pt_PT/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/pt/
    Translation: LXQt Panel/plugin-backlight Menu Entry
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-tray
    Translation: LXQt Panel/plugin-worldclock
    
    * Weblate commits
    
    Co-authored-by: Standreas <standreas@riseup.net>
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Standreas <standreas@riseup.net>

--- plugin-statusnotifier/translations/statusnotifier_pt.ts
+++ plugin-statusnotifier/translations/statusnotifier_pt.ts
@@ -6,53 +6,53 @@
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="14"/>
         <source>Status Notifier Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Definições do notificador de estado</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="28"/>
         <location filename="../statusnotifierconfiguration.ui" line="38"/>
         <source>An auto-hiding item will remain visible for this period if it needs attention.</source>
-        <translation type="unfinished"></translation>
+        <translation>Um item de ocultação automática permanecerá visível durante este período se necessitar de atenção.</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="31"/>
         <source>Attention period:</source>
-        <translation type="unfinished"></translation>
+        <translation>Período de atenção:</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="41"/>
         <source> minute(s)</source>
-        <translation type="unfinished"></translation>
+        <translation> minuto(s)</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="72"/>
         <source>Change visibility of items</source>
-        <translation type="unfinished"></translation>
+        <translation>Mudar a visibilidade dos itens</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="85"/>
         <source>Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Item</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="90"/>
         <source>Visibility</source>
-        <translation type="unfinished"></translation>
+        <translation>Visibilidade</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always show</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar sempre</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Auto-hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocultar automaticamente</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocultar sempre</translation>
     </message>
 </context>
 </TS>
