46f6aee2f9e4564c75ed5b666a50186d4d4ef9cc
Pyscowicz <pyskoovits@gmail.com>
2021-01-07 20:06:46 +0100

    Weblate commits
    Co-authored-by: Pyscowicz <pyskoovits@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/fi/
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-taskbar

--- plugin-mainmenu/translations/mainmenu_fi.ts
+++ plugin-mainmenu/translations/mainmenu_fi.ts
@@ -16,33 +16,34 @@
     <message>
         <location filename="../lxqtmainmenu.cpp" line="496"/>
         <source>Add to desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisää työpöydälle</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="504"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Kysymys</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="505"/>
         <source>A file with the same name already exists.
 Do you want to overwrite it?</source>
-        <translation type="unfinished"></translation>
+        <translation>Samanniminen tiedosto on jo olemassa.
+Haluatko korvata sen?</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="511"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Varoitus</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="512"/>
         <source>The file cannot be overwritten.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedostoa ei voida korvata.</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenu.cpp" line="518"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopioi</translation>
     </message>
 </context>
 <context>
