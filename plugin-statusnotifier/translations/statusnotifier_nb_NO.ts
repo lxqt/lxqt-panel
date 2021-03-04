66f180990d73947314f6fbb814d4db2210f73618
Allan Nordhøy <epost@anotheragency.no>
2021-03-04 06:07:12 +0100

    Weblate commits
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-kbindindicator/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/nb_NO/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-kbindicator
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-mount
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-sysstat
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-worldclock

--- plugin-statusnotifier/translations/statusnotifier_nb_NO.ts
+++ plugin-statusnotifier/translations/statusnotifier_nb_NO.ts
@@ -6,53 +6,53 @@
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="14"/>
         <source>Status Notifier Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Statusmerknadsinnstillinger</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="28"/>
         <location filename="../statusnotifierconfiguration.ui" line="38"/>
         <source>An auto-hiding item will remain visible for this period if it needs attention.</source>
-        <translation type="unfinished"></translation>
+        <translation>Et automatisk skjult element vil forbli synlig i denne tidsperioden hvis det trenger oppmerksomhet.</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="31"/>
         <source>Attention period:</source>
-        <translation type="unfinished"></translation>
+        <translation>Oppmerksomhetsperiode:</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="41"/>
         <source> minute(s)</source>
-        <translation type="unfinished"></translation>
+        <translation> minutt(er)</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="72"/>
         <source>Change visibility of items</source>
-        <translation type="unfinished"></translation>
+        <translation>Endre elementssynlighet</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="85"/>
         <source>Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Element</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="90"/>
         <source>Visibility</source>
-        <translation type="unfinished"></translation>
+        <translation>Synlighet</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always show</source>
-        <translation type="unfinished"></translation>
+        <translation>Alltid vis</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Auto-hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Auto-skjul</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Alltid skjul</translation>
     </message>
 </context>
 </TS>
