0c248aa99ea384dd3740c2b0072ed7b02b362c42
Baptiste Huchon <baptiste.huchon@orange.fr>
2021-03-04 06:07:12 +0100

    Weblate commits
    Co-authored-by: Baptiste Huchon <baptiste.huchon@orange.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray/fr/
    Translation: LXQt Panel/plugin-backlight (Name & Description)
    Translation: LXQt Panel/plugin-mount
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-tray

---
+++ plugin-statusnotifier/translations/statusnotifier_fr.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>StatusNotifierConfiguration</name>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="14"/>
+        <source>Status Notifier Settings</source>
+        <translation type="unfinished">Paramètres de notification d&apos;état</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="28"/>
+        <location filename="../statusnotifierconfiguration.ui" line="38"/>
+        <source>An auto-hiding item will remain visible for this period if it needs attention.</source>
+        <translation>Un élément à masquage automatique restera visible pendant cette période s&apos;il nécessite une attention particulière.</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="31"/>
+        <source>Attention period:</source>
+        <translation>Période d&apos;attention :</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="41"/>
+        <source> minute(s)</source>
+        <translation> minute(s)</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="72"/>
+        <source>Change visibility of items</source>
+        <translation>Modifier la visibilité des éléments</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="85"/>
+        <source>Item</source>
+        <translation>Élément</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="90"/>
+        <source>Visibility</source>
+        <translation>Visibilité</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Always show</source>
+        <translation>Toujours visible</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Auto-hide</source>
+        <translation>Masquer automatiquement</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Always hide</source>
+        <translation>Toujours caché</translation>
+    </message>
+</context>
+</TS>
