38a7bf529d421c8e8584f5474d310757384cb45f
Weblate <noreply@weblate.org>
2021-03-05 10:07:16 +0100

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch-name-and-description/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu-name-and-description/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-spacer/
    Translation: LXQt Panel/plugin-desktopswitch (Name & Description)
    Translation: LXQt Panel/plugin-mainmenu (Name & Description)
    Translation: LXQt Panel/plugin-spacer

---
+++ plugin-mount/translations/mount_si.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="si">
+<context>
+    <name>Button</name>
+    <message>
+        <location filename="../button.cpp" line="38"/>
+        <source>Removable media/devices manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Configuration</name>
+    <message>
+        <location filename="../configuration.ui" line="14"/>
+        <source>Removable Media Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="20"/>
+        <source>Behaviour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="32"/>
+        <source>When a device is connected:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Trigger the following action when eject shortcut is pressed (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="45"/>
+        <source>When eject button is pressed:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="43"/>
+        <source>Popup menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="44"/>
+        <source>Show info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="45"/>
+        <location filename="../configuration.cpp" line="50"/>
+        <source>Do nothing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="51"/>
+        <source>Eject All Optical Drives</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceActionInfo</name>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="40"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="45"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="50"/>
+        <source>Removable media/devices manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EjectActionOptical</name>
+    <message>
+        <location filename="../actions/ejectaction_optical.cpp" line="59"/>
+        <source>Removable media/devices manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../actions/ejectaction_optical.cpp" line="59"/>
+        <source>Ejected all optical drives</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LXQtMountPlugin</name>
+    <message>
+        <location filename="../lxqtmountplugin.cpp" line="79"/>
+        <source>Removable media/devices manager: Global shortcut &apos;%1&apos; cannot be registered</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lxqtmountplugin.cpp" line="116"/>
+        <source>Eject removable media</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuDiskItem</name>
+    <message>
+        <location filename="../menudiskitem.cpp" line="153"/>
+        <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menudiskitem.cpp" line="155"/>
+        <location filename="../menudiskitem.cpp" line="176"/>
+        <source>Removable media/devices manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menudiskitem.cpp" line="174"/>
+        <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Popup</name>
+    <message>
+        <location filename="../popup.cpp" line="68"/>
+        <source>No devices are available</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
