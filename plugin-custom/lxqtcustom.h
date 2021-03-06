9e68ad48d3856bbd47b173088b0a66c205e173d2
slidinghotdog <pexis@email.com>
2021-03-05 23:46:03 -0300

    Add Custom Plugin

---
+++ plugin-custom/lxqtcustom.h
@@ -0,0 +1,103 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXQt - a lightweight, Qt based, desktop toolset
+ * https://lxqt.org
+ *
+ * Copyright: 2021 LXQt team
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#ifndef LXQTCUSTOM_H
+#define LXQTCUSTOM_H
+
+#include "../panel/ilxqtpanelplugin.h"
+
+#include <QToolButton>
+#include <QTimer>
+#include <QProcess>
+#include <QPointer>
+#include <LXQt/RotatedWidget>
+
+class CustomButton;
+class LXQtCustomConfiguration;
+
+class LXQtCustom : public QObject, public ILXQtPanelPlugin
+{
+    Q_OBJECT
+public:
+    LXQtCustom(const ILXQtPanelPluginStartupInfo &startupInfo);
+    ~LXQtCustom();
+
+    virtual QWidget *widget();
+    virtual QString themeId() const { return QStringLiteral("Custom"); }
+    virtual ILXQtPanelPlugin::Flags flags() const { return PreferRightAlignment | HaveConfigDialog ; }
+    void realign();
+    QDialog *configureDialog();
+
+protected slots:
+    virtual void settingsChanged();
+
+private slots:
+    void handleClick();
+    void handleFinished(int exitCode, QProcess::ExitStatus exitStatus);
+    void updateButton();
+    void runCommand();
+    void runDetached(QString command);
+    void wheelScrolled(int yDelta);
+
+private:
+    QWidget *mMainWidget;
+    LXQt::RotatedWidget* mRotatedWidget;
+    CustomButton *mContent;
+    QPointer<LXQtCustomConfiguration> mConfigDialog;
+
+    QProcess *mProcess;
+    QTimer *mTimer;
+
+    bool mFirstRun;
+    QString mOutput;
+
+    bool mAutoRotate;
+    QString mFont;
+    QString mCommand;
+    bool mRunWithBash;
+    bool mRepeat;
+    int mRepeatTimer;
+    QString mIcon;
+    QString mText;
+    int mMaxWidth;
+    QString mClick;
+    QString mWheelUp;
+    QString mWheelDown;
+};
+
+
+class LXQtCustomPluginLibrary: public QObject, public ILXQtPanelPluginLibrary
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "lxqt.org/Panel/PluginInterface/3.0")
+    Q_INTERFACES(ILXQtPanelPluginLibrary)
+public:
+    ILXQtPanelPlugin *instance(const ILXQtPanelPluginStartupInfo &startupInfo) const
+    {
+        return new LXQtCustom(startupInfo);
+    }
+};
+
+#endif // LXQTCUSTOM_H
---
+++ plugin-custom/lxqtcustomconfiguration.h
@@ -0,0 +1,67 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXQt - a lightweight, Qt based, desktop toolset
+ * https://lxqt.org
+ *
+ * Copyright: 2021 LXQt team
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#ifndef LXQTCUSTOMCONFIGURATION_H
+#define LXQTCUSTOMCONFIGURATION_H
+
+#include "../panel/lxqtpanelpluginconfigdialog.h"
+#include "../panel/pluginsettings.h"
+
+namespace Ui {
+    class LXQtCustomConfiguration;
+}
+
+class LXQtCustomConfiguration : public LXQtPanelPluginConfigDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LXQtCustomConfiguration(PluginSettings *settings, QWidget *parent = nullptr);
+    ~LXQtCustomConfiguration();
+
+private slots:
+    void autoRotateChanged(bool autoRotate);
+    void fontButtonClicked();
+    void fontChanged(QString font);
+    void commandPlainTextEditChanged();
+    void runWithBashCheckBoxChanged(bool runWithBash);
+    void repeatGroupBoxChanged(bool repeat);
+    void repeatTimerSpinBoxChanged(int repeatTimer);
+    void iconLineEditChanged(QString icon);
+    void iconBrowseButtonClicked();
+    void textLineEditChanged(QString text);
+    void maxWidthSpinBoxChanged(int maxWidth);
+    void clickLineEditChanged(QString click);
+    void wheelUpLineEditChanged(QString wheelUp);
+    void wheelDownLineEditChanged(QString wheelDown);
+
+protected slots:
+    virtual void loadSettings();
+
+private:
+    Ui::LXQtCustomConfiguration *ui;
+};
+
+#endif // LXQTCUSTOMCONFIGURATION_H
