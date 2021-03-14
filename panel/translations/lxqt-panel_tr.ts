f3f9f65a04668d43904ad20a92aaa07504b0f63e
Hasan Yıldız <hasanyildiz0@yaani.com>
2021-03-14 23:07:01 +0100

    Weblate commits
    Co-authored-by: Hasan Yıldız <hasanyildiz0@yaani.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/tr/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-mount

--- panel/translations/lxqt-panel_tr.ts
+++ panel/translations/lxqt-panel_tr.ts
@@ -165,7 +165,7 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="334"/>
         <source>Hide only on overlapping a window</source>
-        <translation type="unfinished"></translation>
+        <translation>Yalnızca bir pencerenin üst üste binmesini gizle</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="344"/>
