5aebf5ae73c7fde9800bffcc91a2fa4f65a22a4f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-25 07:17:47 +0100

    Weblate commits (#1593)
    Co-authored-by: Einar Mostad <einar@mostad.eu>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/nb_NO/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-quicklaunch
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-worldclock
    
    Co-authored-by: Einar Mostad <einar@mostad.eu>

--- panel/translations/lxqt-panel_nb_NO.ts
+++ panel/translations/lxqt-panel_nb_NO.ts
@@ -136,7 +136,7 @@
     <message>
         <location filename="../config/configpanelwidget.ui" line="334"/>
         <source>Hide only on overlapping a window</source>
-        <translation type="unfinished">Skjul kun ved overlappende vindu</translation>
+        <translation>Skjul kun ved overlappende vindu</translation>
     </message>
     <message>
         <location filename="../config/configpanelwidget.ui" line="344"/>
