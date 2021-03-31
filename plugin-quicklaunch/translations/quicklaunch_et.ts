6fe9e4d804bd298d62a646dc379c3eced8d8092e
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-03-31 09:07:08 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-showdesktop/et/
    Translation: LXQt Panel/plugin-quicklaunch
    Translation: LXQt Panel/plugin-showdesktop

---
+++ plugin-quicklaunch/translations/quicklaunch_et.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQtQuickLaunch</name>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="229"/>
+        <source>Drop Error</source>
+        <translation>Viga kustutamisel</translation>
+    </message>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="230"/>
+        <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
+        <translation>Faili või urli „%1“ ei saa hetkel lisada kiirkäivitajasse</translation>
+    </message>
+    <message>
+        <location filename="../lxqtquicklaunch.cpp" line="336"/>
+        <source>Drop application
+icons here</source>
+        <translation>Lohista siia
+rakenduse ikoone</translation>
+    </message>
+</context>
+<context>
+    <name>QuickLaunchButton</name>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="55"/>
+        <source>(Ctrl + DND)</source>
+        <translation>(Ctrl + DND)</translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="57"/>
+        <source>Move left</source>
+        <translation>Liiguta vasakule</translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="60"/>
+        <source>Move right</source>
+        <translation>Liiguta paremale</translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="64"/>
+        <source>Remove from quicklaunch</source>
+        <translation>Eemalda kiirkäivitajast</translation>
+    </message>
+</context>
+</TS>
