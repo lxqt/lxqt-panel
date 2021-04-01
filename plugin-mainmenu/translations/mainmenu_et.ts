b99c9adc7cb099bd2f69c00b2d9777a73e660734
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-01 23:07:10 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/leave-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel-modul-name/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-spacer/et/
    Translation: LXQt Panel/Leave Menu (Category Name)
    Translation: LXQt Panel/Panel Modul Name
    Translation: LXQt Panel/plugin-backlight (Name & Description)
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-spacer

---
+++ plugin-mainmenu/translations/mainmenu_et.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQtMainMenu</name>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="106"/>
+        <source>Search...</source>
+        <translation>Otsi...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="114"/>
+        <source>Show/hide main menu</source>
+        <translation>Näita või peida põhimenüüd</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="496"/>
+        <source>Add to desktop</source>
+        <translation>Lisa töölauale</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="504"/>
+        <source>Question</source>
+        <translation>Küsimus</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="505"/>
+        <source>A file with the same name already exists.
+Do you want to overwrite it?</source>
+        <translation>Sellise nimega fail on juba olemas.
+Kas sa soovid seda üle kirjutada?</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="511"/>
+        <source>Warning</source>
+        <translation>Hoiatus</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="512"/>
+        <source>The file cannot be overwritten.</source>
+        <translation>Faili ülekirjutamine ei õnnestu.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenu.cpp" line="518"/>
+        <source>Copy</source>
+        <translation>Kopeeri</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtMainMenuConfiguration</name>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Üldist</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="14"/>
+        <source>Main Menu settings</source>
+        <translation>Põhimenüü seadistused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="35"/>
+        <source>Icon:</source>
+        <translation>Ikoon:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="59"/>
+        <source>Button text:</source>
+        <translation>Nupu tekst:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="76"/>
+        <source>Custom font size:</source>
+        <translation>Fondi suurus:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="86"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="105"/>
+        <source>Menu file</source>
+        <translation>Menüüfail</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="111"/>
+        <source>Menu file:</source>
+        <translation>Faili asukoht:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="156"/>
+        <source>Search</source>
+        <translation>Kust otsime</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="162"/>
+        <source>Max. item width:</source>
+        <translation>Tulemuste suurim laius:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="169"/>
+        <source>Show matching entries:</source>
+        <translation>Näita otsingutulemusi:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="183"/>
+        <source>Maximum visible items:</source>
+        <translation>Kirjete arv:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="190"/>
+        <source>Filter menu entries</source>
+        <translation>Filtreeri menüükirjeid</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="197"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="210"/>
+        <source>Hide menu entries while searching</source>
+        <translation>Otsingu ajal peida tulemused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="217"/>
+        <source>Clear search upon showing menu</source>
+        <translation>Põhimenüü uuesti näitamisel kustuta otsingutulemused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="127"/>
+        <source>Keyboard Shortcut</source>
+        <translation>Kiirklahv</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.ui" line="146"/>
+        <source>Click the button to record shortcut:</source>
+        <translation>Kiirklahvi salvestamiseks klõpsi nuppu:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="72"/>
+        <source>Reset</source>
+        <translation>Lähtesta</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="166"/>
+        <source>Choose icon file</source>
+        <translation>Vali ikooni fail</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="168"/>
+        <source>Images (*.svg *.png)</source>
+        <translation>Pildid (*.svg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="180"/>
+        <source>Choose menu file</source>
+        <translation>Vali põhimenüü fail</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmainmenuconfiguration.cpp" line="182"/>
+        <source>Menu files (*.menu)</source>
+        <translation>Menüüfailid (*.menu)</translation>
+    </message>
+</context>
+</TS>
