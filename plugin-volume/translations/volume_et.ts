4b9316fd4f10793812c647797b18cdd0c7061d1c
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-09 09:07:08 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-networkmanager-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-networkmonitor/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sensors-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/et/
    Translation: LXQt Panel/plugin-mainmenu (Name & Description)
    Translation: LXQt Panel/plugin-mount (Name & Description)
    Translation: LXQt Panel/plugin-networkmonitor
    Translation: LXQt Panel/plugin-networkmonitor (Name & Description)
    Translation: LXQt Panel/plugin-quicklaunch (Name & Description)
    Translation: LXQt Panel/plugin-sensors (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier (Name & Description)
    Translation: LXQt Panel/plugin-sysstat (Name & Description)
    Translation: LXQt Panel/plugin-taskbar (Name & Description)
    Translation: LXQt Panel/plugin-tray (Name & Description)
    Translation: LXQt Panel/plugin-volume
    Translation: LXQt Panel/plugin-volume (Name & Description)
    Translation: LXQt Panel/plugin-worldclock

---
+++ plugin-volume/translations/volume_et.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQtVolume</name>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="64"/>
+        <source>Increase sound volume</source>
+        <translation>Suurenda helivaljust</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="70"/>
+        <source>Decrease sound volume</source>
+        <translation>Vähenda helivaljust</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="76"/>
+        <source>Mute/unmute sound volume</source>
+        <translation>Summuta või taasta heli</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="137"/>
+        <source>Volume Control: The following shortcuts can not be registered: %1</source>
+        <translation>Helivaljuse haldus: Järgnevat kiirklahvi ei õnnestu salvestada: %1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="291"/>
+        <source>Volume: %1%%2</source>
+        <translation>Helivaljus: %1%%2</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolume.cpp" line="291"/>
+        <source>(muted)</source>
+        <translation>(summutatud)</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtVolumeConfiguration</name>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="14"/>
+        <source>Volume Control Settings</source>
+        <translation>Helivaljuse halduse seadistused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="20"/>
+        <source>Device to control</source>
+        <translation>Hallatav seade</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="28"/>
+        <source>ALSA</source>
+        <translation>ALSA</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="35"/>
+        <source>PulseAudio</source>
+        <translation>PulseAudio</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="42"/>
+        <source>OSS</source>
+        <translation>OSS</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="57"/>
+        <source>Behavior</source>
+        <translation>Tegevused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="63"/>
+        <source>Mute on middle click</source>
+        <translation>Hiire keskmise klahviga vajutamine summutab heli</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="70"/>
+        <source>Show on mouse click</source>
+        <translation>Mikseri vaade avaneb hiireklõpsuga</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="77"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation>Luba helivaljus üle 100% (0dB)</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="84"/>
+        <source>Always notify about volume changes</source>
+        <translation>Alati teavita helivaljuse muutustest</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="91"/>
+        <source>Notify about volume changes with keyboard</source>
+        <translation>Teavita helivaljuse muutmisest klaviatuurilt</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="106"/>
+        <source>Volume adjust step</source>
+        <translation>Helivaljuse muutmise samm</translation>
+    </message>
+    <message>
+        <location filename="../lxqtvolumeconfiguration.ui" line="125"/>
+        <source>External Mixer</source>
+        <translation>Võimalik väline helimikseri rakendus</translation>
+    </message>
+</context>
+<context>
+    <name>VolumePopup</name>
+    <message>
+        <location filename="../volumepopup.cpp" line="55"/>
+        <source>Launch mixer</source>
+        <translation>Käivita helimikser</translation>
+    </message>
+    <message>
+        <location filename="../volumepopup.cpp" line="56"/>
+        <source>Mi&amp;xer</source>
+        <translation>Mi&amp;kser</translation>
+    </message>
+</context>
+</TS>
