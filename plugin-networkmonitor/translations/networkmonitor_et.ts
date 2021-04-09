3979e17ceab31b3a081c4a2fc3f81c4191f4d592
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-09 12:30:32 +0200

    Weblate commits (#1598)
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-networkmanager-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-networkmonitor/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sensors-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/et/
    Translation: LXQt Panel/plugin-mainmenu (Name & Description)
    Translation: LXQt Panel/plugin-mount (Name & Description)
    Translation: LXQt Panel/plugin-networkmonitor
    Translation: LXQt Panel/plugin-networkmonitor (Name & Description)
    Translation: LXQt Panel/plugin-quicklaunch (Name & Description)
    Translation: LXQt Panel/plugin-sensors (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier (Name & Description)
    Translation: LXQt Panel/plugin-sysstat (Name & Description)
    Translation: LXQt Panel/plugin-taskbar (Name & Description)
    Translation: LXQt Panel/plugin-tray (Name & Description)
    Translation: LXQt Panel/plugin-volume
    Translation: LXQt Panel/plugin-volume (Name & Description)
    Translation: LXQt Panel/plugin-worldclock
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

---
+++ plugin-networkmonitor/translations/networkmonitor_et.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQtNetworkMonitor</name>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="159"/>
+        <source>Network interface &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Võrguliides &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="160"/>
+        <source>Transmitted %1</source>
+        <translation>Saadetud %1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="161"/>
+        <source>Received %1</source>
+        <translation>Vastu võetud %1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="206"/>
+        <source>B</source>
+        <translation>baiti</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="207"/>
+        <source>KiB</source>
+        <translation>kilobaiti</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="207"/>
+        <source>MiB</source>
+        <translation>megabaiti</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="207"/>
+        <source>GiB</source>
+        <translation>gigabaiti</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="207"/>
+        <source>TiB</source>
+        <translation>terabaiti</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitor.cpp" line="207"/>
+        <source>PiB</source>
+        <translation>petabaiti</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtNetworkMonitorConfiguration</name>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="14"/>
+        <source>Network Monitor settings</source>
+        <translation>Võrguhalduri seadistused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Põhiseadistused</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="35"/>
+        <source>Interface</source>
+        <translation>Võrguliides</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="56"/>
+        <source>Modem</source>
+        <translation>Modem</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="65"/>
+        <source>Monitor</source>
+        <translation>Monitor</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="74"/>
+        <source>Network</source>
+        <translation>Võrk</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="83"/>
+        <source>Wireless</source>
+        <translation>Traadita võrk</translation>
+    </message>
+    <message>
+        <location filename="../lxqtnetworkmonitorconfiguration.ui" line="95"/>
+        <source>Icon</source>
+        <translation>Ikoon</translation>
+    </message>
+</context>
+</TS>
