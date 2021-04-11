2c55e50cbe0adac68dd7e3027870881f4d0bb96b
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-12 00:07:38 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mount/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/et/
    Translation: LXQt Panel/plugin-mount
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-sysstat
    Translation: LXQt Panel/plugin-tray
    Translation: LXQt Panel/plugin-worldclock

---
+++ plugin-mount/translations/mount_et.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>Button</name>
+    <message>
+        <location filename="../button.cpp" line="38"/>
+        <source>Removable media/devices manager</source>
+        <translation>Eemaldatavate andmekandjate ja seadmete haldur</translation>
+    </message>
+</context>
+<context>
+    <name>Configuration</name>
+    <message>
+        <location filename="../configuration.ui" line="14"/>
+        <source>Removable Media Settings</source>
+        <translation>Eemaldatavate andmekandjate seadistused</translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="20"/>
+        <source>Behaviour</source>
+        <translation>Tegevused</translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="32"/>
+        <source>When a device is connected:</source>
+        <translation>Seadme ühendamisel:</translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Trigger the following action when eject shortcut is pressed (&lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt; by default)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Väljasta-nupu vajutamisel käivita järgnev toiming (vaikimisi &lt;span style=&quot; font-weight:600;&quot;&gt;XF86Eject&lt;/span&gt;)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../configuration.ui" line="45"/>
+        <source>When eject button is pressed:</source>
+        <translation>Väljasta-nupu vajutamisel:</translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="43"/>
+        <source>Popup menu</source>
+        <translation>Näita valikuakent</translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="44"/>
+        <source>Show info</source>
+        <translation>Näita teavet</translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="45"/>
+        <location filename="../configuration.cpp" line="50"/>
+        <source>Do nothing</source>
+        <translation>Ära tee midagi</translation>
+    </message>
+    <message>
+        <location filename="../configuration.cpp" line="51"/>
+        <source>Eject All Optical Drives</source>
+        <translation>Väljasta kõik optilised seadmed</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceActionInfo</name>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="40"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is connected.</source>
+        <translation>Seade &lt;b&gt;&lt;nobr&gt;„%1“&lt;/nobr&gt;&lt;/b&gt; on ühendatud.</translation>
+    </message>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="45"/>
+        <source>The device &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; is removed.</source>
+        <translation>Seade &lt;b&gt;&lt;nobr&gt;„%1“&lt;/nobr&gt;&lt;/b&gt; on eemaldatud.</translation>
+    </message>
+    <message>
+        <location filename="../actions/deviceaction_info.cpp" line="50"/>
+        <source>Removable media/devices manager</source>
+        <translation>Eemaldatavate andmekandjate ja seadete haldur</translation>
+    </message>
+</context>
+<context>
+    <name>EjectActionOptical</name>
+    <message>
+        <location filename="../actions/ejectaction_optical.cpp" line="59"/>
+        <source>Removable media/devices manager</source>
+        <translation>Eemaldatavate andmekandjate ja seadete haldur</translation>
+    </message>
+    <message>
+        <location filename="../actions/ejectaction_optical.cpp" line="59"/>
+        <source>Ejected all optical drives</source>
+        <translation>Väljastasin kõik plaadid</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtMountPlugin</name>
+    <message>
+        <location filename="../lxqtmountplugin.cpp" line="79"/>
+        <source>Removable media/devices manager: Global shortcut &apos;%1&apos; cannot be registered</source>
+        <translation>Eemaldatavate andmekandjate ja seadete haldur: üldist kiirklahvi „%1“ ei õnnestu salvestada</translation>
+    </message>
+    <message>
+        <location filename="../lxqtmountplugin.cpp" line="116"/>
+        <source>Eject removable media</source>
+        <translation>Eemalda andmekandja</translation>
+    </message>
+</context>
+<context>
+    <name>MenuDiskItem</name>
+    <message>
+        <location filename="../menudiskitem.cpp" line="153"/>
+        <source>Mounting of &lt;b&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/b&gt; failed: %2</source>
+        <translation>&lt;b&gt;&lt;nobr&gt;„%1“&lt;/nobr&gt;&lt;/b&gt; haakimine ei õnnestunud: %2</translation>
+    </message>
+    <message>
+        <location filename="../menudiskitem.cpp" line="155"/>
+        <location filename="../menudiskitem.cpp" line="176"/>
+        <source>Removable media/devices manager</source>
+        <translation>Eemaldatavate andmekandjate ja seadete haldur</translation>
+    </message>
+    <message>
+        <location filename="../menudiskitem.cpp" line="174"/>
+        <source>Unmounting of &lt;strong&gt;&lt;nobr&gt;&quot;%1&quot;&lt;/nobr&gt;&lt;/strong&gt; failed: %2</source>
+        <translation>&lt;b&gt;&lt;nobr&gt;„%1“&lt;/nobr&gt;&lt;/b&gt; lahutamine ei õnnestunud: %2</translation>
+    </message>
+</context>
+<context>
+    <name>Popup</name>
+    <message>
+        <location filename="../popup.cpp" line="68"/>
+        <source>No devices are available</source>
+        <translation>Seadmeid ei leidu</translation>
+    </message>
+</context>
+</TS>
