ad8301b76e737124a096b987ac96302fd7ca1aae
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-23 15:07:16 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/et/
    Translation: LXQt Panel/plugin-worldclock

--- plugin-worldclock/translations/worldclock_et.ts
+++ plugin-worldclock/translations/worldclock_et.ts
@@ -292,7 +292,41 @@
 &lt;tr&gt;&lt;td&gt;TTTTTT&lt;/td&gt;&lt;td&gt;the timezone custom name. You can change it the &apos;Time zones&apos; tab of the configuration window&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;
 &lt;br /&gt;&lt;b&gt;Notes:&lt;/b&gt; &lt;ul&gt;&lt;li&gt;Any characters in the pattern that are not in the ranges of [&apos;a&apos;..&apos;z&apos;] and [&apos;A&apos;..&apos;Z&apos;] will be treated as quoted text. For instance, characters like &apos;:&apos;, &apos;.&apos;, &apos; &apos;, &apos;#&apos; and &apos;@&apos; will appear in the resulting time text even they are not enclosed within single quotes. The single quote is used to &apos;escape&apos; letters. Two single quotes in a row, whether inside or outside a quoted sequence, represent a &apos;real&apos; single quote.&lt;/li&gt;&lt;li&gt;Minimal update interval is 1 second. If z or zzz is configured time is shown with the milliseconds fraction, but not updated on millisecond basis (avoiding big performance hit).&lt;/li&gt;&lt;ul&gt;
 </source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h1&gt;Aja ja kuupäeva kohandatud vorming&lt;/h1&gt;
+&lt;p&gt;Kuupäeva vorming on tähemärkide jada, kus konkreetsed tähemärgid asendatakse vormindamisel kalendri kuupäevade ja kellaaegade andmetega või mida kasutatakse kalendri jaoks andmete loomisel.&lt;/p&gt;
+&lt;p&gt;Allpool esitatud kuupäevavälja sümbolite tabel sisaldab märke, mida kasutatame, et näidata asjakohaseid vorminguid konkreetse lokaadi jaoks, näiteks yyyy aastaarvu jaoks. Märke võib kasutada mitu korda. Näiteks, kui aastaarvuks kasutatad y, võib &apos;yy&apos; anda tulemuseks &apos;99&apos;, samas kui &apos;yyyy&apos; annab tulemuseks &apos;1999&apos;. Enamiku numbriliste väljade puhul määrab tähemärkide arv välja laiuse. Näiteks kui h on tund, võib &apos;h&apos; anda tulemuseks &apos;5&apos;, kuid &apos;hh&apos; annab tulemuseks &apos;05&apos;. Mõne asenduse puhul määrab arv kindlaks, kas kasutame lühendatud või täielikku vormi, kuid võib olla ka muid valikuid, nagu on esitatud allpool.&lt;/p&gt;
+&lt;p&gt;Kaks ühekordset jutumärki tähistab sõna-sõnalist ühekordset jutumärki, mis on kas ühekordsete jutumärkide sees või väljaspool neid. Teksti ühekordsete jutumärkide sees ei tõlgendata kuidagi (välja arvatud kaks kõrvuti asetsevat ühekordset jutumärki). Muul juhul on kõik ASCII-tähed a-st kuni z-ni ja A-st kuni Z-ni reserveeritud süntaksimärkidena ja nõuavad jutumärke, kui nad kujutavad üheselt mõistetavaid märke. Lisaks võivad teatavad ASCII kirjavahemärgid muutuda tulevikus muutuvaks (nt &quot;:&quot; tõlgendatakse aja eraldajana ja &quot;/&quot; kuupäeva eraldajana ning asendatakse vastavate lokaadikohaste märkidega).&lt;br /&gt;&lt;/p&gt;
+&lt;table border=&quot;1&quot; width=&quot;100%&quot; cellpadding=&quot;4&quot; cellspacing=&quot;0&quot;&gt;
+&lt;tr&gt;&lt;th width=&quot;20%&quot;&gt;Code&lt;/th&gt;&lt;th&gt;Tähendus&lt;/th&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;d&lt;/td&gt;&lt;td&gt;päev numbrina ilma esimese nullita (1 kuni 31)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;dd&lt;/td&gt;&lt;td&gt;päev numbrina esimese nulliga (01 kuni 31)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ddd&lt;/td&gt;&lt;td&gt;päeva lühend sinu keeles (&apos;esm&apos; kuni &apos;püh&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;dddd&lt;/td&gt;&lt;td&gt;päeva nimi sinu keeles (&apos;esmaspäev&apos; kuni &apos;pühapäev&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;M&lt;/td&gt;&lt;td&gt;kuu numbrina ilma esimese nullita (1-12)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MM&lt;/td&gt;&lt;td&gt;kuu numbrina esimese nulliga (01-12)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MMM&lt;/td&gt;&lt;td&gt;kuu lühend sinu keeles (&apos;jaan&apos; kuni &apos;dets&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;MMMM&lt;/td&gt;&lt;td&gt;kuu nimi sinu keeles (&apos;jaanuar&apos; kuni &apos;detsember&apos;).&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;yy&lt;/td&gt;&lt;td&gt;aasta kahekohalise numbrina (00-99)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;yyyy&lt;/td&gt;&lt;td&gt;aasta neljakohalise numbrina&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;h&lt;/td&gt;&lt;td&gt;tund numbrina ilma esimese nullita (0 kuni 23 või 1 kuni 12, kui kasutad AM/PM esitusviisi)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;hh&lt;/td&gt;&lt;td&gt;tund numbrina esimese nulliga (00 kuni 23 või 01 kuni 12, kui kasutad AM/PM esitusviisi)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;H&lt;/td&gt;&lt;td&gt;tund numbrina ilma esimese nullita (0 kuni 23, sealhulgas AM/PM esitusviisil)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;HH&lt;/td&gt;&lt;td&gt;tund numbrina esimese nulliga (00 kuni 23, sealhulgas AM/PM esitusviisil)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;m&lt;/td&gt;&lt;td&gt;minut numbrina ilma esimese nullita (0 kuni 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;mm&lt;/td&gt;&lt;td&gt;minut numbrina esimese nulliga (00 kuni 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;s&lt;/td&gt;&lt;td&gt;sekund numbrina ilma esimese nullita (0 kuni 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ss&lt;/td&gt;&lt;td&gt;sekund numbrina esimese nulliga (00 kuni 59)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;AP &lt;i&gt;või&lt;/i&gt; A&lt;/td&gt;&lt;td&gt;kasuta AM/PM esitusviisi. &lt;b&gt;A/AP&lt;/b&gt; asendub vastavalt kas &quot;AM&quot; või &quot;PM&quot; märkega.&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ap &lt;i&gt;või&lt;/i&gt; a&lt;/td&gt;&lt;td&gt;kasuta am/pm esitusviisi. &lt;b&gt;a/ap&lt;/b&gt; asendub vastavalt kas &quot;am&quot; või &quot;pm&quot; märkega.&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;t&lt;/td&gt;&lt;td&gt;ajavöönd (näiteks &quot;EEST&quot;)&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;T&lt;/td&gt;&lt;td&gt;erinevus UTC/GMT ajast&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TT&lt;/td&gt;&lt;td&gt;ajavööndi IANA tunnus&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTT&lt;/td&gt;&lt;td&gt;ajavööndi lühend&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTT&lt;/td&gt;&lt;td&gt;ajavööndi lühike nimi&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTTT&lt;/td&gt;&lt;td&gt;ajavööndi pikk nimi&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;TTTTTT&lt;/td&gt;&lt;td&gt;ajavööndi kohandatud nimi, mida saad muuta kellaaja seadistuste vaates Ajavööndite kaardilt&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;
+&lt;br /&gt;&lt;b&gt;Notes:&lt;/b&gt; &lt;ul&gt;&lt;li&gt;Kõik märgid, mis ei kuulu vahemikku [&apos;a&apos;...z&apos;] ja [&apos;A&apos;...Z&apos;], loetakse tsiteeritud tekstiks. Näiteks sellised märgid nagu &apos;:&apos;, &apos;.&apos;, &apos; &apos;, &apos;#&apos; ja &apos;@&apos; ilmuvad tulemuseks olevas ajatekstis isegi siis, kui need ei ole ühekordsete jutumärkide vahel. Ühekordset jutumärki kasutatakse tähtede vältimiseks. Kaks ühekordset jutumärki järjest, nii jutumärkide jada sees kui ka väljaspool, kujutavad endast &quot;tõelist&quot; ühekordset jutumärki.&lt;/li&gt;&lt;li&gt;Andmeid uuendame kord sekundis. Kui sa juhtumisi kasutad z või zzz asendusi, siis aega kuvame millisekundi täpsusega, kuid uuendame seda vaid kord sekundis ja väldime sellega kaasnevat asjatut koormust sinu süsteemis.&lt;/li&gt;&lt;ul&gt;
+</translation>
     </message>
 </context>
 <context>
