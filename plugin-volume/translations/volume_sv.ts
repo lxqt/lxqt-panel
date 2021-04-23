6be6c79b72ad3e5cf1b4348b3dc989822f30bc78
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-23 15:35:33 +0200

    Translations update from Weblate (#1611)
    * Weblate commits
    
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/kk/
    Translation: LXQt Panel/plugin-mainmenu
    
    * Weblate commits
    
    Co-authored-by: Luna Jernberg <droidbittin@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume/sv/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-volume
    
    * Weblate commits
    
    Co-authored-by: bittin <bittin@reimu.nl>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/sv/
    Translation: LXQt Panel/Panel
    
    * Weblate commits
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/et/
    Translation: LXQt Panel/plugin-worldclock
    
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: Luna Jernberg <droidbittin@gmail.com>
    Co-authored-by: bittin <bittin@reimu.nl>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>

--- plugin-volume/translations/volume_sv.ts
+++ plugin-volume/translations/volume_sv.ts
@@ -89,7 +89,7 @@
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="91"/>
         <source>Notify about volume changes with keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Meddela om volymförändringar med tangentbordet</translation>
     </message>
     <message>
         <location filename="../lxqtvolumeconfiguration.ui" line="106"/>
