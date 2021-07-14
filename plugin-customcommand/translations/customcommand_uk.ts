adca1494a6f2a6ba90eb77ddbef468f3adcc6279
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-07-14 17:18:56 +0200

    Translations update from Weblate (#1636)
    * Weblate commits
    
    Co-authored-by: Standreas <standreas@riseup.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/it/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    
    * Weblate commits
    
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/nl/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    
    * Weblate commits
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/uk/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>

---
+++ plugin-customcommand/translations/customcommand_uk.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>LXQtCustomCommandConfiguration</name>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="14"/>
+        <source>Custom Command Configuration</source>
+        <translation>Конфігурування користувацьких команд</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="20"/>
+        <source>Display</source>
+        <translation>Дисплей</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="28"/>
+        <source>Use icon from theme or browse file</source>
+        <translation>Застосувати піктограму з теми або знайти файл</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="35"/>
+        <source>Run with &quot;bash -c&quot;</source>
+        <translation>Запустити з «bash -c»</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="45"/>
+        <source>Select Font</source>
+        <translation>Вибрати шрифт</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="55"/>
+        <source>Text</source>
+        <translation>Текст</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="65"/>
+        <source>Command</source>
+        <translation>Команда</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="75"/>
+        <source>Font</source>
+        <translation>Шрифт</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="82"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="101"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="104"/>
+        <source>Use %1 to display command output</source>
+        <translation>Застосуйте %1, щоб показати вивід команди</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="111"/>
+        <source>Icon</source>
+        <translation>Піктограма</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="121"/>
+        <source>Browse</source>
+        <translation>Огляд</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="131"/>
+        <source>Autorotate when the panel is vertical</source>
+        <translation>Автообертання, коли панель вертикальна</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="150"/>
+        <source>echo Configure...</source>
+        <translation>echo Конфігурація...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="153"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="271"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="278"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="285"/>
+        <source>Command to run</source>
+        <translation>Команда для запуску</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="160"/>
+        <source>Max Width</source>
+        <translation>Максимальна ширина</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="212"/>
+        <source>Repeat command after:</source>
+        <translation>Повторити команду через:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="228"/>
+        <source> second(s)</source>
+        <translation> секунд(у)</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="255"/>
+        <source>Mouse Commands</source>
+        <translation>Команди миші</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="261"/>
+        <source>Click</source>
+        <translation>Клацнути</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="292"/>
+        <source>Wheel Up</source>
+        <translation>Прокрутити коліщатко вгору</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="302"/>
+        <source>Wheel Down</source>
+        <translation>Прокрутити коліщатко вниз</translation>
+    </message>
+</context>
+</TS>
