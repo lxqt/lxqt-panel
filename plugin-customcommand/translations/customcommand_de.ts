b8892850c246d82530db179cde271cd1a2d82395
Roman Volak <romanvolak@web.de>
2021-08-18 11:07:56 +0200

    Weblate commits
    Co-authored-by: Roman Volak <romanvolak@web.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/de/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)

--- plugin-customcommand/translations/customcommand_de.ts
+++ plugin-customcommand/translations/customcommand_de.ts
@@ -6,72 +6,72 @@
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="14"/>
         <source>Custom Command Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellung der persönlichen Befehlszeile</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="20"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzeige</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="28"/>
         <source>Use icon from theme or browse file</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwende Symbolthema oder suche Datei</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="35"/>
         <source>Run with &quot;bash -c&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausführen mit &quot;bash -c&quot;</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="45"/>
         <source>Select Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Wähle Schriftart</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="55"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Text</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="65"/>
         <source>Command</source>
-        <translation type="unfinished"></translation>
+        <translation>Befehl</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="75"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Schriftart</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="82"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="101"/>
         <source>%1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="104"/>
         <source>Use %1 to display command output</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwende %1 zur Anzeige der Befehlszeile</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="111"/>
         <source>Icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Symbol</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="121"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Durchsuche</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="131"/>
         <source>Autorotate when the panel is vertical</source>
-        <translation type="unfinished"></translation>
+        <translation>Selbstdrehen bei vertikaler Anzeige</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="150"/>
