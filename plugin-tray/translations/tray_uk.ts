876cb697ac792c183b4c18e84f5b71534448ddc9
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-09 20:20:49 +0200

    Translations update from Weblate (#1659)
    * Weblate commits
    
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/leave-menu-entry/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-dom-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-kbindicator-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-spacer-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/settings-menu-entry/uk/
    Translation: LXQt Panel/LXQt Settings (Category Name)
    Translation: LXQt Panel/Leave Menu (Category Name)
    Translation: LXQt Panel/plugin-backlight (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu (Name & Description)
    Translation: LXQt Panel/plugin-dom (Name & Description)
    Translation: LXQt Panel/plugin-kbindicator (Name & Description)
    Translation: LXQt Panel/plugin-spacer (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-statusnotifier (Name & Description)
    Translation: LXQt Panel/plugin-sysstat
    Translation: LXQt Panel/plugin-sysstat (Name & Description)
    Translation: LXQt Panel/plugin-taskbar (Name & Description)
    Translation: LXQt Panel/plugin-tray
    Translation: LXQt Panel/plugin-worldclock (Name & Description)
    
    * Weblate commits
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/leave-menu-entry/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray/uk/
    Translation: LXQt Panel/Leave Menu (Category Name)
    Translation: LXQt Panel/plugin-backlight (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-tray
    
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>

---
+++ plugin-tray/translations/tray_uk.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>LXQtTrayConfiguration</name>
+    <message>
+        <location filename="../lxqttrayconfiguration.ui" line="6"/>
+        <source>System Tray Settings</source>
+        <translation>Налаштування системного лотка</translation>
+    </message>
+    <message>
+        <location filename="../lxqttrayconfiguration.ui" line="12"/>
+        <source>General</source>
+        <translation>Загальні</translation>
+    </message>
+    <message>
+        <location filename="../lxqttrayconfiguration.ui" line="18"/>
+        <source>Spacing between icons:</source>
+        <translation>Відстань між піктограмами:</translation>
+    </message>
+    <message>
+        <location filename="../lxqttrayconfiguration.ui" line="25"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../lxqttrayconfiguration.ui" line="32"/>
+        <source>Sort icons by window class</source>
+        <translation>Сортувати піктограми за класом вікна</translation>
+    </message>
+</context>
+</TS>
