132c9e803c0026d71c3f8f67824ada36d4a3fb41
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-11-27 02:05:20 +0100

    Weblate commit (#1685)
    Co-authored-by: KacperMisztal <kacper.misztal@o2.pl>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat-name-and-description/pl/
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-statusnotifier
    Translation: LXQt Panel/plugin-sysstat (Name & Description)

--- plugin-desktopswitch/translations/desktopswitch_pl.ts
+++ plugin-desktopswitch/translations/desktopswitch_pl.ts
@@ -70,7 +70,7 @@
     <message>
         <location filename="../desktopswitchconfiguration.cpp" line="69"/>
         <source>Desktop %1:</source>
-        <translation type="unfinished">Pulpit %1:</translation>
+        <translation>Pulpit %1:</translation>
     </message>
 </context>
 </TS>
--- plugin-statusnotifier/translations/statusnotifier_pl.ts
+++ plugin-statusnotifier/translations/statusnotifier_pl.ts
@@ -6,53 +6,53 @@
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="14"/>
         <source>Status Notifier Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia powiadomień o statusie</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="28"/>
         <location filename="../statusnotifierconfiguration.ui" line="38"/>
         <source>An auto-hiding item will remain visible for this period if it needs attention.</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatycznie ukrywany element pozostanie widoczny przez ten czas, jeśli wymaga uwagi.</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="31"/>
         <source>Attention period:</source>
-        <translation type="unfinished"></translation>
+        <translation>Okres uwagi:</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="41"/>
         <source> minute(s)</source>
-        <translation type="unfinished"></translation>
+        <translation> min</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="72"/>
         <source>Change visibility of items</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmień widoczność elementów</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="85"/>
         <source>Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Element</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.ui" line="90"/>
         <source>Visibility</source>
-        <translation type="unfinished"></translation>
+        <translation>Widoczność</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always show</source>
-        <translation type="unfinished"></translation>
+        <translation>Zawsze pokazuj</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Auto-hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukrywaj automatycznie</translation>
     </message>
     <message>
         <location filename="../statusnotifierconfiguration.cpp" line="84"/>
         <source>Always hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Zawsze ukrywaj</translation>
     </message>
 </context>
 </TS>
