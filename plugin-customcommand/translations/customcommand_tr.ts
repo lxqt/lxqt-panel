0b384bb7b3027448555e04fb63cab3fc6b79cbdd
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-02-04 13:40:32 +0100

    Weblate commit (#1713)
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/tr/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-customcommand

--- plugin-customcommand/translations/customcommand_tr.ts
+++ plugin-customcommand/translations/customcommand_tr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../lxqtcustomcommand.cpp" line="173"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Hata</translation>
     </message>
 </context>
 <context>
@@ -132,12 +132,12 @@
     <message>
         <location filename="../lxqtcustomcommandconfiguration.cpp" line="157"/>
         <source>Select Icon File</source>
-        <translation type="unfinished"></translation>
+        <translation>Simge Dosyasını Seçin</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.cpp" line="157"/>
         <source>Images (*.png *.svg *.xpm *.jpg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Resim (*.png *.svg *.xpm *.jpg)</translation>
     </message>
 </context>
 </TS>
