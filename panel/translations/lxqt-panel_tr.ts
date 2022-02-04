a300fd113e7f6dd86b10f5f04097b21732001dd4
Weblate <noreply@weblate.org>
2022-02-04 13:08:56 +0100

    Weblate commit
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/tr/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-customcommand

--- panel/translations/lxqt-panel_tr.ts
+++ panel/translations/lxqt-panel_tr.ts
@@ -39,12 +39,12 @@
     <message>
         <location filename="../config/configpaneldialog.cpp" line="39"/>
         <source>Placement</source>
-        <translation type="unfinished"></translation>
+        <translation>Yerleştirme</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="43"/>
         <source>Styling</source>
-        <translation type="unfinished"></translation>
+        <translation>Biçem</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="47"/>
@@ -57,7 +57,7 @@
     <message>
         <location filename="../config/configplacement.ui" line="20"/>
         <source>Configure placement</source>
-        <translation type="unfinished"></translation>
+        <translation>Yerleşimi yapılandır</translation>
     </message>
     <message>
         <location filename="../config/configplacement.ui" line="44"/>
@@ -295,7 +295,7 @@
     <message>
         <location filename="../config/configstyling.ui" line="20"/>
         <source>Configure Styling</source>
-        <translation type="unfinished"></translation>
+        <translation>Biçemi Yapılandır</translation>
     </message>
     <message>
         <location filename="../config/configstyling.ui" line="44"/>
