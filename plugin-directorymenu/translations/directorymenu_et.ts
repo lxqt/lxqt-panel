c612148b7ad4defca656e7872e684244c60d54a6
Weblate <noreply@weblate.org>
2022-03-20 22:46:21 +0100

    Weblate commit
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/nl/
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-colorpicker (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-taskbar

--- plugin-directorymenu/translations/directorymenu_et.ts
+++ plugin-directorymenu/translations/directorymenu_et.ts
@@ -44,12 +44,12 @@
     <message>
         <location filename="../directorymenuconfiguration.ui" line="66"/>
         <source>Label</source>
-        <translation type="unfinished"></translation>
+        <translation>Silt</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="86"/>
         <source>Button style</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuppude stiil</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.ui" line="103"/>
@@ -65,17 +65,17 @@
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="54"/>
         <source>Only icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Ainult ikoon</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="55"/>
         <source>Only text</source>
-        <translation type="unfinished"></translation>
+        <translation>Ainult tekst</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="56"/>
         <source>Icon and text</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikoon ja tekst</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="115"/>
@@ -90,17 +90,17 @@
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="164"/>
         <source>Icons (*.png *.xpm *.jpg *.svg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikoonid (*.png *.xpm *.jpg *.svg)</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="188"/>
         <source>Choose Label</source>
-        <translation type="unfinished"></translation>
+        <translation>Vali silt</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="189"/>
         <source>Label:</source>
-        <translation type="unfinished"></translation>
+        <translation>Silt:</translation>
     </message>
     <message>
         <location filename="../directorymenuconfiguration.cpp" line="173"/>
