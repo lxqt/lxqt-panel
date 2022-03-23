d4c1b9970a0ca269359b8be9d4d091f4513b3684
Weblate <noreply@weblate.org>
2022-03-23 12:09:23 +0100

    Weblate commit
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/tr/
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-colorpicker (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-taskbar

---
+++ plugin-colorpicker/translations/colorpicker_nl.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>ColorPickerWidget</name>
+    <message>
+        <location filename="../colorpicker.cpp" line="209"/>
+        <source>Clear list</source>
+        <translation>Lijst wissen</translation>
+    </message>
+    <message>
+        <location filename="../colorpicker.cpp" line="239"/>
+        <source>empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
