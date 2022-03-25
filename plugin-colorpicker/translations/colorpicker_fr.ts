bc0b8f7862c7135f0154322e5a9f4c4157862737
Weblate <noreply@weblate.org>
2022-03-25 19:09:13 +0100

    Weblate commit
    Co-authored-by: Baptiste H <baptiste.huchon@bechamail.fr>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/nl/
    Translation: LXQt Panel/plugin-colorpicker

--- plugin-colorpicker/translations/colorpicker_fr.ts
+++ plugin-colorpicker/translations/colorpicker_fr.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl">
+<TS version="2.1" language="fr">
 <context>
     <name>ColorPickerWidget</name>
     <message>
         <location filename="../colorpicker.cpp" line="209"/>
         <source>Clear list</source>
-        <translation>Lijst wissen</translation>
+        <translation>Effacer la liste</translation>
     </message>
     <message>
         <location filename="../colorpicker.cpp" line="239"/>
         <source>empty</source>
-        <translation>leeg</translation>
+        <translation>vide</translation>
     </message>
 </context>
 </TS>
