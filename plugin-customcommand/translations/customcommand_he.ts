e8f418ee15f930844e893c36e66e75f6bff0ac1e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-04-15 23:35:13 +0200

    Weblate commit (#1776)
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/he/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-colorpicker (Name & Description)
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-tray (Name & Description)

--- plugin-customcommand/translations/customcommand_he.ts
+++ plugin-customcommand/translations/customcommand_he.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../lxqtcustomcommand.cpp" line="176"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>שגיאה</translation>
     </message>
 </context>
 <context>
@@ -14,77 +14,77 @@
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="14"/>
         <source>Custom Command Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>הגדרות פקודה משלך</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="20"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>תצוגה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="28"/>
         <source>Use icon from theme or browse file</source>
-        <translation type="unfinished"></translation>
+        <translation>להשתמש בסמל מערכת העיצוב או לבחור קובץ</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="35"/>
         <source>Run with &quot;bash -c&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>הפעלה עם „bash -c”</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="45"/>
         <source>Select Font</source>
-        <translation type="unfinished"></translation>
+        <translation>בחירת גופן</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="55"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>טקסט</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="65"/>
         <source>Command</source>
-        <translation type="unfinished"></translation>
+        <translation>פקודה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="75"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>גופן</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="82"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> פיקסלים</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="101"/>
         <source>%1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="104"/>
         <source>Use %1 to display command output</source>
-        <translation type="unfinished"></translation>
+        <translation>להשתמש ב־%1 להצגת פלט הפקודה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="111"/>
         <source>Icon</source>
-        <translation type="unfinished"></translation>
+        <translation>סמל</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="121"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>עיון</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="131"/>
         <source>Autorotate when the panel is vertical</source>
-        <translation type="unfinished"></translation>
+        <translation>לסובב אוטומטית כאשר הלוח הוא אנכי</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="150"/>
         <source>echo Configure...</source>
-        <translation type="unfinished"></translation>
+        <translation>echo הגדרה…</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="153"/>
@@ -92,52 +92,52 @@
         <location filename="../lxqtcustomcommandconfiguration.ui" line="278"/>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="285"/>
         <source>Command to run</source>
-        <translation type="unfinished"></translation>
+        <translation>פקודה להרצה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="160"/>
         <source>Max Width</source>
-        <translation type="unfinished"></translation>
+        <translation>רוחב מרבי</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="212"/>
         <source>Repeat command after:</source>
-        <translation type="unfinished"></translation>
+        <translation>לחזור על הפקודה לאחר:</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="228"/>
         <source> second(s)</source>
-        <translation type="unfinished"></translation>
+        <translation> שניות</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="255"/>
         <source>Mouse Commands</source>
-        <translation type="unfinished"></translation>
+        <translation>פקודות עכבר</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="261"/>
         <source>Click</source>
-        <translation type="unfinished"></translation>
+        <translation>לחיצה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="292"/>
         <source>Wheel Up</source>
-        <translation type="unfinished"></translation>
+        <translation>גלגלת למעלה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="302"/>
         <source>Wheel Down</source>
-        <translation type="unfinished"></translation>
+        <translation>גלגלת למטה</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.cpp" line="139"/>
         <source>Select Icon File</source>
-        <translation type="unfinished"></translation>
+        <translation>בחירת קובץ סמל</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.cpp" line="139"/>
         <source>Images (*.png *.svg *.xpm *.jpg)</source>
-        <translation type="unfinished"></translation>
+        <translation>תמונות (‎*.png‏ ‎*.svg‏ ‎*.xpm‏ ‎*.jpg)</translation>
     </message>
 </context>
 </TS>
