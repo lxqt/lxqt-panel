04207d76a9de9b22f82ebafbd66e22b773660be5
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-07-23 17:22:15 +0200

    Weblate commit (#1797)
    Co-authored-by: Moo <hazap@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/lt/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-colorpicker (Name & Description)
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-taskbar

--- panel/translations/lxqt-panel_lt.ts
+++ panel/translations/lxqt-panel_lt.ts
@@ -39,12 +39,12 @@
     <message>
         <location filename="../config/configpaneldialog.cpp" line="39"/>
         <source>Placement</source>
-        <translation type="unfinished"></translation>
+        <translation>Išdėstymas</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="43"/>
         <source>Styling</source>
-        <translation type="unfinished"></translation>
+        <translation>Dizainas</translation>
     </message>
     <message>
         <location filename="../config/configpaneldialog.cpp" line="47"/>
@@ -57,7 +57,7 @@
     <message>
         <location filename="../config/configplacement.ui" line="20"/>
         <source>Configure placement</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfigūruoti išdėstymą</translation>
     </message>
     <message>
         <location filename="../config/configplacement.ui" line="44"/>
@@ -295,7 +295,7 @@
     <message>
         <location filename="../config/configstyling.ui" line="20"/>
         <source>Configure Styling</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfigūruoti dizainą</translation>
     </message>
     <message>
         <location filename="../config/configstyling.ui" line="44"/>
