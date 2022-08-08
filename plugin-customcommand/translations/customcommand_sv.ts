acc74950f331f39e362554bdc59ea8804bb56cea
Weblate <noreply@weblate.org>
2022-08-08 07:10:03 +0200

    Weblate commit
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Luna Jernberg <droidbittin@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/el/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-quicklaunch

--- plugin-customcommand/translations/customcommand_sv.ts
+++ plugin-customcommand/translations/customcommand_sv.ts
@@ -84,7 +84,7 @@
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="150"/>
         <source>echo Configure...</source>
-        <translation type="unfinished"></translation>
+        <translation>echo konfigurera...</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="153"/>
