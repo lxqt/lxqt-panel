1d61dff73031851da3e3add306dae03eac1e391d
Tsu Jan <tsujan2000@gmail.com>
2022-09-16 19:09:59 +0430

    Set window flags of volume popup explicitly
    It may make a difference under Wayland, with no change under X11.
    
    Closes https://github.com/lxqt/lxqt-panel/issues/1815

--- plugin-volume/volumepopup.cpp
+++ plugin-volume/volumepopup.cpp
@@ -49,6 +49,10 @@ VolumePopup::VolumePopup(QWidget* parent):
     m_anchor(Qt::TopLeftCorner),
     m_device(nullptr)
 {
+    // Under some Wayland compositors, setting window flags in the c-tor of the base class
+    // may not be enough for a correct positioning of the popup.
+    setWindowFlags(Qt::WindowStaysOnTopHint | Qt::CustomizeWindowHint | Qt::Popup | Qt::X11BypassWindowManagerHint);
+
     m_mixerButton = new QPushButton(this);
     m_mixerButton->setObjectName(QStringLiteral("MixerLink"));
     m_mixerButton->setMinimumWidth(1);
