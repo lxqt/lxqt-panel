ee860ab73b47bf7e8813f28d2282bc6095bc2ac7
Weblate <noreply@weblate.org>
2022-11-03 15:10:58 +0100

    Weblate commit
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-networkmanager-name-and-description/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/ja/
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-networkmonitor (Name & Description)
    Translation: LXQt Panel/plugin-quicklaunch
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-tray (Name & Description)

--- plugin-taskbar/translations/taskbar_ja.ts
+++ plugin-taskbar/translations/taskbar_ja.ts
@@ -39,7 +39,7 @@
     <message>
         <location filename="../lxqttaskbutton.cpp" line="738"/>
         <source>Move To N&amp;ext Monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>次のモニターへ移動(&amp;E)</translation>
     </message>
     <message>
         <location filename="../lxqttaskbutton.cpp" line="741"/>
