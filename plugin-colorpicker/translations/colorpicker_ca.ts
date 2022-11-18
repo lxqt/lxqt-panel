0fff04d09b42b637b0f5c2b2eb77944407b95426
Weblate <noreply@weblate.org>
2022-11-18 09:10:43 +0100

    Weblate commit
    Co-authored-by: Carl Andersen <carl@flying-cloud.dk>
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker-name-and-description/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/ca/
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-colorpicker (Name & Description)
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-quicklaunch
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-tray (Name & Description)
    Translation: LXQt Panel/plugin-volume
    Translation: LXQt Panel/plugin-worldclock

---
+++ plugin-colorpicker/translations/colorpicker_ca.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>ColorPickerWidget</name>
+    <message>
+        <location filename="../colorpicker.cpp" line="209"/>
+        <source>Clear list</source>
+        <translation>Neteja la llista</translation>
+    </message>
+    <message>
+        <location filename="../colorpicker.cpp" line="239"/>
+        <source>empty</source>
+        <translation>buit</translation>
+    </message>
+</context>
+</TS>
