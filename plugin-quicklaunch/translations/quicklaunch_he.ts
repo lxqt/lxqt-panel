721ca6e77687fdb1be8402584bc945ca1e3ccda1
Weblate <noreply@weblate.org>
2022-12-01 15:10:46 +0100

    Weblate commit
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-quicklaunch/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/ca/
    Translation: LXQt Panel/plugin-quicklaunch
    Translation: LXQt Panel/plugin-worldclock

--- plugin-quicklaunch/translations/quicklaunch_he.ts
+++ plugin-quicklaunch/translations/quicklaunch_he.ts
@@ -46,7 +46,7 @@ icons here</source>
     <message>
         <location filename="../quicklaunchbutton.cpp" line="72"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>רענון</translation>
     </message>
 </context>
 </TS>
