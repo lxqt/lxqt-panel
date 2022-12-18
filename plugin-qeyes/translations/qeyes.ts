2b6cc63dcc3112d39bbd55a6faf78b2c37f51e7a
Goffredo Baroncelli <kreijack@inwind.it>
2022-12-18 21:11:14 +0100

    Add qeyes.ts file

---
+++ plugin-qeyes/translations/qeyes.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QEyesConfigDialog</name>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="44"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="50"/>
+        <source>Number of eyes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="56"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="60"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="65"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="69"/>
+        <source>QEyes setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="98"/>
+        <source>QEyes default</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
