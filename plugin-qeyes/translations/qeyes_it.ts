2d50cdc999f480d55786fb161db28378c34beec9
Weblate <noreply@weblate.org>
2022-12-24 09:10:52 +0100

    Weblate commit
    Co-authored-by: Baptiste Huchon <baptiste.huchon@ikmail.com>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/it/
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)

---
+++ plugin-qeyes/translations/qeyes_it.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>QEyesConfigDialog</name>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="44"/>
+        <source>Appearance</source>
+        <translation>Aspetto</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="50"/>
+        <source>Number of eyes</source>
+        <translation>Numeri di occhi</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="56"/>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="60"/>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="65"/>
+        <source>Reset</source>
+        <translation>Ripristina</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="69"/>
+        <source>QEyes setting</source>
+        <translation>Impostazioni QEyes</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="98"/>
+        <source>QEyes default</source>
+        <translation>QEyes predefinito</translation>
+    </message>
+</context>
+</TS>
