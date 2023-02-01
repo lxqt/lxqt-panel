a8580adf4ef87a42d3477cd8faed5b453f4cff12
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-02-01 20:30:01 +0800

    Weblate commit (#1861)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-dom-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-kbindicator-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-spacer-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-sysstat-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-volume-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock-name-and-description/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/tr/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-directorymenu (Name & Description)
    Translation: LXQt Panel/plugin-dom (Name & Description)
    Translation: LXQt Panel/plugin-kbindicator (Name & Description)
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)
    Translation: LXQt Panel/plugin-spacer (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier (Name & Description)
    Translation: LXQt Panel/plugin-sysstat (Name & Description)
    Translation: LXQt Panel/plugin-volume (Name & Description)
    Translation: LXQt Panel/plugin-worldclock
    Translation: LXQt Panel/plugin-worldclock (Name & Description)
    
    Co-authored-by: Sabri Ünal <libreajans@gmail.com>

---
+++ plugin-colorpicker/translations/colorpicker_tr.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>ColorPickerWidget</name>
+    <message>
+        <location filename="../colorpicker.cpp" line="209"/>
+        <source>Clear list</source>
+        <translation>Listeyi temizle</translation>
+    </message>
+    <message>
+        <location filename="../colorpicker.cpp" line="239"/>
+        <source>empty</source>
+        <translation>boş</translation>
+    </message>
+</context>
+</TS>
---
+++ plugin-qeyes/translations/qeyes_tr.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>QEyesConfigDialog</name>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="44"/>
+        <source>Appearance</source>
+        <translation>Görünüm</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="50"/>
+        <source>Number of eyes</source>
+        <translation>Göz sayısı</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="56"/>
+        <source>Type</source>
+        <translation>Tür</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="60"/>
+        <source>Close</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="65"/>
+        <source>Reset</source>
+        <translation>Sıfırla</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="69"/>
+        <source>QEyes setting</source>
+        <translation>QEyes ayarı</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="98"/>
+        <source>QEyes default</source>
+        <translation>QEyes öntanımlı</translation>
+    </message>
+</context>
+</TS>
