e43b750ad5a0ae1f08c3e68160cb1399100c75a3
Chih-Hsuan Yen <1937689+yan12125@users.noreply.github.com>
2023-05-13 23:18:52 +0800

    Add lxqt-menu-data CMake dependency (#1892)
    * Add lxqt-menu-data CMake dependency
    
    * Update deps for CI
    
    Also updates the link to PKGBUILD - I don't maintain it in [archlinuxcn] anymore.

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,8 +2,8 @@ set -ex
 
 source shared-ci/prepare-archlinux.sh
 
-# See *depends in https://github.com/archlinuxcn/repo/blob/master/archlinuxcn/lxqt-panel-git/PKGBUILD
-pacman -S --noconfirm --needed git cmake qt5-tools lxqt-build-tools-git alsa-lib libpulse lm_sensors libstatgrab libsysstat-git solid menu-cache libxcomposite lxmenu-data libdbusmenu-qt5 lxqt-globalkeys-git libxtst
+# See *depends in https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=lxqt-panel-git
+pacman -S --noconfirm --needed git cmake qt5-tools lxqt-build-tools-git alsa-lib libpulse lm_sensors libstatgrab libsysstat-git solid menu-cache libxcomposite lxqt-menu-data-git libdbusmenu-qt5 lxqt-globalkeys-git libxtst
 
 cmake -B build -S .
 make -C build
