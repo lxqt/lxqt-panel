8eec3ce538c147676a5fd864c61f9aad8181d940
Weblate <noreply@weblate.org>
2023-08-05 03:41:20 +0200

    Weblate commit
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/nl/
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)

---
+++ plugin-qeyes/translations/qeyes_he.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he">
+<context>
+    <name>QEyesConfigDialog</name>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="44"/>
+        <source>Appearance</source>
+        <translation>מראה</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="50"/>
+        <source>Number of eyes</source>
+        <translation>מספר העיניים</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="56"/>
+        <source>Type</source>
+        <translation>סוג</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="60"/>
+        <source>Close</source>
+        <translation>סגירה</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="65"/>
+        <source>Reset</source>
+        <translation>איפוס</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="69"/>
+        <source>QEyes setting</source>
+        <translation>הגדרות Qעיניים</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="98"/>
+        <source>QEyes default</source>
+        <translation>ברירת מחדל ל־Qעיניים</translation>
+    </message>
+</context>
+</TS>
