543d3001f6354e166f2328b8f8442ad518c8ca7e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-01-12 17:39:54 +0100

    Weblate commit (#1978)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-tray-name-and-description/lg/
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-taskbar (Name & Description)
    Translation: LXQt Panel/plugin-tray (Name & Description)
    
    Co-authored-by: Sensalire <kbirabwa@yahoo.co.uk>

---
+++ plugin-taskbar/translations/taskbar_lg.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>LXQtTaskBar</name>
+    <message>
+        <location filename="../lxqttaskbar.cpp" line="681"/>
+        <source>Activate task %1</source>
+        <translation>Zuukusa omulimu gwa %1</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskButton</name>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="674"/>
+        <source>Application</source>
+        <translation>Puloguramu</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="708"/>
+        <source>To &amp;Desktop</source>
+        <translation>Kyusa &amp;awakolerwa kwe lirabikira</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="710"/>
+        <source>&amp;All Desktops</source>
+        <translation>Lirabikire ku wakolerwa &amp;zonna</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="720"/>
+        <source>Desktop &amp;%1</source>
+        <translation>Awakolerwa namba &amp;%1</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="730"/>
+        <source>&amp;To Current Desktop</source>
+        <translation>Lirabikire &amp;wano</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="739"/>
+        <source>Move To N&amp;ext Monitor</source>
+        <translation>Lig&amp;ende ku lutimbe oluddako</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="742"/>
+        <source>Move To &amp;Previous Monitor</source>
+        <translation>Ligende ku lutimbe ol&amp;ukulembera kweliri kakano</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="746"/>
+        <source>&amp;Move</source>
+        <translation>Ddirisa &amp;lijulule</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="749"/>
+        <source>Resi&amp;ze</source>
+        <translation>K&amp;yusa bunene bw&apos;eddirisa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="756"/>
+        <source>Ma&amp;ximize</source>
+        <translation>Ddirisa li&amp;bune olutimbe</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="763"/>
+        <source>Maximize vertically</source>
+        <translation>Obugulumivu bw&apos;eddirisa bwenkane obw&apos;olutimbe</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="768"/>
+        <source>Maximize horizontally</source>
+        <translation>Obugazi bw&apos;eddirisa bwenkane obw&apos;olutimbe</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="774"/>
+        <source>&amp;Restore</source>
+        <translation>&amp;Komyawo ddirisa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="778"/>
+        <source>Mi&amp;nimize</source>
+        <translation>Eddirisa lizi&amp;ngeko</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="784"/>
+        <source>Roll down</source>
+        <translation>Komyawo akatimbe ku mutwe gw&apos;eddirisa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="790"/>
+        <source>Roll up</source>
+        <translation>Ddirisa lirabikeko mutwe gwokka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="798"/>
+        <source>&amp;Layer</source>
+        <translation>&amp;Ensoosawaza</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="800"/>
+        <source>Always on &amp;top</source>
+        <translation>Lis&amp;ookenga ku bwanjo</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="806"/>
+        <source>&amp;Normal</source>
+        <translation>&amp;Teriba na nkizo</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="812"/>
+        <source>Always on &amp;bottom</source>
+        <translation>Lise&amp;mbenga mabega</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbutton.cpp" line="820"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Gala ddirisa</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskGroup</name>
+    <message>
+        <location filename="../lxqttaskgroup.cpp" line="82"/>
+        <source>Group</source>
+        <translation>Ttuluba</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskgroup.cpp" line="84"/>
+        <source>Close group</source>
+        <translation>Gala ettuluba lyonna</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskbarConfiguration</name>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="14"/>
+        <source>Task Manager Settings</source>
+        <translation>Enteekateeka za kiteekateekamirimu</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Enkola ey&apos;awamu</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="43"/>
+        <source>Show only windows from desktop</source>
+        <translation>Amadirisa ge kiba kiraganga</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="55"/>
+        <source>Show only windows from &amp;panel&apos;s screen</source>
+        <translation>Kiragenga agali ku lutimbe awali olu&amp;baawo gokka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="62"/>
+        <source>Show only minimized windows</source>
+        <translation>Kiragenga amadirisa agazingidwako gokka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="69"/>
+        <source>Raise minimized windows on current desktop</source>
+        <translation>Amadirisa agakomezebwawo galabikerenga w&apos;okolera</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="76"/>
+        <source>Close on middle-click</source>
+        <translation>Eppeesa ery&apos;okukasongesebwa erya mu makkati lye liggalanga ddirisa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="49"/>
+        <source>Cycle windows on wheel scrolling</source>
+        <translation>Okunyoola namuziga kikyuse ddirisa eriri kungulu</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="143"/>
+        <source>Window &amp;grouping</source>
+        <translation>Amadirisa agaleetedwa puloguramu y&apos;emu ga&amp;funzibwenga awamu</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="152"/>
+        <source>Show popup on mouse hover</source>
+        <translation>Akasonga bwe kagwa ku kapande k&apos;amadirisa agafunzidwa wabonekengawo olukalala lwa go</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="169"/>
+        <source>Appearance</source>
+        <translation>Endabika</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="178"/>
+        <source>Button style</source>
+        <translation>Endabika ya bupande</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="188"/>
+        <source>Maximum button width</source>
+        <translation>Ekkomo ly&apos;obugazi bw&apos;obupande</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="125"/>
+        <location filename="../lxqttaskbarconfiguration.ui" line="201"/>
+        <location filename="../lxqttaskbarconfiguration.ui" line="227"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="86"/>
+        <source>Mouse Wheel</source>
+        <translation>Enkola ya namuziga ya ku kasongesebwa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="112"/>
+        <source>Wheel Delta Threshold</source>
+        <translation>Ebbanga namuziga ly&apos;emala okwetoolola ebalibwe nga enyooledwa</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="162"/>
+        <source>Put buttons of the same class next to each other</source>
+        <translation>Obupande obuleetedwa puluguramu y&apos;emu buliraanaganenga</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="214"/>
+        <source>Maximum button height</source>
+        <translation>Kkomo ly&apos;obugulumivu bw&apos;obupande</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="240"/>
+        <source>Auto&amp;rotate buttons when the panel is vertical</source>
+        <translation>Obupande b&amp;wetereezanga olubaawo nga lwesimbye</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.ui" line="247"/>
+        <source>Use icons by WindowClass, if available</source>
+        <translation>Obufaananyi obw&apos;oku bupande bubenga bwa WindowClass buli lwe kisoboka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="44"/>
+        <source>Icon and text</source>
+        <translation>Bubengako kafaananayi n&apos;ebigambo</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="45"/>
+        <source>Only icon</source>
+        <translation>Bubengako kafaananayi kokka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="46"/>
+        <source>Only text</source>
+        <translation>Bubengako bigambo byokka</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="48"/>
+        <source>Disabled</source>
+        <translation>Ereme okukola</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="50"/>
+        <source>Scroll up to raise, down to minimize</source>
+        <translation>Bw&apos;oginyoolera ku ddyo kireeta ddirisa, ku kkono kirizingako</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="51"/>
+        <source>Scroll up to minimize, down to raise</source>
+        <translation>Bw&apos;oginyoolera ku ddyo eddirisa kirizingako, ku kkono kirikomyawo</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="52"/>
+        <source>Scroll up to move to next desktop, down to previous</source>
+        <translation>Bw&apos;oginyoolera ku ddyo ogenda awakolerwa awaddako, ku kkono odda awakulembera w&apos;oli</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="53"/>
+        <source>Scroll up to move to previous desktop, down to next</source>
+        <translation>Bw&apos;oginyoolera ku ddyo odda awakolerwa awakulembera w&apos;oli, ku kkono ogenda awaddako</translation>
+    </message>
+    <message>
+        <location filename="../lxqttaskbarconfiguration.cpp" line="55"/>
+        <source>Current</source>
+        <translation>Agali w&apos;okolera kakano gokka</translation>
+    </message>
+</context>
+</TS>
