3b38495b6417af0ef9cae46a2b9aa627b988ceb0
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-01-22 08:13:34 +0100

    Weblate commit (#1992)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-dom-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-dom/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu-name-and-description/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu-name-and-description/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu/pl/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-customcommand (Name & Description)
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-desktopswitch (Name & Description)
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-directorymenu (Name & Description)
    Translation: LXQt Panel/plugin-dom
    Translation: LXQt Panel/plugin-dom (Name & Description)
    Translation: LXQt Panel/plugin-fancymenu
    Translation: LXQt Panel/plugin-fancymenu (Name & Description)
    
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Sensalire <kbirabwa@yahoo.co.uk>
    Co-authored-by: Standreas <standreas@riseup.net>

---
+++ plugin-customcommand/translations/customcommand_lg.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>LXQtCustomCommand</name>
+    <message>
+        <location filename="../lxqtcustomcommand.cpp" line="186"/>
+        <source>Error</source>
+        <translation>Kiremya</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtCustomCommandConfiguration</name>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="14"/>
+        <source>Custom Command Configuration</source>
+        <translation>Enteekateeka ez&apos;akalaga ebiva mu kiragiro kikyo</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="20"/>
+        <source>Display</source>
+        <translation>Endabika</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="181"/>
+        <source>Use icon from theme or browse file</source>
+        <translation>Tegeka wano fayiro ey&apos;akafaananyi gy&apos;esangibwa</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="188"/>
+        <source>Run with &quot;bash -c&quot;</source>
+        <translation>Ekiragiro kitandikise &quot;bash -c&quot;</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="198"/>
+        <source>Select Font</source>
+        <translation>Londa enkula y&apos;ennukuta</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="218"/>
+        <source>Text</source>
+        <translation>Ekiwandikibwa ku ppeesa</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="135"/>
+        <source>Command</source>
+        <translation>Ebifa ku kiragiro</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="128"/>
+        <source>Font</source>
+        <translation>Nkula y&apos;ennukuta</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="162"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="208"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="211"/>
+        <source>Use %1 to display command output</source>
+        <translation>Kozesa %1 okulaga ebiva mu kiragiro</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="145"/>
+        <source>Icon</source>
+        <translation>Kafaananyi</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="108"/>
+        <source>Browse</source>
+        <translation>Noonya</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="155"/>
+        <source>Autorotate when the panel is vertical</source>
+        <translation>Eppeesa lyetereeze okujja ku lubaawo bwe lubeera lwa mu busimba</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="240"/>
+        <source>echo Configure...</source>
+        <translation>wandika Teekateeka...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="243"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="278"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="285"/>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="292"/>
+        <source>Command to run</source>
+        <translation>Ekiragiro ekibeera kitandikibwa</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="118"/>
+        <source>Max Width</source>
+        <translation>Kkomo ly&apos;obugazi bw&apos;eppeesa</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="70"/>
+        <source>Repeat command after:</source>
+        <translation>Ekiragiro kiddengamu oluvannyuma lw&apos;ekiseera kino:</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="86"/>
+        <source> second(s)</source>
+        <translation> sikonda</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="250"/>
+        <source>Command outputs an image</source>
+        <translation>Ekiragiro kivaamu ekifaananyi</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="262"/>
+        <source>Mouse Commands</source>
+        <translation>Ebiragiro ebitandikibwa akasongesebwa</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="268"/>
+        <source>Click</source>
+        <translation>Bw&apos;onyiga eppeesa lyako</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="299"/>
+        <source>Wheel Up</source>
+        <translation>Namuziga ya ko bw&apos;oginyoolera ku kkono</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.ui" line="309"/>
+        <source>Wheel Down</source>
+        <translation>Namuziga ya ko bw&apos;oginyoolera ku ddyo</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.cpp" line="147"/>
+        <source>Select Icon File</source>
+        <translation>Londa fayiro ey&apos;akafaananyi</translation>
+    </message>
+    <message>
+        <location filename="../lxqtcustomcommandconfiguration.cpp" line="147"/>
+        <source>Images (*.png *.svg *.xpm *.jpg)</source>
+        <translation>Bifaananyi (*.png *.svg *.xpm *.jpg)</translation>
+    </message>
+</context>
+</TS>
---
+++ plugin-desktopswitch/translations/desktopswitch_lg.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>DesktopSwitch</name>
+    <message>
+        <location filename="../desktopswitch.cpp" line="88"/>
+        <source>Switch to desktop %1</source>
+        <translation>Genda ku awakolerwa %1</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitch.cpp" line="149"/>
+        <location filename="../desktopswitch.cpp" line="160"/>
+        <source>Desktop %1</source>
+        <translation>Awakolerwa %1</translation>
+    </message>
+</context>
+<context>
+    <name>DesktopSwitchConfiguration</name>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="14"/>
+        <source>DesktopSwitch settings</source>
+        <translation>Enteekateeka za kakyusa awakolerwa</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="46"/>
+        <source>Number of rows:</source>
+        <translation>Ennyiriri z&apos;amapeesa agakyusa awakolerwa:</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="39"/>
+        <source>Desktop labels:</source>
+        <translation>Ekibaalambanga awakolerwa:</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="20"/>
+        <source>Appearance</source>
+        <translation>Endabika</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="54"/>
+        <source>Numbers</source>
+        <translation>Namba</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="59"/>
+        <source>Names</source>
+        <translation>Bigambo</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="64"/>
+        <source>None</source>
+        <translation>Bwereere</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="72"/>
+        <source>If checked, widget will be just an indicator</source>
+        <translation>Bw&apos;otonyeza wano wajjakubaawo akalagabulazi awakolerwa kw&apos;oli</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="75"/>
+        <source>Show only active desktop</source>
+        <translation>Laga awakolerwa kw&apos;okolera wokka</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.ui" line="85"/>
+        <source>Desktop names</source>
+        <translation>Entuuma y&apos;awakolerwa</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitchconfiguration.cpp" line="71"/>
+        <source>Desktop %1:</source>
+        <translation>Awakolerwa %1:</translation>
+    </message>
+</context>
+<context>
+    <name>DesktopSwitchUnsupported</name>
+    <message>
+        <location filename="../desktopswitch.h" line="111"/>
+        <source>n/a</source>
+        <translation>Awakolerwa tewakyusibwa</translation>
+    </message>
+    <message>
+        <location filename="../desktopswitch.h" line="113"/>
+        <source>DesktopSwitch is unsupported on current platform: %1</source>
+        <translation>Ku sisitemu eno tekisoboka okyusa awakolerwa: %1</translation>
+    </message>
+</context>
+</TS>
