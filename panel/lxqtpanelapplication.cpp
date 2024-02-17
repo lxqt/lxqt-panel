84366c2fd9854e039e33caf15a0467098376e0e9
Filippo Gentile <filippogentile@disroot.org>
2024-02-17 19:18:09 +0100

    TaskBar: add experimental KWin Wayland backend
    NOTE: works only on KWin
    
    - Choose backend at runtime
    - Windows filter logic is re-evaluated on window property changes

--- panel/lxqtpanelapplication.cpp
+++ panel/lxqtpanelapplication.cpp
@@ -40,11 +40,14 @@
 
 #include "backends/lxqttaskbardummybackend.h"
 #include "backends/xcb/lxqttaskbarbackend_x11.h"
+#include "backends/wayland/lxqttaskbarbackendwayland.h"
 
 ILXQtTaskbarAbstractBackend *createWMBackend()
 {
     if(qGuiApp->nativeInterface<QNativeInterface::QX11Application>())
         return new LXQtTaskbarX11Backend;
+    else if(qGuiApp->nativeInterface<QNativeInterface::QWaylandApplication>())
+        return new LXQtTaskbarWaylandBackend;
 
     qWarning() << "\n"
                << "ERROR: Could not create a backend for window managment operations.\n"
