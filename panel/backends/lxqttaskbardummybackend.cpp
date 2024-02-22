a7a6e5dc66f41518fbc377a28c8d78147ac121bc
Filippo Gentile <filippogentile@disroot.org>
2024-02-22 20:32:50 +0100

    ILXQtTaskbarAbstractBackend: add dummy implementation
    This will be used to avoid crashing panel in case no backend could be
    created.
    A warning message will be printed in this case.

---
+++ panel/backends/lxqttaskbardummybackend.cpp
@@ -0,0 +1,156 @@
+#include "lxqttaskbardummybackend.h"
+
+#include <QIcon>
+
+LXQtTaskBarDummyBackend::LXQtTaskBarDummyBackend(QObject *parent)
+    : ILXQtTaskbarAbstractBackend(parent)
+{
+
+}
+
+
+/************************************************
+ *   Windows function
+ ************************************************/
+bool LXQtTaskBarDummyBackend::supportsAction(WId, LXQtTaskBarBackendAction) const
+{
+    return false;
+}
+
+bool LXQtTaskBarDummyBackend::reloadWindows()
+{
+    return false;
+}
+
+QVector<WId> LXQtTaskBarDummyBackend::getCurrentWindows() const
+{
+    return {};
+}
+
+QString LXQtTaskBarDummyBackend::getWindowTitle(WId) const
+{
+    return QString();
+}
+
+bool LXQtTaskBarDummyBackend::applicationDemandsAttention(WId) const
+{
+    return false;
+}
+
+QIcon LXQtTaskBarDummyBackend::getApplicationIcon(WId, int) const
+{
+    return QIcon();
+}
+
+QString LXQtTaskBarDummyBackend::getWindowClass(WId) const
+{
+    return QString();
+}
+
+LXQtTaskBarWindowLayer LXQtTaskBarDummyBackend::getWindowLayer(WId) const
+{
+    return LXQtTaskBarWindowLayer::Normal;
+}
+
+bool LXQtTaskBarDummyBackend::setWindowLayer(WId, LXQtTaskBarWindowLayer)
+{
+    return false;
+}
+
+LXQtTaskBarWindowState LXQtTaskBarDummyBackend::getWindowState(WId) const
+{
+    return LXQtTaskBarWindowState::Normal;
+}
+
+bool LXQtTaskBarDummyBackend::setWindowState(WId, LXQtTaskBarWindowState, bool)
+{
+    return false;
+}
+
+bool LXQtTaskBarDummyBackend::isWindowActive(WId) const
+{
+    return false;
+}
+
+bool LXQtTaskBarDummyBackend::raiseWindow(WId, bool)
+{
+    return false;
+}
+
+bool LXQtTaskBarDummyBackend::closeWindow(WId)
+{
+    return false;
+}
+
+WId LXQtTaskBarDummyBackend::getActiveWindow() const
+{
+    return 0;
+}
+
+
+/************************************************
+ *   Workspaces
+ ************************************************/
+int LXQtTaskBarDummyBackend::getWorkspacesCount() const
+{
+    return 1; // Fake 1 workspace
+}
+
+QString LXQtTaskBarDummyBackend::getWorkspaceName(int) const
+{
+    return QString();
+}
+
+int LXQtTaskBarDummyBackend::getCurrentWorkspace() const
+{
+    return 0;
+}
+
+bool LXQtTaskBarDummyBackend::setCurrentWorkspace(int)
+{
+    return false;
+}
+
+int LXQtTaskBarDummyBackend::getWindowWorkspace(WId) const
+{
+    return 0;
+}
+
+bool LXQtTaskBarDummyBackend::setWindowOnWorkspace(WId, int)
+{
+    return false;
+}
+
+void LXQtTaskBarDummyBackend::moveApplicationToPrevNextMonitor(WId, bool, bool)
+{
+    //No-op
+}
+
+bool LXQtTaskBarDummyBackend::isWindowOnScreen(QScreen *, WId) const
+{
+    return false;
+}
+
+/************************************************
+ *   X11 Specific
+ ************************************************/
+void LXQtTaskBarDummyBackend::moveApplication(WId)
+{
+    //No-op
+}
+
+void LXQtTaskBarDummyBackend::resizeApplication(WId)
+{
+    //No-op
+}
+
+void LXQtTaskBarDummyBackend::refreshIconGeometry(WId, QRect const &)
+{
+    //No-op
+}
+
+bool LXQtTaskBarDummyBackend::isAreaOverlapped(const QRect &) const
+{
+    return false;
+}
+
