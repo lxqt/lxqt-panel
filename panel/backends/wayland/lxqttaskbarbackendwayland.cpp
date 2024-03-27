9f72a3b0b317345e703d98b7d9be7b99eeff90a0
Marcus Britanicus <marcusbritanicus@gmail.com>
2024-03-27 15:13:47 +0530

    Allow both wlroots and plasma backends to coexist

---
+++ panel/backends/wayland/lxqttaskbarbackendwayland.cpp
@@ -0,0 +1,219 @@
+#include "lxqttaskbarbackendwayland.h"
+#include "plasma/lxqttaskbarbackendplasma.h"
+#include "wlroots/lxqttaskbarbackendwlr.h"
+
+#include <QIcon>
+
+LXQtTaskbarWaylandBackend::LXQtTaskbarWaylandBackend(QObject *parent)
+    : ILXQtTaskbarAbstractBackend(parent)
+{
+    /**
+     * HACK: For now, we will simply concentrate on plasma and wlroots.
+     * Eventually, we should be dynamically loading different plugins.
+     */
+
+    /**
+     * Let's get our DE:
+     * If plasma/kde is in desktopsList, we'll use the plasma backend.
+     * If wayfire/sway/labwc/hyprland/wlroots is in desktopsList, we'll use the wlroots backend.
+     */
+    QList<QByteArray> desktopsList = qgetenv("XDG_CURRENT_DESKTOP").toLower().split(':');
+    qDebug() << "--------------> Current desktop" << desktopsList;
+    for( QByteArray desktop: desktopsList ) {
+        if ( desktop == "plasma" || desktop == "kde" )
+        {
+            qDebug() << "--------------> Using plasma backend";
+            m_backend = new LXQtTaskbarPlasmaBackend();
+            break;
+        }
+
+        else if ( desktop == "wayfire" )
+        {
+            qDebug() << "--------------> Using wlroots backend";
+            m_backend = new LXQtTaskbarWlrootsBackend();
+            break;
+        }
+
+        else if ( desktop == "sway" )
+        {
+            qDebug() << "--------------> Using wlroots backend";
+            m_backend = new LXQtTaskbarWlrootsBackend();
+            break;
+        }
+
+        else if ( desktop == "labwc" )
+        {
+            qDebug() << "--------------> Using wlroots backend";
+            m_backend = new LXQtTaskbarWlrootsBackend();
+            break;
+        }
+
+        else if ( desktop == "hyprland" )
+        {
+            qDebug() << "--------------> Using wlroots backend";
+            m_backend = new LXQtTaskbarWlrootsBackend();
+            break;
+        }
+
+        else
+        {
+            // m_backend = nullptr;
+        }
+    }
+}
+
+
+/************************************************
+ *   Windows function
+ ************************************************/
+bool LXQtTaskbarWaylandBackend::supportsAction(WId id, LXQtTaskBarBackendAction act) const
+{
+    return ( m_backend == nullptr ? false : m_backend->supportsAction(id, act) );
+}
+
+bool LXQtTaskbarWaylandBackend::reloadWindows()
+{
+    return ( m_backend == nullptr ? false : m_backend->reloadWindows() );
+}
+
+QVector<WId> LXQtTaskbarWaylandBackend::getCurrentWindows() const
+{
+    return ( m_backend == nullptr ? QVector<WId>() : m_backend->getCurrentWindows() );
+}
+
+QString LXQtTaskbarWaylandBackend::getWindowTitle(WId id) const
+{
+    return ( m_backend == nullptr ? QString() : m_backend->getWindowTitle(id) );
+}
+
+bool LXQtTaskbarWaylandBackend::applicationDemandsAttention(WId id) const
+{
+    return ( m_backend == nullptr ? false : m_backend->applicationDemandsAttention(id) );
+}
+
+QIcon LXQtTaskbarWaylandBackend::getApplicationIcon(WId id, int fallbackDevicePixels) const
+{
+    return ( m_backend == nullptr ? QIcon() : m_backend->getApplicationIcon(id, fallbackDevicePixels) );
+}
+
+QString LXQtTaskbarWaylandBackend::getWindowClass(WId id) const
+{
+    return ( m_backend == nullptr ? QString() : m_backend->getWindowClass(id) );
+}
+
+LXQtTaskBarWindowLayer LXQtTaskbarWaylandBackend::getWindowLayer(WId id) const
+{
+    return ( m_backend == nullptr ? LXQtTaskBarWindowLayer::Normal : m_backend->getWindowLayer(id) );
+}
+
+bool LXQtTaskbarWaylandBackend::setWindowLayer(WId id, LXQtTaskBarWindowLayer lyr)
+{
+    return ( m_backend == nullptr ? false : m_backend->setWindowLayer(id, lyr) );
+}
+
+LXQtTaskBarWindowState LXQtTaskbarWaylandBackend::getWindowState(WId id) const
+{
+    return ( m_backend == nullptr ? LXQtTaskBarWindowState::Normal : m_backend->getWindowState(id) );
+}
+
+bool LXQtTaskbarWaylandBackend::setWindowState(WId id, LXQtTaskBarWindowState state, bool set)
+{
+    return ( m_backend == nullptr ? false : m_backend->setWindowState(id, state, set) );
+}
+
+bool LXQtTaskbarWaylandBackend::isWindowActive(WId id) const
+{
+    return ( m_backend == nullptr ? false : m_backend->isWindowActive(id) );
+}
+
+bool LXQtTaskbarWaylandBackend::raiseWindow(WId id, bool yes)
+{
+    return ( m_backend == nullptr ? false : m_backend->raiseWindow(id, yes) );
+}
+
+bool LXQtTaskbarWaylandBackend::closeWindow(WId id)
+{
+    return ( m_backend == nullptr ? false : m_backend->closeWindow(id) );
+}
+
+WId LXQtTaskbarWaylandBackend::getActiveWindow() const
+{
+    return ( m_backend == nullptr ? 0 : m_backend->getActiveWindow() );
+}
+
+
+/************************************************
+ *   Workspaces
+ ************************************************/
+int LXQtTaskbarWaylandBackend::getWorkspacesCount() const
+{
+    return ( m_backend == nullptr ? 1 : m_backend->getWorkspacesCount() ); // Fake 1 workspace
+}
+
+QString LXQtTaskbarWaylandBackend::getWorkspaceName(int ws) const
+{
+    return ( m_backend == nullptr ? QString() : m_backend->getWorkspaceName( ws ) );
+}
+
+int LXQtTaskbarWaylandBackend::getCurrentWorkspace() const
+{
+    return ( m_backend == nullptr ? 0 : m_backend->getCurrentWorkspace() );
+}
+
+bool LXQtTaskbarWaylandBackend::setCurrentWorkspace(int ws)
+{
+    return ( m_backend == nullptr ? false : m_backend->setCurrentWorkspace(ws) );
+}
+
+int LXQtTaskbarWaylandBackend::getWindowWorkspace(WId id) const
+{
+    return ( m_backend == nullptr ? 0 : m_backend->getWindowWorkspace( id ) );
+}
+
+bool LXQtTaskbarWaylandBackend::setWindowOnWorkspace(WId id, int ws)
+{
+    return ( m_backend == nullptr ? false : m_backend->setWindowOnWorkspace(id, ws) );
+}
+
+void LXQtTaskbarWaylandBackend::moveApplicationToPrevNextMonitor(WId id, bool next, bool raise)
+{
+    ( m_backend == nullptr ? void() : m_backend->moveApplicationToPrevNextMonitor( id, next, raise) );
+}
+
+bool LXQtTaskbarWaylandBackend::isWindowOnScreen(QScreen *scrn, WId id) const
+{
+    return ( m_backend == nullptr ? false : m_backend->isWindowOnScreen(scrn, id) );
+}
+
+/************************************************
+ *   X11 Specific
+ ************************************************/
+void LXQtTaskbarWaylandBackend::moveApplication(WId id)
+{
+    ( m_backend == nullptr ? void() : m_backend->moveApplication(id) );
+}
+
+void LXQtTaskbarWaylandBackend::resizeApplication(WId id)
+{
+    ( m_backend == nullptr ? void() : m_backend->resizeApplication(id) );
+}
+
+void LXQtTaskbarWaylandBackend::refreshIconGeometry(WId id, QRect const &rect)
+{
+    ( m_backend == nullptr ? void() : m_backend->refreshIconGeometry(id, rect) );
+}
+
+bool LXQtTaskbarWaylandBackend::isAreaOverlapped(const QRect &rect) const
+{
+    return ( m_backend == nullptr ? false : m_backend->isAreaOverlapped(rect) );
+}
+
+bool LXQtTaskbarWaylandBackend::isShowingDesktop() const
+{
+    return ( m_backend == nullptr ? false : m_backend->isShowingDesktop() );
+}
+
+bool LXQtTaskbarWaylandBackend::showDesktop(bool yes)
+{
+    return ( m_backend == nullptr ? false : m_backend->showDesktop(yes) );
+}
---
+++ panel/backends/wayland/lxqttaskbarbackendwayland.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "../ilxqttaskbarabstractbackend.h"
+
+class LXQtTaskbarWaylandBackend : public ILXQtTaskbarAbstractBackend
+{
+    Q_OBJECT
+
+public:
+    explicit LXQtTaskbarWaylandBackend(QObject *parent = nullptr);
+
+    // Backend
+    bool supportsAction(WId windowId, LXQtTaskBarBackendAction action) const override;
+
+    // Windows
+    bool reloadWindows() override;
+
+    QVector<WId> getCurrentWindows() const override;
+
+    QString getWindowTitle(WId windowId) const override;
+
+    bool applicationDemandsAttention(WId windowId) const override;
+
+    QIcon getApplicationIcon(WId windowId, int fallbackDevicePixels) const override;
+
+    QString getWindowClass(WId windowId) const override;
+
+    LXQtTaskBarWindowLayer getWindowLayer(WId windowId) const override;
+    bool setWindowLayer(WId windowId, LXQtTaskBarWindowLayer layer) override;
+
+    LXQtTaskBarWindowState getWindowState(WId windowId) const override;
+    bool setWindowState(WId windowId, LXQtTaskBarWindowState state, bool set = true) override;
+
+    bool isWindowActive(WId windowId) const override;
+    bool raiseWindow(WId windowId, bool onCurrentWorkSpace) override;
+
+    bool closeWindow(WId windowId) override;
+
+    WId getActiveWindow() const override;
+
+    // Workspaces
+    int getWorkspacesCount() const override;
+    QString getWorkspaceName(int idx) const override;
+
+    int getCurrentWorkspace() const override;
+    bool setCurrentWorkspace(int idx) override;
+
+    int getWindowWorkspace(WId windowId) const override;
+    bool setWindowOnWorkspace(WId windowId, int idx) override;
+
+    void moveApplicationToPrevNextMonitor(WId windowId, bool next, bool raiseOnCurrentDesktop) override;
+
+    bool isWindowOnScreen(QScreen *screen, WId windowId) const override;
+
+    // X11 Specific
+    void moveApplication(WId windowId) override;
+    void resizeApplication(WId windowId) override;
+
+    void refreshIconGeometry(WId windowId, const QRect &geom) override;
+
+    // Panel internal
+    bool isAreaOverlapped(const QRect& area) const override;
+
+    // Show Destop
+    bool isShowingDesktop() const override;
+    bool showDesktop(bool value) override;
+
+private:
+    ILXQtTaskbarAbstractBackend *m_backend = nullptr;
+
+signals:
+    void reloaded();
+
+    // Windows
+    void windowAdded(WId windowId);
+    void windowRemoved(WId windowId);
+    void windowPropertyChanged(WId windowId, int prop);
+
+    // Workspaces
+    void workspacesCountChanged();
+    void workspaceNameChanged(int idx);
+    void currentWorkspaceChanged(int idx);
+
+    // TODO: needed?
+    void activeWindowChanged(WId windowId);
+};
