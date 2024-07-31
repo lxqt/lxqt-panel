4559a077aa1f6b105e483aa2ae42a7883a0b8180
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-07-31 20:38:00 +0200

    Weblate commit (#2081)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-backlight-menu-entry/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu-name-and-description/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-statusnotifier/el/
    Translation: LXQt Panel/plugin-backlight (Name & Description)
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-fancymenu (Name & Description)
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)
    Translation: LXQt Panel/plugin-statusnotifier
    
    Co-authored-by: Giannis Antypas <gianni.antypas@gmail.com>

---
+++ plugin-statusnotifier/translations/statusnotifier_el.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>StatusNotifierConfiguration</name>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="14"/>
+        <source>Status Notifier Settings</source>
+        <translation>Ρυθμίσεις εφαρμογής ειδοποιήσεων κατάστασης</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="28"/>
+        <location filename="../statusnotifierconfiguration.ui" line="38"/>
+        <source>An auto-hiding item will remain visible for this period if it needs attention.</source>
+        <translation>Ένα στοιχείο αυτόματης απόκρυψης θα παραμείνει ορατό για αυτή την περίοδο εάν χρήζει προσήλωσης.</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="31"/>
+        <source>Attention period:</source>
+        <translation>Περίοδος προσήλωσης:</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="41"/>
+        <source> minute(s)</source>
+        <translation> λεπτό(ά)</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="72"/>
+        <source>Change visibility of items</source>
+        <translation>Αλλαγή ορατότητας στοιχείων</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="85"/>
+        <source>Item</source>
+        <translation>Στοιχείο</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.ui" line="90"/>
+        <source>Visibility</source>
+        <translation>Ορατότητα</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Always show</source>
+        <translation>Πάντα εμφάνιση</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Auto-hide</source>
+        <translation>Αυτόματη απόκρυψη</translation>
+    </message>
+    <message>
+        <location filename="../statusnotifierconfiguration.cpp" line="84"/>
+        <source>Always hide</source>
+        <translation>Πάντα απόκρυψη</translation>
+    </message>
+</context>
+</TS>
