9cefb0cdcfe686231c63a9cf55944f109c18a854
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-10-13 22:47:41 +0200

    Weblate commit (#2142)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-cpuload-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/lg/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-cpuload (Name & Description)
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)
    
    Co-authored-by: Giannis Antypas <gianni.antypas@gmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ plugin-qeyes/translations/qeyes_lg.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>QEyesConfigDialog</name>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="44"/>
+        <source>Appearance</source>
+        <translation>Endabika</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="50"/>
+        <source>Number of eyes</source>
+        <translation>Obungi bw&apos;amaaso</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="56"/>
+        <source>Type</source>
+        <translation>Kika</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="60"/>
+        <source>Close</source>
+        <translation>Gala</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="65"/>
+        <source>Reset</source>
+        <translation>Za bugya</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="69"/>
+        <source>QEyes Settings</source>
+        <translation>Enteekateeka ya QEyes</translation>
+    </message>
+    <message>
+        <location filename="../qeyesconfigdialog.cpp" line="103"/>
+        <source>QEyes default</source>
+        <translation>Enteekateeka ya QEyes ensangwawo</translation>
+    </message>
+</context>
+</TS>
