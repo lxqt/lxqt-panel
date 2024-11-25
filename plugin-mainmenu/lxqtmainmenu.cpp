lxqt/lxqt-panel

commit 2c263d58886c6f454aad0870a4417adfb49e5241

Take into account an empty shortcut in Fancy/Main Menu (#2178)
This is especially relevant under Wayland and fixes https://github.com/lxqt/lxqt-panel/issues/2177

plugin-mainmenu/lxqtmainmenu.cpp
@@ -653,6 +653,10 @@ bool LXQtMainMenu::eventFilter(QObject *obj, QEvent *event)
     {
         if(event->type() == QEvent::KeyRelease)
         {
+            if (mShortcutSeq.isEmpty())
+            {
+                return false;
+            }
             static const auto key_meta = QMetaEnum::fromType<Qt::Key>();
             // if our shortcut key is pressed while the menu is open, close the menu
             QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);

plugin-fancymenu/lxqtfancymenu.cpp
@@ -348,6 +348,10 @@ bool LXQtFancyMenu::eventFilter(QObject *obj, QEvent *event)
     {
         if(event->type() == QEvent::KeyRelease)
         {
+            if (mShortcutSeq.isEmpty())
+            {
+                return false;
+            }
             static const auto key_meta = QMetaEnum::fromType<Qt::Key>();
             // if our shortcut key is pressed while the menu is open, close the menu
             QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);

commit 92a0289812b4fa8cf6360ec0cb6a12fb24f16629

Fixed a regression in keyboard navigation of Main Menu
It was caused by https://github.com/lxqt/lxqt-panel/commit/2c263d58886c6f454aad0870a4417adfb49e5241

plugin-mainmenu/lxqtmainmenu.cpp
@@ -653,40 +653,39 @@ bool LXQtMainMenu::eventFilter(QObject *obj, QEvent *event)
     {
         if(event->type() == QEvent::KeyRelease)
         {
-            if (mShortcutSeq.isEmpty())
-            {
-                return false;
-            }
-            static const auto key_meta = QMetaEnum::fromType<Qt::Key>();
-            // if our shortcut key is pressed while the menu is open, close the menu
-            QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
-            QFlags<Qt::KeyboardModifier> mod = keyEvent->modifiers();
-            QList<Qt::Key> keys = {static_cast<Qt::Key>(keyEvent->key())};
-            switch (keyEvent->key()) {
-                case Qt::Key_Alt:
-                    mod &= ~Qt::AltModifier;
-                    break;
-                case Qt::Key_Control:
-                    mod &= ~Qt::ControlModifier;
-                    break;
-                case Qt::Key_Shift:
-                    mod &= ~Qt::ShiftModifier;
-                    break;
-                case Qt::Key_Meta:
-                    keys << Qt::Key_Super_L << Qt::Key_Super_R;
-                    [[fallthrough]];
-                case Qt::Key_Super_L:
-                case Qt::Key_Super_R:
-                    mod &= ~Qt::MetaModifier;
-                    break;
-            }
-            for (const auto & key : std::as_const(keys))
-            {
-                const QString press = QKeySequence{static_cast<int>(mod)}.toString() % QString::fromLatin1(key_meta.valueToKey(key)).remove(0, 4);
-                if (press == mShortcutSeq)
+           QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
+
+           // if our shortcut key is pressed while the menu is open, close the menu
+            if (!mShortcutSeq.isEmpty()) {
+                static const auto key_meta = QMetaEnum::fromType<Qt::Key>();
+                QFlags<Qt::KeyboardModifier> mod = keyEvent->modifiers();
+                QList<Qt::Key> keys = {static_cast<Qt::Key>(keyEvent->key())};
+                switch (keyEvent->key()) {
+                    case Qt::Key_Alt:
+                        mod &= ~Qt::AltModifier;
+                        break;
+                    case Qt::Key_Control:
+                        mod &= ~Qt::ControlModifier;
+                        break;
+                    case Qt::Key_Shift:
+                        mod &= ~Qt::ShiftModifier;
+                        break;
+                    case Qt::Key_Meta:
+                        keys << Qt::Key_Super_L << Qt::Key_Super_R;
+                        [[fallthrough]];
+                    case Qt::Key_Super_L:
+                    case Qt::Key_Super_R:
+                        mod &= ~Qt::MetaModifier;
+                        break;
+                }
+                for (const auto & key : std::as_const(keys))
                 {
-                    mMenu->hide(); // close the app menu
-                    return true;
+                    const QString press = QKeySequence{static_cast<int>(mod)}.toString() % QString::fromLatin1(key_meta.valueToKey(key)).remove(0, 4);
+                    if (press == mShortcutSeq)
+                    {
+                        mMenu->hide(); // close the app menu
+                        return true;
+                    }
                 }
             }
 