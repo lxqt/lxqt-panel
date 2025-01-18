4124045636a83c17e9195117656c6423ae30286e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-18 22:20:04 +0100

    Weblate commit (#2218)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-kbindicator-name-and-description/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-kbindicator-name-and-description/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/lt/
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-kbindicator (Name & Description)
    Translation: LXQt Panel/plugin-taskbar
    
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Moo <hazap@hotmail.com>

--- plugin-customcommand/translations/customcommand_pt.ts
+++ plugin-customcommand/translations/customcommand_pt.ts
@@ -117,7 +117,7 @@
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="228"/>
         <source>Tooltip</source>
-        <translation type="unfinished"></translation>
+        <translation>Dica</translation>
     </message>
     <message>
         <location filename="../lxqtcustomcommandconfiguration.ui" line="272"/>
