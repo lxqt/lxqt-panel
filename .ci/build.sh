3ceff89b77d24062966e75dd2cd6fb1e07d323f1
Chih-Hsuan Yen <1937689+yan12125@users.noreply.github.com>
2025-04-22 22:48:42 +0800

    Update dependencies
    * libxtst is now used instead of libxcomposite
    
    See the port to SNI in https://github.com/lxqt/lxqt-panel/pull/1721
    
    * menu-cache is not needed anymore, either
    
    See: https://github.com/lxqt/lxqt-panel/pull/2252

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,7 +3,7 @@ set -ex
 source shared-ci/prepare-archlinux.sh
 
 # See *depends in https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=lxqt-panel-git
-pacman -S --noconfirm --needed git cmake qt6-base qt6-tools lxqt-build-tools-git alsa-lib libpulse lm_sensors libstatgrab libsysstat-git solid menu-cache libxcomposite lxqt-menu-data-git libdbusmenu-lxqt-git lxqt-globalkeys-git libxtst
+pacman -S --noconfirm --needed git cmake qt6-base qt6-tools lxqt-build-tools-git alsa-lib libpulse lm_sensors libstatgrab libsysstat-git solid lxqt-menu-data-git libdbusmenu-lxqt-git lxqt-globalkeys-git libxtst
 
 cmake -B build -S .
 make -C build
