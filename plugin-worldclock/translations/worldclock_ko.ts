8842a42c64440e6b5eb193c07955fd0d34e30217
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-08-02 12:42:00 +0200

    Weblate commit (#2312)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/ru/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-worldclock
    
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: Valera <ValeraDX@yandex.ru>
    Co-authored-by: 이정희 <daemul72@gmail.com>

--- plugin-worldclock/translations/worldclock_ko.ts
+++ plugin-worldclock/translations/worldclock_ko.ts
@@ -220,7 +220,7 @@
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="518"/>
         <source>Change displayed time zone with mouse wheel</source>
-        <translation type="unfinished"></translation>
+        <translation>마우스 휠로 표시된 시간대 변경하기</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="525"/>
