c9147bff5e8e1b0d6188814324c4e78690789115
Weblate <noreply@weblate.org>
2025-10-11 19:32:21 +0200

    Weblate commit
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: UTUMI Hirosi <utuhiro78@yahoo.co.jp>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-colorpicker/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu-name-and-description/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes-name-and-description/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-qeyes/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/cs/
    Translation: LXQt Panel/plugin-colorpicker
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-fancymenu
    Translation: LXQt Panel/plugin-fancymenu (Name & Description)
    Translation: LXQt Panel/plugin-qeyes
    Translation: LXQt Panel/plugin-qeyes (Name & Description)
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-worldclock

---
+++ plugin-colorpicker/translations/colorpicker_cs.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>ColorPickerWidget</name>
+    <message>
+        <location filename="../colorpicker.cpp" line="224"/>
+        <source>Clear list</source>
+        <translation>Vyčistit seznam</translation>
+    </message>
+    <message>
+        <location filename="../colorpicker.cpp" line="254"/>
+        <source>empty</source>
+        <translation>prázdné</translation>
+    </message>
+</context>
+</TS>
