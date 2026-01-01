06e51f3b9c1eb06e547b41002e99c6fac7ac1b6d
Weblate <noreply@weblate.org>
2026-01-01 02:28:10 +0100

    Weblate commit
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Dwi <wongciecahyono@gmail.com>
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: alwanasary <baity.jannati@gmail.com>
    Co-authored-by: therealmate <hellogaming91@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel-modul-name/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/hu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-customcommand/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-directorymenu/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/hu/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/Panel Modul Name
    Translation: LXQt Panel/plugin-customcommand
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-directorymenu
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-worldclock

--- panel/translations/lxqt-panel_hu.ts
+++ panel/translations/lxqt-panel_hu.ts
@@ -195,42 +195,42 @@
     <message>
         <location filename="../config/configplacement.cpp" line="154"/>
         <source>Top of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő teteje</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="155"/>
         <source>Left of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő bal oldala</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="156"/>
         <source>Right of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő jobb oldala</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="157"/>
         <source>Bottom of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő alja</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="166"/>
         <source>Top of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő teteje %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="167"/>
         <source>Left of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő bal oldala %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="168"/>
         <source>Right of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő jobb oldala %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="169"/>
         <source>Bottom of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyő alja %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="194"/>
--- plugin-worldclock/translations/worldclock_hu.ts
+++ plugin-worldclock/translations/worldclock_hu.ts
@@ -220,7 +220,7 @@
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="518"/>
         <source>Change displayed time zone with mouse wheel</source>
-        <translation type="unfinished"></translation>
+        <translation>Időzóna megváltoztatása az egérgörgő használatával</translation>
     </message>
     <message>
         <location filename="../lxqtworldclockconfiguration.ui" line="525"/>
