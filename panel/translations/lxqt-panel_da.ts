4a710d1e953496151a34bab15920f54c3edc6f77
Weblate <noreply@weblate.org>
2026-03-06 18:28:39 +0100

    Weblate commit
    Co-authored-by: Peter  Jespersen <flywheel@illogical.dk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/panel/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-desktopswitch/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-fancymenu/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-mainmenu/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-taskbar/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-panel/plugin-worldclock/da/
    Translation: LXQt Panel/Panel
    Translation: LXQt Panel/plugin-desktopswitch
    Translation: LXQt Panel/plugin-fancymenu
    Translation: LXQt Panel/plugin-mainmenu
    Translation: LXQt Panel/plugin-taskbar
    Translation: LXQt Panel/plugin-worldclock

--- panel/translations/lxqt-panel_da.ts
+++ panel/translations/lxqt-panel_da.ts
@@ -195,42 +195,42 @@
     <message>
         <location filename="../config/configplacement.cpp" line="154"/>
         <source>Top of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Øverst på skærmen</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="155"/>
         <source>Left of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Venstre side af skærmen</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="156"/>
         <source>Right of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Højre side af skærmen</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="157"/>
         <source>Bottom of screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederst på skærmen</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="166"/>
         <source>Top of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Øverst på skærmen %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="167"/>
         <source>Left of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Venstre side af skærmen %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="168"/>
         <source>Right of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Højre side af skærneb %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="169"/>
         <source>Bottom of screen %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederst på skærmen %1</translation>
     </message>
     <message>
         <location filename="../config/configplacement.cpp" line="194"/>
