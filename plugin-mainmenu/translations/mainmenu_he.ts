11690b7bf70d7c33476fcee8ac065fc2c16f6e1c
stefonarch <standreas@riseup.net>
2026-04-14 12:02:08 +0200

    Updated *.ts files

--- plugin-mainmenu/translations/mainmenu_he.ts
+++ plugin-mainmenu/translations/mainmenu_he.ts
@@ -145,8 +145,9 @@ Do you want to overwrite it?</source>
     </message>
     <message>
         <location filename="../lxqtmainmenuconfiguration.cpp" line="134"/>
-        <source>Currently not possible under Wayland</source>
-        <translation>כרגע בלתי אפשרי תחת Wayland</translation>
+        <source>Modify or add a shortcut to &apos;lxqt-qdbus openmenu&apos;
+in the compositor settings under Wayland</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtmainmenuconfiguration.cpp" line="181"/>
--- plugin-mainmenu/translations/mainmenu_lt.ts
+++ plugin-mainmenu/translations/mainmenu_lt.ts
@@ -145,8 +145,9 @@ Ar norite jį perrašyti?</translation>
     </message>
     <message>
         <location filename="../lxqtmainmenuconfiguration.cpp" line="134"/>
-        <source>Currently not possible under Wayland</source>
-        <translation>Šiuo metu „Wayland“ aplinkoje neįmanoma</translation>
+        <source>Modify or add a shortcut to &apos;lxqt-qdbus openmenu&apos;
+in the compositor settings under Wayland</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtmainmenuconfiguration.cpp" line="181"/>
